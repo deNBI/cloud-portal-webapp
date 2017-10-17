9b1be152f6092d359b76423b70ea501602cc0287
Maximilian Wiens <mwiens@cebitec.uni-bielefeld.de>
2017-10-17 15:52:30 +0200

    fix(project-management) fixed dependencies of the module, added new providers

--- src/app/projectmanagement/projectmanagement.module.ts
+++ src/app/projectmanagement/projectmanagement.module.ts
@@ -4,12 +4,13 @@ import ***REMOVED*** TabsModule ***REMOVED*** from 'ngx-bootstrap/tabs';
 import ***REMOVED*** OverviewComponent ***REMOVED*** from './overview.component';
 import ***REMOVED*** ProjectManagementRoutingModule ***REMOVED*** from './projectmanagement-routing.module';
 import ***REMOVED***CommonModule***REMOVED*** from "@angular/common";
-
+import ***REMOVED*** ModalModule ***REMOVED*** from 'ngx-bootstrap/modal';
 @NgModule(***REMOVED***
   imports: [
     ProjectManagementRoutingModule,
     TabsModule,
-    CommonModule
+    CommonModule,
+    ModalModule.forRoot(),
   ],
   declarations: [
     OverviewComponent
