46432c1523f82b57a596df94fc3443ada32f826d
Sören Giller <sgiller@techfak.uni-bielefeld.de>
2017-11-15 20:52:26 +0100

    feat(exceptionHandling): added first exceptionhandling to start_vm

--- src/app/applications/addvm.component.ts
+++ src/app/applications/addvm.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
 import {Image} from "../virtualmachinemodels/image";
+import {ModalDirective} from 'ngx-bootstrap/modal/modal.component';
 import {Flavor} from '../virtualmachinemodels/flavor';
 import { ImageService } from '../api-connector/image.service';
 import {FlavorService} from '../api-connector/flavor.service';
@@ -18,11 +19,13 @@ import {VirtualmachineService} from "../api-connector/virtualmachine.service";
 export class VirtualMachineComponent implements OnInit{
 
   constructor (private imageService:ImageService,private  flavorService:FlavorService,private virtualmachineservice:VirtualmachineService){}
+  data:string;
   images:Image[];
-  metadatalist:Metadata []=[]
+  metadatalist:Metadata []=[];
   flavors:Flavor[];
   selectedImage:Image;
   selectedFlavor:Flavor;
+
 
   getImages():void{
   this.imageService.getImages().subscribe(images => this.images = images);
@@ -32,11 +35,25 @@ export class VirtualMachineComponent implements OnInit{
 
   }
 
-  startVM(flavor :string,image :string,key:string ,servername:string ):void{
-   this.virtualmachineservice.startVM(flavor, image,key,servername);
+  startVM(flavor :string,image :string,key:string ,servername:string ):void {
+    if (image  && key && flavor && servername) {
 
+        this.virtualmachineservice.startVM(flavor, image, key, servername).subscribe(data => {
+          console.log(data.text());
+          this.data = data.text();
+          console.log(this.data);
+          this.printData();
+
+        });
+    }
+    else{
+      this.data="INVALID"
+      console.log(this.data)
+    }
   }
-
+  printData():void {console.log(this.data)}
+  resetData():void{if(this.data=='INVALID' ) {return; }this.data=null;}
+   resetData2():void{this.data=null;}
   onSelectFlavor(flavor: Flavor): void {
   this.selectedFlavor = flavor;
 }
