b114e1d79de3c089eea27eed3834d2f47cbace90
Sören Giller <sgiller@techfak.uni-bielefeld.de>
2017-11-15 20:52:26 +0100

    feat(exceptionHandling): added first exceptionhandling to start_vm

--- src/app/api-connector/virtualmachine.service.ts
+++ src/app/api-connector/virtualmachine.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 import {URLSearchParams} from "@angular/http";
+import {VirtualMachineComponent} from  '../applications/addvm.component'
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import {observableToBeFn} from "rxjs/testing/TestScheduler";
 
 @Injectable()
 export class VirtualmachineService {
-   constructor (private http: Http){}
 
-   startVM(flavor :string,image :string,key:string ,servername:string ,):void{
+   constructor (private http: Http){}
+  data:string;
+   startVM(flavor :string,image :string,key:string ,servername:string ,):Observable<Response>{
     let urlSearchParams=new URLSearchParams();
     urlSearchParams.append('flavor',flavor);
     urlSearchParams.append('image',image);
     urlSearchParams.append('key',key);
     urlSearchParams.append('servername',servername);
-    this.http.post('https://localhost:8443/vms/',urlSearchParams).subscribe(data => {alert('ok');},error => {console.log(JSON.stringify(error.jsn()));})
-  }
+     return this.http.post('https://localhost:8443/vms/',urlSearchParams)
+   }
 
 }
