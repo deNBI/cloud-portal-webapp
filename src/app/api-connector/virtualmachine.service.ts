a29f4694d37a1b95fac51165f87d94bf30485618
Sören Giller <sgiller@techfak.uni-bielefeld.de>
2017-11-15 20:52:26 +0100

    feat(exceptionHandling): added first exceptionhandling to start_vm

--- src/app/api-connector/virtualmachine.service.ts
+++ src/app/api-connector/virtualmachine.service.ts
@@ -1,22 +1,25 @@
 import ***REMOVED*** Injectable ***REMOVED*** from '@angular/core';
 import ***REMOVED***URLSearchParams***REMOVED*** from "@angular/http";
+import ***REMOVED***VirtualMachineComponent***REMOVED*** from  '../applications/addvm.component'
 import ***REMOVED***Http, Response, Headers, RequestOptions***REMOVED*** from '@angular/http';
 import ***REMOVED***Observable***REMOVED*** from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import ***REMOVED***observableToBeFn***REMOVED*** from "rxjs/testing/TestScheduler";
 
 @Injectable()
 export class VirtualmachineService ***REMOVED***
-   constructor (private http: Http)***REMOVED******REMOVED***
 
-   startVM(flavor :string,image :string,key:string ,servername:string ,):void***REMOVED***
+   constructor (private http: Http)***REMOVED******REMOVED***
+  data:string;
+   startVM(flavor :string,image :string,key:string ,servername:string ,):Observable<Response>***REMOVED***
     let urlSearchParams=new URLSearchParams();
     urlSearchParams.append('flavor',flavor);
     urlSearchParams.append('image',image);
     urlSearchParams.append('key',key);
     urlSearchParams.append('servername',servername);
-    this.http.post('https://localhost:8443/vms/',urlSearchParams).subscribe(data => ***REMOVED***alert('ok');***REMOVED***,error => ***REMOVED***console.log(JSON.stringify(error.jsn()));***REMOVED***)
-  ***REMOVED***
+     return this.http.post('https://localhost:8443/vms/',urlSearchParams)
+   ***REMOVED***
 
 ***REMOVED***
