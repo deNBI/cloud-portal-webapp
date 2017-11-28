e11e1d1d7fa4a8dbabaae1c0ce0ae854106caea9
David Weinholz <david-weinholz@web.de>
2017-11-28 14:18:28 +0100

    (api-connector):fix vmservice

--- src/app/api-connector/virtualmachine.service.ts
+++ src/app/api-connector/virtualmachine.service.ts
@@ -50,4 +50,11 @@ export class VirtualmachineService {
     return this.http.get('https://portal-dev.denbi.de/connector/vms/', {search: urlSearchParams}).map((res: Response) => res.json()).catch((error: any) => Observable.throw(error.json().error || 'Server error'))
   }
 
+  stopVM(elixir_id: string, openstack_id: string) {
+    let urlSearchParams = new URLSearchParams();
+    urlSearchParams.append('elixir_id', elixir_id)
+    urlSearchParams.append('openstack_id', openstack_id)
+    return this.http.post('https://portal-dev.denbi.de/connector/vms/', urlSearchParams);
+  }
+
 }
