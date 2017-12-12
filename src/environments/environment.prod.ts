affa481be925f860c498b3b380c29b74a1cd3294
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2017-12-12 15:01:07 +0100

    feat(environment): provide and document environment for dev stage and production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   vo: 3335,
+
   apiBaseUrl: 'https://cloud.denbi.de/portal/api/v0/',
   voRegistrationLink: 'https://perun.elixir-czech.cz/registrar/?vo=denbi',
+
+
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,8 @@
 export const environment = {
   vo: 3334,
   production: true,
+
   apiBaseUrl: 'https://portal-dev.denbi.de/portal/api/v0/',
   voRegistrationLink: 'https://perun.elixir-czech.cz/registrar/?vo=denbi-dev'
+
 };
