e1c54188d6a3007f7db529dc84d4546a4d870d7f
root <david-weinholz@web.de>
2018-02-28 18:49:13 +0100

    feat(application): now seperate projects types

--- src/app/applications/addapplication.component.ts
+++ src/app/applications/addapplication.component.ts
@@ -8,46 +8,65 @@ import {ApiSettings} from '../api-connector/api-settings.service'
 import {ApplicationsService} from '../api-connector/applications.service'
 
 @Component({
-  templateUrl: 'addapplication.component.html',
-  providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
+    templateUrl: 'addapplication.component.html',
+    providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
 })
 
 export class AddApplicationComponent {
 
-  //notification Modal variables
-  public notificationModalTitle: string = "Notification";
-  public notificationModalMessage: string = "Please wait...";
-  public notificationModalType: string = "info";
-  public notificationModalIsClosable: boolean = false;
-  public notificationModalStay: boolean = true;
+    //notification Modal variables
+    public notificationModalTitle: string = "Notification";
+    public notificationModalMessage: string = "Please wait...";
+    public notificationModalType: string = "info";
+    public notificationModalIsClosable: boolean = false;
+    public notificationModalStay: boolean = true;
+    showjustvm: boolean;
+    project_application_openstack_project: boolean;
 
-  csrf: Object = Cookie.get("csrftoken");
-  special_hardware: SpecialHardware[] = new Array();
 
-  constructor(private specialhardwareservice: SpecialHardwareService,
-              private  applicationsservice: ApplicationsService) {
-    this.getSpecialHardware();
-  }
+    csrf: Object = Cookie.get("csrftoken");
+    special_hardware: SpecialHardware[] = new Array();
 
-  getSpecialHardware() {
-    this.specialhardwareservice.getAllSpecialHardware().toPromise()
-      .then(result => {
-        let res = result.json();
-        for (let key in res) {
-          let shj = res[key];
-          let sh = new SpecialHardware(shj["special_hardware_id"], shj["special_hardware_key"], shj["special_hardware_name"]);
-          this.special_hardware.push(sh)
-        }
-      });
-  }
+    constructor(private specialhardwareservice: SpecialHardwareService,
+                private  applicationsservice: ApplicationsService) {
+        this.getSpecialHardware();
 
-  onSubmit(f: NgForm) {
-    f.controls['project_application_special_hardware']
-      .setValue(this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id))
-    if (!f.value['project_application_openstack_project']) {
-      f.controls['project_application_openstack_project'].setValue(false);
-     
     }
+
+    chosenProjectType(checkbox: number) {
+        if (checkbox == 0){
+            if (this.project_application_openstack_project) {
+                this.showjustvm = false;
+            }
+        }
+
+        else if (checkbox == 1) {
+            if (this.showjustvm) {
+                this.project_application_openstack_project = false;
+            }
+        }
+    }
+
+    getSpecialHardware() {
+        this.specialhardwareservice.getAllSpecialHardware().toPromise()
+            .then(result => {
+                let res = result.json();
+                for (let key in res) {
+                    let shj = res[key];
+                    let sh = new SpecialHardware(shj["special_hardware_id"], shj["special_hardware_key"], shj["special_hardware_name"]);
+                    this.special_hardware.push(sh)
+                }
+            });
+    }
+
+    onSubmit(f: NgForm) {
+    if(this.project_application_openstack_project){
+    f.controls['project_application_special_hardware']
+      .setValue(this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id))}
+
+
+
+
     this.applicationsservice.addNewApplication(f.value).toPromise()
       .then(result => {
         this.updateNotificaitonModal("Success", "The application was submitted", true, "success");
@@ -56,13 +75,13 @@ export class AddApplicationComponent {
       this.updateNotificaitonModal("Failed", "The application was not submitted, please check the required fields and try again.", true, "danger");
       this.notificationModalStay = true;
     })
-  }
+}
 
-  public updateNotificaitonModal(title: string, message: string, closable: true, type: string) {
-    this.notificationModalTitle = title;
-    this.notificationModalMessage = message;
-    this.notificationModalIsClosable = closable;
-    this.notificationModalType = type;
-  }
+    public updateNotificaitonModal(title: string, message: string, closable: true, type: string) {
+        this.notificationModalTitle = title;
+        this.notificationModalMessage = message;
+        this.notificationModalIsClosable = closable;
+        this.notificationModalType = type;
+    }
 }
 
