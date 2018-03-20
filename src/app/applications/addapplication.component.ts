4ae3cfe2dba7eb33e052144951e354288b55b78b
DavidWein94 <david-weinholz@web.de>
2018-03-20 16:42:06 +0100

    fix(checkboxes):fixed checkboxes (#58)
    * fix(checkboxes):fixed checkboxes
    
    * fix(checkbox):fixed checkob in html

--- src/app/applications/addapplication.component.ts
+++ src/app/applications/addapplication.component.ts
@@ -8,65 +8,66 @@ import ***REMOVED***ApiSettings***REMOVED*** from '../api-connector/api-settings
 import ***REMOVED***ApplicationsService***REMOVED*** from '../api-connector/applications.service'
 
 @Component(***REMOVED***
-  templateUrl: 'addapplication.component.html',
-  providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
+    templateUrl: 'addapplication.component.html',
+    providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
 ***REMOVED***)
 
 export class AddApplicationComponent ***REMOVED***
 
-  //notification Modal variables
-  public notificationModalTitle: string = "Notification";
-  public notificationModalMessage: string = "Please wait...";
-  public notificationModalType: string = "info";
-  public notificationModalIsClosable: boolean = false;
-  public notificationModalStay: boolean = true;
+    //notification Modal variables
+    public notificationModalTitle: string = "Notification";
+    public notificationModalMessage: string = "Please wait...";
+    public notificationModalType: string = "info";
+    public notificationModalIsClosable: boolean = false;
+    public notificationModalStay: boolean = true;
 
-  csrf: Object = Cookie.get("csrftoken");
-  special_hardware: SpecialHardware[] = new Array();
+    csrf: Object = Cookie.get("csrftoken");
+    special_hardware: SpecialHardware[] = new Array();
 
-  constructor(private specialhardwareservice: SpecialHardwareService,
-              private  applicationsservice: ApplicationsService) ***REMOVED***
-    this.getSpecialHardware();
-  ***REMOVED***
-
-  getSpecialHardware() ***REMOVED***
-    this.specialhardwareservice.getAllSpecialHardware().toPromise()
-      .then(result => ***REMOVED***
-        let res = result.json();
-        for (let key in res) ***REMOVED***
-          let shj = res[key];
-          let sh = new SpecialHardware(shj["special_hardware_id"], shj["special_hardware_key"], shj["special_hardware_name"]);
-          this.special_hardware.push(sh)
-        ***REMOVED***
-      ***REMOVED***);
-  ***REMOVED***
-
-  onSubmit(f: NgForm) ***REMOVED***
-    f.controls['project_application_special_hardware']
-      .setValue(this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id))
-        let values=***REMOVED******REMOVED***;
-         for(let v in f.controls)***REMOVED***
-
-        if(f.controls[v].value)***REMOVED***
-            values[v]=f.controls[v].value;
-        ***REMOVED***
+    constructor(private specialhardwareservice: SpecialHardwareService,
+                private  applicationsservice: ApplicationsService) ***REMOVED***
+        this.getSpecialHardware();
     ***REMOVED***
 
-    this.applicationsservice.addNewApplication(values).toPromise()
-      .then(result => ***REMOVED***
-        this.updateNotificaitonModal("Success", "The application was submitted", true, "success");
-        this.notificationModalStay = false;
-      ***REMOVED***).catch(error => ***REMOVED***
-      this.updateNotificaitonModal("Failed", "The application was not submitted, please check the required fields and try again.", true, "danger");
-      this.notificationModalStay = true;
-    ***REMOVED***)
-  ***REMOVED***
+    getSpecialHardware() ***REMOVED***
+        this.specialhardwareservice.getAllSpecialHardware().toPromise()
+            .then(result => ***REMOVED***
+                let res = result.json();
+                for (let key in res) ***REMOVED***
+                    let shj = res[key];
+                    let sh = new SpecialHardware(shj["special_hardware_id"], shj["special_hardware_key"], shj["special_hardware_name"]);
+                    this.special_hardware.push(sh)
+                ***REMOVED***
+            ***REMOVED***);
+    ***REMOVED***
 
-  public updateNotificaitonModal(title: string, message: string, closable: true, type: string) ***REMOVED***
-    this.notificationModalTitle = title;
-    this.notificationModalMessage = message;
-    this.notificationModalIsClosable = closable;
-    this.notificationModalType = type;
-  ***REMOVED***
+    onSubmit(f: NgForm) ***REMOVED***
+        let values = ***REMOVED******REMOVED***;
+        values['project_application_special_hardware'] = this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id)
+        for (let v in f.controls) ***REMOVED***
+
+            if (f.controls[v].value) ***REMOVED***
+                values[v] = f.controls[v].value;
+            ***REMOVED***
+        ***REMOVED***
+
+        this.applicationsservice.addNewApplication(values).toPromise()
+            .then(result => ***REMOVED***
+                this.updateNotificaitonModal("Success", "The application was submitted", true, "success");
+                this.notificationModalStay = false;
+            ***REMOVED***).catch(error => ***REMOVED***
+            this.updateNotificaitonModal("Failed", "The application was not submitted, please check the required fields and try again.", true, "danger");
+            this.notificationModalStay = true;
+        ***REMOVED***)
+
+
+    ***REMOVED***
+
+    public updateNotificaitonModal(title: string, message: string, closable: true, type: string) ***REMOVED***
+        this.notificationModalTitle = title;
+        this.notificationModalMessage = message;
+        this.notificationModalIsClosable = closable;
+        this.notificationModalType = type;
+    ***REMOVED***
 ***REMOVED***
 
