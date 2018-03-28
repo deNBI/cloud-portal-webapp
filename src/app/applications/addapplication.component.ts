b1faaa9553e378c7e3886ce48eb4e130f7a6b99d
sgiller <sgiller@techfak.uni-bielefeld.de>
2018-03-28 18:30:12 +0200

    fix(umlauts): added parser for short description

--- src/app/applications/addapplication.component.ts
+++ src/app/applications/addapplication.component.ts
@@ -61,7 +61,9 @@ export class AddApplicationComponent {
     }
 
     onSubmit(f: NgForm) {
-
+        this.parseName(f.controls['project_application_shortname'].value)
+        f.controls['project_application_shortname'].setValue(this.parseName(f.controls['project_application_shortname'].value));
+        console.log(f.controls['project_application_shortname'].value);
         let values = {};
         values['project_application_special_hardware'] = this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id)
         for (let v in f.controls) {
@@ -88,5 +90,17 @@ export class AddApplicationComponent {
         this.notificationModalIsClosable = closable;
         this.notificationModalType = type;
     }
+
+public parseName(message:string){
+    message = message.replace(/\u00e4/g, "ae");
+    message = message.replace(/\u00c4/g, "ae");
+    message = message.replace(/\u00dc/g, "ue");
+    message = message.replace(/\u00fc/g, "ue");
+    message = message.replace(/\u00d6/g, "oe");
+    message = message.replace(/\u00f6/g, "oe");
+    message = message.replace(/\u00df/g, "ss");
+    return message;
 }
+}
+
 
