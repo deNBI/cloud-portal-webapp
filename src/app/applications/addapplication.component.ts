10f1c2ba28006b332207e07c496a09ec29cc2cf6
sgiller <sgiller@techfak.uni-bielefeld.de>
2018-04-03 19:01:37 +0200

    fix(umlauts): only letters and digits are allowed for project application

--- src/app/applications/addapplication.component.ts
+++ src/app/applications/addapplication.component.ts
@@ -8,98 +8,111 @@ import ***REMOVED***ApiSettings***REMOVED*** from '../api-connector/api-settings
 import ***REMOVED***ApplicationsService***REMOVED*** from '../api-connector/applications.service'
 
 @Component(***REMOVED***
-    templateUrl: 'addapplication.component.html',
-    providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
+  templateUrl: 'addapplication.component.html',
+  providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
 ***REMOVED***)
 
 export class AddApplicationComponent ***REMOVED***
 
-    //notification Modal variables
-    public notificationModalTitle: string = "Notification";
-    public notificationModalMessage: string = "Please wait...";
-    public notificationModalType: string = "info";
-    public notificationModalIsClosable: boolean = false;
-    public notificationModalStay: boolean = true;
+  public wronginput: boolean = false;
 
-    showjustvm: boolean;
-    project_application_openstack_project: boolean;
+  //notification Modal variables
+  public notificationModalTitle: string = 'Notification';
+  public notificationModalMessage: string = 'Please wait...';
+  public notificationModalType: string = 'info';
+  public notificationModalIsClosable: boolean = false;
+  public notificationModalStay: boolean = true;
+
+  showjustvm: boolean;
+  project_application_openstack_project: boolean;
 
 
-    csrf: Object = Cookie.get("csrftoken");
-    special_hardware: SpecialHardware[] = new Array();
+  csrf: Object = Cookie.get('csrftoken');
+  special_hardware: SpecialHardware[] = new Array();
 
-    constructor(private specialhardwareservice: SpecialHardwareService,
-                private  applicationsservice: ApplicationsService) ***REMOVED***
-        this.getSpecialHardware();
+  constructor(private specialhardwareservice: SpecialHardwareService,
+              private  applicationsservice: ApplicationsService) ***REMOVED***
+    this.getSpecialHardware();
 
+  ***REMOVED***
+
+  chosenProjectType(checkbox: number) ***REMOVED***
+    if (checkbox == 0) ***REMOVED***
+      if (this.project_application_openstack_project) ***REMOVED***
+        this.showjustvm = false;
+      ***REMOVED***
     ***REMOVED***
 
-    chosenProjectType(checkbox: number) ***REMOVED***
-        if (checkbox == 0) ***REMOVED***
-            if (this.project_application_openstack_project) ***REMOVED***
-                this.showjustvm = false;
-            ***REMOVED***
+    else if (checkbox == 1) ***REMOVED***
+      if (this.showjustvm) ***REMOVED***
+        this.project_application_openstack_project = false;
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***
+
+  getSpecialHardware() ***REMOVED***
+    this.specialhardwareservice.getAllSpecialHardware().toPromise()
+      .then(result => ***REMOVED***
+        let res = result.json();
+        for (let key in res) ***REMOVED***
+          let shj = res[key];
+          let sh = new SpecialHardware(shj['special_hardware_id'], shj['special_hardware_key'], shj['special_hardware_name']);
+          this.special_hardware.push(sh)
         ***REMOVED***
+      ***REMOVED***);
+  ***REMOVED***
 
-        else if (checkbox == 1) ***REMOVED***
-            if (this.showjustvm) ***REMOVED***
-                this.project_application_openstack_project = false;
-            ***REMOVED***
+  onSubmit(f: NgForm) ***REMOVED***
+    if (this.wronginput == true) ***REMOVED***
+      this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+      this.notificationModalStay = true;
+    ***REMOVED***
+    else ***REMOVED***
+      let values = ***REMOVED******REMOVED***;
+      values['project_application_special_hardware'] = this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id)
+      for (let v in f.controls) ***REMOVED***
+        if (f.controls[v].value) ***REMOVED***
+          values[v] = f.controls[v].value;
         ***REMOVED***
+      ***REMOVED***
+
+
+      this.applicationsservice.addNewApplication(values).toPromise()
+        .then(result => ***REMOVED***
+          this.updateNotificaitonModal('Success', 'The application was submitted', true, 'success');
+          this.notificationModalStay = false;
+        ***REMOVED***).catch(error => ***REMOVED***
+        this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+        this.notificationModalStay = true;
+      ***REMOVED***)
     ***REMOVED***
+  ***REMOVED***
 
-    getSpecialHardware() ***REMOVED***
-        this.specialhardwareservice.getAllSpecialHardware().toPromise()
-            .then(result => ***REMOVED***
-                let res = result.json();
-                for (let key in res) ***REMOVED***
-                    let shj = res[key];
-                    let sh = new SpecialHardware(shj["special_hardware_id"], shj["special_hardware_key"], shj["special_hardware_name"]);
-                    this.special_hardware.push(sh)
-                ***REMOVED***
-            ***REMOVED***);
+
+  public updateNotificaitonModal(title: string, message: string, closable: true, type: string) ***REMOVED***
+    this.notificationModalTitle = title;
+    this.notificationModalMessage = message;
+    this.notificationModalIsClosable = closable;
+    this.notificationModalType = type;
+  ***REMOVED***
+
+  public resetModal()***REMOVED***
+
+     this.notificationModalTitle = 'Notification';
+     this.notificationModalMessage = 'Please wait...';
+     this.notificationModalType = 'info';
+     this.notificationModalIsClosable = false;
+     this.notificationModalStay = true;
+  ***REMOVED***
+
+  public checkShortname(shortname: string) ***REMOVED***
+    if (/^[a-zA-Z0-9\s]*$/.test(shortname) == false) ***REMOVED***
+      this.wronginput = true;
     ***REMOVED***
-
-    onSubmit(f: NgForm) ***REMOVED***
-        this.parseName(f.controls['project_application_shortname'].value)
-        f.controls['project_application_shortname'].setValue(this.parseName(f.controls['project_application_shortname'].value));
-        let values = ***REMOVED******REMOVED***;
-        values['project_application_special_hardware'] = this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id)
-        for (let v in f.controls) ***REMOVED***
-            if (f.controls[v].value) ***REMOVED***
-                values[v] = f.controls[v].value;
-            ***REMOVED***
-        ***REMOVED***
-
-
-        this.applicationsservice.addNewApplication(values).toPromise()
-            .then(result => ***REMOVED***
-                this.updateNotificaitonModal("Success", "The application was submitted", true, "success");
-                this.notificationModalStay = false;
-            ***REMOVED***).catch(error => ***REMOVED***
-            this.updateNotificaitonModal("Failed", "The application was not submitted, please check the required fields and try again.", true, "danger");
-            this.notificationModalStay = true;
-        ***REMOVED***)
+    else ***REMOVED***
+      this.wronginput = false;
     ***REMOVED***
-
-
-    public updateNotificaitonModal(title: string, message: string, closable: true, type: string) ***REMOVED***
-        this.notificationModalTitle = title;
-        this.notificationModalMessage = message;
-        this.notificationModalIsClosable = closable;
-        this.notificationModalType = type;
-    ***REMOVED***
-
-public parseName(message:string)***REMOVED***
-    message = message.replace(/\u00e4/g, "ae");
-    message = message.replace(/\u00c4/g, "ae");
-    message = message.replace(/\u00dc/g, "ue");
-    message = message.replace(/\u00fc/g, "ue");
-    message = message.replace(/\u00d6/g, "oe");
-    message = message.replace(/\u00f6/g, "oe");
-    message = message.replace(/\u00df/g, "ss");
-    return message;
-***REMOVED***
+  ***REMOVED***
 ***REMOVED***
 
 
