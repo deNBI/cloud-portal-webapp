aa64c5763e1004efff560f7e3be6c283d583df4c
DavidWein94 <david-weinholz@web.de>
2018-04-18 10:28:08 +0200

    Feeature/do not allow 0 (#98)
    * feat(projects): 0 is not allowed anymore
    
    * feat(projects):make fields required strong
    
    * fix(applications): fixed bug,also object storage can be zero again
    
    * fix(applications):removead an console.log

--- src/app/applications/addapplication.component.ts
+++ src/app/applications/addapplication.component.ts
@@ -8,111 +8,133 @@ import ***REMOVED***ApiSettings***REMOVED*** from '../api-connector/api-settings
 import ***REMOVED***ApplicationsService***REMOVED*** from '../api-connector/applications.service'
 
 @Component(***REMOVED***
-  templateUrl: 'addapplication.component.html',
-  providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
+    templateUrl: 'addapplication.component.html',
+    providers: [SpecialHardwareService, ApiSettings, ApplicationsService]
 ***REMOVED***)
 
 export class AddApplicationComponent ***REMOVED***
 
-  public wronginput: boolean = false;
+    public wronginput: boolean = false;
 
-  //notification Modal variables
-  public notificationModalTitle: string = 'Notification';
-  public notificationModalMessage: string = 'Please wait...';
-  public notificationModalType: string = 'info';
-  public notificationModalIsClosable: boolean = false;
-  public notificationModalStay: boolean = true;
+    //notification Modal variables
+    public notificationModalTitle: string = 'Notification';
+    public notificationModalMessage: string = 'Please wait...';
+    public notificationModalType: string = 'info';
+    public notificationModalIsClosable: boolean = false;
+    public notificationModalStay: boolean = true;
 
-  showjustvm: boolean;
-  project_application_openstack_project: boolean;
+    showjustvm: boolean;
+    project_application_openstack_project: boolean;
 
 
-  csrf: Object = Cookie.get('csrftoken');
-  special_hardware: SpecialHardware[] = new Array();
+    csrf: Object = Cookie.get('csrftoken');
+    special_hardware: SpecialHardware[] = new Array();
 
-  constructor(private specialhardwareservice: SpecialHardwareService,
-              private  applicationsservice: ApplicationsService) ***REMOVED***
-    this.getSpecialHardware();
+    constructor(private specialhardwareservice: SpecialHardwareService,
+                private  applicationsservice: ApplicationsService) ***REMOVED***
+        this.getSpecialHardware();
 
-  ***REMOVED***
-
-  chosenProjectType(checkbox: number) ***REMOVED***
-    if (checkbox == 0) ***REMOVED***
-      if (this.project_application_openstack_project) ***REMOVED***
-        this.showjustvm = false;
-      ***REMOVED***
     ***REMOVED***
 
-    else if (checkbox == 1) ***REMOVED***
-      if (this.showjustvm) ***REMOVED***
-        this.project_application_openstack_project = false;
-      ***REMOVED***
-    ***REMOVED***
-  ***REMOVED***
-
-  getSpecialHardware() ***REMOVED***
-    this.specialhardwareservice.getAllSpecialHardware().toPromise()
-      .then(result => ***REMOVED***
-        let res = result.json();
-        for (let key in res) ***REMOVED***
-          let shj = res[key];
-          let sh = new SpecialHardware(shj['special_hardware_id'], shj['special_hardware_key'], shj['special_hardware_name']);
-          this.special_hardware.push(sh)
+    chosenProjectType(checkbox: number) ***REMOVED***
+        if (checkbox == 0) ***REMOVED***
+            if (this.project_application_openstack_project) ***REMOVED***
+                this.showjustvm = false;
+            ***REMOVED***
         ***REMOVED***
-      ***REMOVED***);
-  ***REMOVED***
 
-  onSubmit(f: NgForm) ***REMOVED***
-    if (this.wronginput == true) ***REMOVED***
-      this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
-      this.notificationModalStay = true;
-    ***REMOVED***
-    else ***REMOVED***
-      let values = ***REMOVED******REMOVED***;
-      values['project_application_special_hardware'] = this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id)
-      for (let v in f.controls) ***REMOVED***
-        if (f.controls[v].value) ***REMOVED***
-          values[v] = f.controls[v].value;
+        else if (checkbox == 1) ***REMOVED***
+            if (this.showjustvm) ***REMOVED***
+                this.project_application_openstack_project = false;
+            ***REMOVED***
         ***REMOVED***
-      ***REMOVED***
+    ***REMOVED***
+
+    getSpecialHardware() ***REMOVED***
+        this.specialhardwareservice.getAllSpecialHardware().toPromise()
+            .then(result => ***REMOVED***
+                let res = result.json();
+                for (let key in res) ***REMOVED***
+                    let shj = res[key];
+                    let sh = new SpecialHardware(shj['special_hardware_id'], shj['special_hardware_key'], shj['special_hardware_name']);
+                    this.special_hardware.push(sh)
+                ***REMOVED***
+            ***REMOVED***);
+    ***REMOVED***
+
+    check_not_zero(values: ***REMOVED******REMOVED***) ***REMOVED***
+        if ('project_application_openstack_project' in values) ***REMOVED***
+            
+
+            if ('project_application_cores_per_vm' in values && values['project_application_cores_per_vm'] > 0 && 'project_application_ram_per_vm' in values
+                && values['project_application_ram_per_vm'] > 0 && 'project_application_disk_space' in values && values['project_application_disk_space'] > 0) ***REMOVED***
+                return true;
+            ***REMOVED***
+
+            return false;
+        ***REMOVED***
+        else ***REMOVED***
+
+            return true;
+        ***REMOVED***
+    ***REMOVED***
+
+    onSubmit(f: NgForm) ***REMOVED***
+        if (this.wronginput == true) ***REMOVED***
+            this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+            this.notificationModalStay = true;
+        ***REMOVED***
+        else ***REMOVED***
+            let values = ***REMOVED******REMOVED***;
+            values['project_application_special_hardware'] = this.special_hardware.filter(hardware => hardware.Checked).map(hardware => hardware.Id)
+            for (let v in f.controls) ***REMOVED***
+                if (f.controls[v].value) ***REMOVED***
+                    values[v] = f.controls[v].value;
+                ***REMOVED***
+            ***REMOVED***
+            if (this.check_not_zero(values) == false) ***REMOVED***
+                this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+                this.notificationModalStay = true;
+                return;
+            ***REMOVED***
 
 
-      this.applicationsservice.addNewApplication(values).toPromise()
-        .then(result => ***REMOVED***
-          this.updateNotificaitonModal('Success', 'The application was submitted', true, 'success');
-          this.notificationModalStay = false;
-        ***REMOVED***).catch(error => ***REMOVED***
-        this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+            this.applicationsservice.addNewApplication(values).toPromise()
+                .then(result => ***REMOVED***
+                    this.updateNotificaitonModal('Success', 'The application was submitted', true, 'success');
+                    this.notificationModalStay = false;
+                ***REMOVED***).catch(error => ***REMOVED***
+                this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+                this.notificationModalStay = true;
+            ***REMOVED***)
+        ***REMOVED***
+    ***REMOVED***
+
+
+    public updateNotificaitonModal(title: string, message: string, closable: true, type: string) ***REMOVED***
+        this.notificationModalTitle = title;
+        this.notificationModalMessage = message;
+        this.notificationModalIsClosable = closable;
+        this.notificationModalType = type;
+    ***REMOVED***
+
+    public resetModal() ***REMOVED***
+
+        this.notificationModalTitle = 'Notification';
+        this.notificationModalMessage = 'Please wait...';
+        this.notificationModalType = 'info';
+        this.notificationModalIsClosable = false;
         this.notificationModalStay = true;
-      ***REMOVED***)
     ***REMOVED***
-  ***REMOVED***
 
-
-  public updateNotificaitonModal(title: string, message: string, closable: true, type: string) ***REMOVED***
-    this.notificationModalTitle = title;
-    this.notificationModalMessage = message;
-    this.notificationModalIsClosable = closable;
-    this.notificationModalType = type;
-  ***REMOVED***
-
-  public resetModal()***REMOVED***
-
-     this.notificationModalTitle = 'Notification';
-     this.notificationModalMessage = 'Please wait...';
-     this.notificationModalType = 'info';
-     this.notificationModalIsClosable = false;
-     this.notificationModalStay = true;
-  ***REMOVED***
-
-  public checkShortname(shortname: string) ***REMOVED***
-    if (/^[a-zA-Z0-9\s]*$/.test(shortname) == false) ***REMOVED***
-      this.wronginput = true;
+    public checkShortname(shortname: string) ***REMOVED***
+        if (/^[a-zA-Z0-9\s]*$/.test(shortname) == false) ***REMOVED***
+            this.wronginput = true;
+        ***REMOVED***
+        else ***REMOVED***
+            this.wronginput = false;
+        ***REMOVED***
     ***REMOVED***
-    else ***REMOVED***
-      this.wronginput = false;
-    ***REMOVED***
-  ***REMOVED***
 ***REMOVED***
 
 
