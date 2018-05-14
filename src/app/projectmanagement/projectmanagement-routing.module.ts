80c9f3511de88637a00e44a34db44e9b410e5d0b
David Weinholz <david-weinholz@web.de>
2018-05-14 11:22:30 +0200

    feat(image):first link to image tags

--- src/app/projectmanagement/projectmanagement-routing.module.ts
+++ src/app/projectmanagement/projectmanagement-routing.module.ts
@@ -1,29 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {FacilityProjectsOverviewComponent} from "./facilityprojectsoverview.component";
-import { OverviewComponent } from './overview.component';
+import {OverviewComponent} from './overview.component';
 
 const routes: Routes = [
-  {
-    path: 'project-management',
-    component: OverviewComponent,
-    data: {
-      title: 'Project overview'
+    {
+        path: 'project-management',
+        component: OverviewComponent,
+        data: {
+            title: 'Project overview'
+        }
+
+    },
+    {
+        path: 'facilityProjects',
+        component: FacilityProjectsOverviewComponent,
+        data: {
+            title: 'Facility Projects'
+        }
+
+    }, {
+        path: 'imageTags',
+        component: FacilityProjectsOverviewComponent,
+        data: {
+            title: 'Image Tags'
+        }
     }
 
-  },
-      {
-    path: 'facilityProjects',
-    component: FacilityProjectsOverviewComponent,
-    data: {
-      title: 'Facility Projects'
-    }
-
-  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class ProjectManagementRoutingModule {}
+export class ProjectManagementRoutingModule {
+}
