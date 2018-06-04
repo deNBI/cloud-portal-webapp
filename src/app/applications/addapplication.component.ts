3697c3fdda4a23cb86eb8f4c620c44139ce84439
David Weinholz <david-weinholz@web.de>
2018-06-04 12:19:13 +0200

    feat(applicaton):now it shows in the modal which inputs where wrong

--- src/app/applications/addapplication.component.ts
+++ src/app/applications/addapplication.component.ts
@@ -22,9 +22,11 @@ export class AddApplicationComponent ***REMOVED***
     public notificationModalType: string = 'info';
     public notificationModalIsClosable: boolean = false;
     public notificationModalStay: boolean = true;
+    public error: string[];
+
 
     public acknowledgeModalMessage: string = 'The development and support of the cloud is possible above all through the funding of the cloud infrastructure by the Federal Ministry of Education and Research (BMBF)!\n' +
-      'We would highly appreciate the following citation in your next publication(s): ‘This work was supported by the BMBF-funded de.NBI Cloud within the German Network for Bioinformatics Infrastructure (de.NBI) (031A537B, 031A533A, 031A538A, 031A533B, 031A535A, 031A537C, 031A534A, 031A532B).';
+        'We would highly appreciate the following citation in your next publication(s): ‘This work was supported by the BMBF-funded de.NBI Cloud within the German Network for Bioinformatics Infrastructure (de.NBI) (031A537B, 031A533A, 031A538A, 031A533B, 031A535A, 031A537C, 031A534A, 031A532B).';
     public acknowledgeModalTitle: string = 'Acknowledge';
     public acknowledgeModalType: string = 'info';
 
@@ -86,6 +88,7 @@ export class AddApplicationComponent ***REMOVED***
     ***REMOVED***
 
     onSubmit(f: NgForm) ***REMOVED***
+        this.error = null;
         if (this.wronginput == true) ***REMOVED***
             this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
             this.notificationModalStay = true;
@@ -110,6 +113,14 @@ export class AddApplicationComponent ***REMOVED***
                     this.updateNotificaitonModal('Success', 'The application was submitted', true, 'success');
                     this.notificationModalStay = false;
                 ***REMOVED***).catch(error => ***REMOVED***
+                var error_json = error.json()
+                this.error = []
+                for (let key of Object.keys(error_json)) ***REMOVED***
+                    this.error.push(key.split('_',)[2])
+
+                ***REMOVED***
+
+
                 this.updateNotificaitonModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
                 this.notificationModalStay = true;
             ***REMOVED***)
