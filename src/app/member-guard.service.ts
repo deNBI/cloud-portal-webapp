49eb0a4c813a9534a87c2fb687ddf372f6b79b76
DavidWein94 <david-weinholz@web.de>
2018-06-12 15:31:21 +0200

    feat(guard):if logged in registration, if not redirect to login (#153)

--- src/app/member-guard.service.ts
+++ src/app/member-guard.service.ts
@@ -3,36 +3,49 @@ import {CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot} from "
 import {MembersManager} from "./perun-connector/members-manager.service";
 import {AuthzResolver} from "./perun-connector/authz-resolver.service";
 import {Observable} from "rxjs/Observable";
+import {environment} from "../environments/environment";
 
 
 @Injectable()
 export class MemberGuardService implements CanActivate {
 
 
-  constructor(private router: Router, private  authzresolver: AuthzResolver, private memberssmanager: MembersManager) {
-  }
+    constructor(private router: Router, private  authzresolver: AuthzResolver, private memberssmanager: MembersManager) {
+    }
 
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return new Promise((resolve, reject) => {
-      this.authzresolver.getLoggedUser().toPromise()
-        .then(result => {
-          let res = result.json();
-          return this.memberssmanager.getMemberByUser(res['id']).toPromise();
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return new Promise((resolve, reject) => {
+            this.authzresolver.getLoggedUser().toPromise()
+                .then(result => {
 
-        }).then(memberinfo => {
-        return resolve(true);
+                    let res = result.json();
+
+                    return res
+
+                }).then(res => {
+
+                this.memberssmanager.getMemberByUser(res['id']).toPromise().then(memberinfo => {
+                    return resolve(true);
+
+                }).catch(rejection => {
+
+                    this.router.navigate(['/registration-info']);
+                    resolve(false);
+
+                });
+            }).catch(rejection => {
+
+                //this.router.navigate(['/portal']);
+                window.location.href=environment.login
+                resolve(false);
+
+            });
+
+        })
 
 
-      }).catch(rejection => {
-        this.router.navigate(['/registration-info']);
-        resolve(false);
-
-      });
-    });
-
-
-  }
+    }
 }
 
 
