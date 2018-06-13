588b7b060517aa085a3926cc963223314bff0407
David Weinholz <david-weinholz@web.de>
2018-06-13 17:10:57 +0200

    feat(freemium):added freemium_group_id to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   vo: 3334,
-
+  freemium_project_id:10392,
   apiBaseUrl: 'https://portal-dev.denbi.de/api/v0/',
   voRegistrationLink: 'https://perun.elixir-czech.cz/registrar/?vo=denbi-dev',
   connectorBaseUrl : 'https://portal-dev.denbi.de/connector/'
