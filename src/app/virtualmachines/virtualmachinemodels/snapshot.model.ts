b3b56c9272cbfe5a8b702956b4a51f2f55ff4d75
DavidWein94 <david-weinholz@web.de>
2018-09-05 23:03:24 +0200

    Feature/snapshot visible (#213)
    * feat(registrar):added invitation link
    
    * feat(group):added application service methods
    
    * feat(registrar): addedapplication model
    
    * feat(registrar):added modal
    
    * feat(registrar):reload application for proejct after approval
    
    * feat(registrar):feedback if sucesfully approved or rejected
    
    * feat(registrar): added feedback
    
    * feat(snapshot):now visibe for tea members in overview
    
    * feat(snapshot):now visibe for tea members in overview
    
    * feat(snapshot):buttons just visible if user created snapshot
    
    * refactor(snapshot):changed the text

--- src/app/virtualmachines/virtualmachinemodels/snapshot.model.ts
+++ src/app/virtualmachines/virtualmachinemodels/snapshot.model.ts
@@ -6,4 +6,6 @@ export class SnapshotModel ***REMOVED***
     snapshot_openstackid: string;
     snapshot_client = Vmclient;
     snapshot_name: string;
+    snapshot_created:boolean;
+    snapshot_project:string;
 ***REMOVED***
