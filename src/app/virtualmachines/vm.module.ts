9fcf945f52416840992cd30343c4046fcb6d6601
David Weinholz <david-weinholz@web.de>
2018-10-17 12:32:11 +0200

    feat(key):own module

--- src/app/virtualmachines/vm.module.ts
+++ src/app/virtualmachines/vm.module.ts
@@ -14,11 +14,12 @@ import ***REMOVED***VolumeOverviewComponent***REMOVED*** from "./volumeOverview.
 import ***REMOVED***SnapshotOverviewComponent***REMOVED*** from "./snapshotOverview.component";
 import ***REMOVED***PaginationModule***REMOVED*** from 'ngx-bootstrap/pagination'
 import ***REMOVED***UserinfoModule***REMOVED*** from "../userinfo/userinfo.module";
+import ***REMOVED***PublicKeyModule***REMOVED*** from "../shared_modules/public-key/public-key.module";
 
 
 @NgModule(***REMOVED***
     imports: [
-        UserinfoModule,
+        PublicKeyModule,
         VmRoutingModule,
         TabsModule,
         CommonModule,
