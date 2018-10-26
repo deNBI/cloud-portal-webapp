9786dd96d2ce0c8a8c26690f6ff41a9ae868dd1e
David Weinholz <david-weinholz@web.de>
2018-10-26 16:42:05 +0200

    feat(flavor):added flavor type model

--- src/app/virtualmachines/virtualmachinemodels/flavor.ts
+++ src/app/virtualmachines/virtualmachinemodels/flavor.ts
@@ -1,12 +1,14 @@
+import ***REMOVED***FlavorType***REMOVED*** from "./flavorType";
+
 export class Flavor ***REMOVED***
-  id: string;
-  name: string;
-  vcpus: number;
-  ram: number;
-  rootdisk: number;
-  gpu:number;
-  epheremal_disk:number;
-  type:string;
+    id: string;
+    name: string;
+    vcpus: number;
+    ram: number;
+    rootdisk: number;
+    gpu: number;
+    epheremal_disk: number;
+    type: FlavorType;
 
 ***REMOVED***
 
