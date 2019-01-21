ad7f5cd606277e0c6180814fc0ee656774595a0b
David Weinholz <david-weinholz@web.de>
2019-01-21 18:04:14 +0100

    feat(Application):added test app btn

--- src/app/applications/addcloudapplication.component.ts
+++ src/app/applications/addcloudapplication.component.ts
@@ -240,6 +240,7 @@ export class AddcloudapplicationComponent extends AbstractBaseClasse {
 
     }
 
+
     /**
      * Get all Special Hardware.
      */
@@ -295,6 +296,43 @@ export class AddcloudapplicationComponent extends AbstractBaseClasse {
                 this.notificationModalStay = true;
             })
         }
+    }
+
+
+    sendTestApplication() {
+        let values: { [key: string]: any } = {};
+
+        values['project_application_comment'] = 'TestApplication';
+        values['project_application_description'] = 'TestApplication';
+        values['project_application_institute'] = 'TestApplication';
+        values['project_application_lifetime'] = 3;
+        values['project_application_name'] = 'TestApplication';
+        values['project_application_openstack_project'] = true;
+        values['project_application_report_allowed'] = true;
+        values['project_application_shortname'] = 'TestApplication';
+        values['project_application_special_hardware'] = [1, 2];
+        values['project_application_volume_counter'] = 5;
+        values['project_application_volume_limit'] = 20;
+        values['project_application_workgroup'] = 'TestApplication'
+
+        this.applicationsservice.addNewApplication(values).toPromise()
+            .then(result => {
+                this.updateNotificationModal('Success', 'The application was submitted', true, 'success');
+                this.notificationModalStay = false;
+            }).catch(error => {
+            var error_json = error
+            this.error = []
+            for (let key of Object.keys(error_json)) {
+                this.error.push(key.split('_',)[2])
+
+            }
+
+
+            this.updateNotificationModal('Failed', 'The application was not submitted, please check the required fields and try again.', true, 'danger');
+            this.notificationModalStay = true;
+        })
+
+
     }
 
 
