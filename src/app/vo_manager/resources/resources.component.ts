089b70fb0f62d05d2588b93ca72f2a0bf2c9e507
David Weinholz <david-weinholz@web.de>
2019-02-07 16:24:19 +0100

    feat(Resources):now at vo listet for all facilities

--- src/app/vo_manager/resources/resources.component.ts
+++ src/app/vo_manager/resources/resources.component.ts
@@ -1,5 +1,9 @@
 import ***REMOVED***Component, OnInit***REMOVED*** from '@angular/core';
 import ***REMOVED***VoService***REMOVED*** from "../../api-connector/vo.service";
+import ***REMOVED***FacilityService***REMOVED*** from "../../api-connector/facility.service";
+import * as jspdf from 'jspdf';
+import ***REMOVED***Resources***REMOVED*** from "./resources";
+import html2canvas from 'html2canvas';
 
 @Component(***REMOVED***
     selector: 'app-resources',
@@ -10,84 +14,31 @@ import ***REMOVED***VoService***REMOVED*** from "../../api-connector/vo.service"
 export class ResourcesComponent implements OnInit ***REMOVED***
 
     isLoaded = false;
-
-    simpleVmTotalRam = 0;
-    simpleVmTotalCores = 0;
-    simpleVmTotalVms = 0;
-    simpleVmTotalVolumeLimit = 0;
-    simpleVmTotalVolumeCounter = 0;
-
-    openStackApprovedTotalRam = 0;
-    openStackApprovedTotalCores = 0;
-    openStackApprovedTotalVms = 0;
-    openStackApprovedTotalVolumeLimit = 0;
-    openStackApprovedTotalVolumeCounter = 0;
-    openStackApprovedTotalObjectStorage = 0;
-    openStackApprovedTotalFPGA = 0;
-    openStackApprovedTotalGPU = 0;
-
-    openStackWFCTotalRam = 0;
-    openStackWFCTotalCores = 0;
-    openStackWFCTotalVms = 0;
-    openStackWFCTotalVolumeLimit = 0;
-    openStackWFCTotalVolumeCounter = 0;
-    openStackWFCTotalObjectStorage = 0;
-    openStackWFCTotalFPGA = 0;
-    openStackWFCTotalGPU = 0;
-    totalRam = 0;
-    totalCores = 0;
-    totalVms = 0;
-    totalVolumeLimit = 0;
-    totalVolumeCounter = 0;
-    totalObjectStorage = 0;
-    totalFPGA = 0;
-    totalGPU = 0;
+    voResources: Resources[] = [];
+    totalResource: Resources;
 
 
-    constructor(private  facilityService: FacilityService) ***REMOVED***
-        this.facilityService.getManagerFacilities().subscribe(result => ***REMOVED***
-            this.managerFacilities = result;
-            this.selectedFacility = this.managerFacilities[0];
-            this.getSelectedFacilityResources()
+    constructor(private voservice: VoService) ***REMOVED***
+        this.getVoProjectResources()
 
-
-        ***REMOVED***)
     ***REMOVED***
 
-    public getSelectedFacilityResources() ***REMOVED***
-        this.facilityService.getFacilityResources(this.selectedFacility['FacilityId']).subscribe(res => ***REMOVED***
-            this.simpleVmTotalRam = res['simpleVmApplications']['totalRam'];
-            this.simpleVmTotalCores = res['simpleVmApplications']['totalCores'];
-            this.simpleVmTotalVms = res['simpleVmApplications']['totalVms'];
-            this.simpleVmTotalVolumeLimit = res['simpleVmApplications']['totalVolumeLimit'];
-            this.simpleVmTotalVolumeCounter = res['simpleVmApplications']['totalVolumeCounter'];
+    public getVoProjectResources() ***REMOVED***
+        this.voservice.getVoProjectResources().subscribe(res => ***REMOVED***
+            for (let r in res) ***REMOVED***
+                if (r != 'Total') ***REMOVED***
+                    let resource = new Resources(r, res[r]['totalRam'], res[r]['totalCores'],
+                        res[r]['totalVms'], res[r]['totalVolumeLimit'], res[r]['totalVolumeCounter'],
+                        res[r]['totalObjectStorage'], res[r]['totalFPGA'], res[r]['totalGPU']);
+                    this.voResources.push(resource);
+                ***REMOVED***
+                else ***REMOVED***
+                    this.totalResource = new Resources('Total', res['Total']['totalRam'], res['Total']['totalCores'], res['Total']['totalVms'], res['Total']['totalVolumeLimit'],
+                        res['Total']['totalVolumeCounter'], res['Total']['totalObjectStorage'], res['Total']['totalFPGA'], res['Total']['totalGPU']);
+                ***REMOVED***
+                console.log(this.voResources)
+            ***REMOVED***
 
-            this.openStackApprovedTotalRam = res['approvedOpenStackApplications']['totalRam'];
-            this.openStackApprovedTotalCores = res['approvedOpenStackApplications']['totalCores'];
-            this.openStackApprovedTotalVms = res['approvedOpenStackApplications']['totalVms'];
-            this.openStackApprovedTotalVolumeLimit = res['approvedOpenStackApplications']['totalVolumeLimit'];
-            this.openStackApprovedTotalVolumeCounter = res['approvedOpenStackApplications']['totalVolumeCounter'];
-            this.openStackApprovedTotalObjectStorage = res['approvedOpenStackApplications']['totalObjectStorage'];
-            this.openStackApprovedTotalFPGA = res['approvedOpenStackApplications']['totalFPGA'];
-            this.openStackApprovedTotalGPU = res['approvedOpenStackApplications']['totalGPU'];
-
-            this.openStackWFCTotalRam = res['wfcOpenStackApplications']['totalRam'];
-            this.openStackWFCTotalCores = res['wfcOpenStackApplications']['totalCores'];
-            this.openStackWFCTotalVms = res['wfcOpenStackApplications']['totalVms'];
-            this.openStackWFCTotalVolumeLimit = res['wfcOpenStackApplications']['totalVolumeLimit'];
-            this.openStackWFCTotalVolumeCounter = res['wfcOpenStackApplications']['totalVolumeCounter'];
-            this.openStackWFCTotalObjectStorage = res['wfcOpenStackApplications']['totalObjectStorage'];
-            this.openStackWFCTotalFPGA = res['wfcOpenStackApplications']['totalFPGA'];
-            this.openStackWFCTotalGPU = res['wfcOpenStackApplications']['totalGPU'];
-
-            this.totalRam = res['total']['totalRam'];
-            this.totalCores = res['total']['totalCores'];
-            this.totalVms = res['total']['totalVms'];
-            this.totalVolumeLimit = res['total']['totalVolumeLimit'];
-            this.totalVolumeCounter = res['total']['totalVolumeCounter'];
-            this.totalObjectStorage = res['total']['totalObjectStorage'];
-            this.totalFPGA = res['total']['totalFPGA'];
-            this.totalGPU = res['total']['totalGPU'];
 
             this.isLoaded = true;
         ***REMOVED***)
@@ -108,14 +59,10 @@ export class ResourcesComponent implements OnInit ***REMOVED***
             let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
             var position = 0;
             pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-            pdf.save(this.selectedFacility['Facility'] + '.pdf'); // Generated PDF
+            pdf.save('VoResources.pdf'); // Generated PDF
         ***REMOVED***);
     ***REMOVED***
 
-
-    onChangeSelectedFacility(value) ***REMOVED***
-        this.getSelectedFacilityResources()
-    ***REMOVED***
 
     ngOnInit() ***REMOVED***
     ***REMOVED***
