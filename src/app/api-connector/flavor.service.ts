86dc7a91159552d84be5b5c9736a879e100d3e80
David Weinholz <david-weinholz@web.de>
2019-02-08 10:14:18 +0100

    feat(Flavors):now public url

--- src/app/api-connector/flavor.service.ts
+++ src/app/api-connector/flavor.service.ts
@@ -13,28 +13,24 @@ export class FlavorService {
     constructor(private http: HttpClient, private settings: ApiSettings) {
     }
 
-    getFlavors(project_id:number): Observable<Flavor[]> {
-                let params = new HttpParams().set('project_id', project_id.toString());
-
-
-        return this.http.get<Flavor[]>(this.settings.getConnectorBaseUrl() + 'flavors/', {
+    getFlavors(project_id: number): Observable<Flavor[]> {
+        return this.http.get<Flavor[]>(this.settings.getConnectorBaseUrl() + 'projects/' + project_id + '/flavors/', {
             withCredentials: true,
-            params:params
 
         }).pipe(catchError((error: any) => throwError(error)));
 
     }
 
     getListOfTypesAvailable(): Observable<FlavorType[]> {
-      return this.http.get<FlavorType[]>(this.settings.getConnectorBaseUrl() + 'project_applications/flavorTypes/', {
-        withCredentials: true
-      }).pipe(catchError((error: any) => throwError(error)));
+        return this.http.get<FlavorType[]>(this.settings.getConnectorBaseUrl() + 'project_applications/flavorTypes/', {
+            withCredentials: true
+        }).pipe(catchError((error: any) => throwError(error)));
     }
 
     getListOfFlavorsAvailable(): Observable<Flavor[]> {
-      return this.http.get<Flavor[]>(this.settings.getConnectorBaseUrl() + 'project_applications/flavors/',{
-        withCredentials: true
-      }).pipe(catchError((error: any) => throwError(error)));
+        return this.http.get<Flavor[]>(this.settings.getConnectorBaseUrl() + 'project_applications/flavors/', {
+            withCredentials: true
+        }).pipe(catchError((error: any) => throwError(error)));
     }
 
 }
