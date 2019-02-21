acbc5c4631f0bb8e43c2e7bc33fc92a51245a775
David Weinholz <david-weinholz@web.de>
2019-02-21 13:01:27 +0100

    feat(Applications):added dissemination

--- src/app/applications/application.model.ts
+++ src/app/applications/application.model.ts
@@ -4,283 +4,304 @@ import {Flavor} from '../virtualmachines/virtualmachinemodels/flavor';
 
 
 export class Application {
-  private _Id: number;
-  private _Name: string;
-  private _Shortname: string;
-  private _Institute: string;
-  private _Workgroup: string;
-  private _Lifetime: number;
-  private _VMsRequested: number;
-  private _CoresPerVM: number;
-  private _RamPerVM: number;
-  private _VolumeLimit: number;
-  private _VolumeCounter: number;
-  private _ObjectStorage: number;
-  private _SpecialHardware: number;
-  private _Description: string;
-  private _Comment: string;
-  private _DateSubmitted: string;
-  private _DateStatusChanged: string;
-  private _User: number;
-  private _UserEmail: number;
-  private _UserAffiliations:string[];
-  private _Status: number;
-  private _ComputeCenter: ComputecenterComponent;
-  private _OpenStackProject: boolean;
-  private _DaysRunning: number;
-  private _ApplicationExtension:ApplicationExtension;
-  private _PerunId:number;
-  private _TotalCores: number;
-  private _TotalRam: number;
-  private _DateApproved: string;
-  private _CurrentFlavors: { [id: string]: {counter: number, tag: string,ram: number,rootdisk:number,vcpus:number,gpu:number,epheremal_disk:number} };
+    private _Id: number;
+    private _Name: string;
+    private _Shortname: string;
+    private _Institute: string;
+    private _Workgroup: string;
+    private _Lifetime: number;
+    private _VMsRequested: number;
+    private _CoresPerVM: number;
+    private _RamPerVM: number;
+    private _VolumeLimit: number;
+    private _VolumeCounter: number;
+    private _ObjectStorage: number;
+    private _SpecialHardware: number;
+    private _Description: string;
+    private _Comment: string;
+    private _DateSubmitted: string;
+    private _DateStatusChanged: string;
+    private _User: number;
+    private _UserEmail: number;
+    private _UserAffiliations: string[];
+    private _Status: number;
+    private _ComputeCenter: ComputecenterComponent;
+    private _OpenStackProject: boolean;
+    private _DaysRunning: number;
+    private _ApplicationExtension: ApplicationExtension;
+    private _PerunId: number;
+    private _TotalCores: number;
+    private _TotalRam: number;
+    private _DateApproved: string;
 
 
-
-  constructor() {
-    this._CurrentFlavors={};
-  }
-
- public  addFlavorToCurrent(name: string, counter: number, tag: string,ram: number,rootdisk:number,vcpus:number,gpu:number,epheremal_disk:number ):void {
-    this._CurrentFlavors[name]={counter:counter,tag: tag,ram: ram,rootdisk:rootdisk,vcpus:vcpus,gpu:gpu,epheremal_disk:epheremal_disk};
- }
-
-  get CurrentFlavors(): { [id: string]: {counter: number, tag: string,ram: number,rootdisk:number,vcpus:number,gpu:number,epheremal_disk:number} } {
-    return this._CurrentFlavors
-  }
-
-  set CurrentFlavors(value: { [id: string]: {counter: number, tag: string,ram: number,rootdisk:number,vcpus:number,gpu:number,epheremal_disk:number} } ){
-    this._CurrentFlavors  = value;
-  }
-
-  get DateApproved(): string {
-    return this._DateApproved;
-  }
-
-  set DateApproved(value: string) {
-    this._DateApproved = value;
-  }
-
-  get TotalCores():number{
-    return this._TotalCores;
-  }
-
-  set TotalCores(value:number){
-    this._TotalCores=value;
-  }
-
-  get TotalRam():number{
-    return this._TotalRam;
-  }
-
-  set TotalRam(value:number){
-    this._TotalRam=value;
-  }
-  get UserAffiliations():string[]{
-    return this._UserAffiliations
-  }
-
-  set UserAffiliations(value:string[]){
-    this._UserAffiliations=value;
-  }
-  get ApplicationExtension():ApplicationExtension{
-    return this._ApplicationExtension;
-  }
-
-  set ApplicationExtension(value:ApplicationExtension){
-    this._ApplicationExtension=value;
-  }
-  get DaysRunning():number{
-    return this._DaysRunning;
-  }
-
-  set DaysRunning(value:number){
-    this._DaysRunning=value;
-  }
-  get OpenStackProject(): boolean {
-    return this._OpenStackProject
-  }
-
-  set OpenStackProject(value: boolean) {
-    this._OpenStackProject = value;
-  }
+    private _Dissemination: boolean;
+    private _CurrentFlavors: { [id: string]: { counter: number, tag: string, ram: number, rootdisk: number, vcpus: number, gpu: number, epheremal_disk: number } };
 
 
+    constructor() {
+        this._CurrentFlavors = {};
+    }
+
+    public addFlavorToCurrent(name: string, counter: number, tag: string, ram: number, rootdisk: number, vcpus: number, gpu: number, epheremal_disk: number): void {
+        this._CurrentFlavors[name] = {
+            counter: counter,
+            tag: tag,
+            ram: ram,
+            rootdisk: rootdisk,
+            vcpus: vcpus,
+            gpu: gpu,
+            epheremal_disk: epheremal_disk
+        };
+    }
+     get Dissemination(): boolean {
+        return this._Dissemination;
+    }
+
+    set Dissemination(value: boolean) {
+        this._Dissemination = value;
+    }
+
+    get CurrentFlavors(): { [id: string]: { counter: number, tag: string, ram: number, rootdisk: number, vcpus: number, gpu: number, epheremal_disk: number } } {
+        return this._CurrentFlavors
+    }
+
+    set CurrentFlavors(value: { [id: string]: { counter: number, tag: string, ram: number, rootdisk: number, vcpus: number, gpu: number, epheremal_disk: number } }) {
+        this._CurrentFlavors = value;
+    }
+
+    get DateApproved(): string {
+        return this._DateApproved;
+    }
+
+    set DateApproved(value: string) {
+        this._DateApproved = value;
+    }
+
+    get TotalCores(): number {
+        return this._TotalCores;
+    }
+
+    set TotalCores(value: number) {
+        this._TotalCores = value;
+    }
+
+    get TotalRam(): number {
+        return this._TotalRam;
+    }
+
+    set TotalRam(value: number) {
+        this._TotalRam = value;
+    }
+
+    get UserAffiliations(): string[] {
+        return this._UserAffiliations
+    }
+
+    set UserAffiliations(value: string[]) {
+        this._UserAffiliations = value;
+    }
+
+    get ApplicationExtension(): ApplicationExtension {
+        return this._ApplicationExtension;
+    }
+
+    set ApplicationExtension(value: ApplicationExtension) {
+        this._ApplicationExtension = value;
+    }
+
+    get DaysRunning(): number {
+        return this._DaysRunning;
+    }
+
+    set DaysRunning(value: number) {
+        this._DaysRunning = value;
+    }
+
+    get OpenStackProject(): boolean {
+        return this._OpenStackProject
+    }
+
+    set OpenStackProject(value: boolean) {
+        this._OpenStackProject = value;
+    }
 
 
-  get ComputeCenter(): ComputecenterComponent {
-    return this._ComputeCenter
-  }
+    get ComputeCenter(): ComputecenterComponent {
+        return this._ComputeCenter
+    }
 
-  set ComputeCenter(value: ComputecenterComponent) {
-    this._ComputeCenter = value;
-  }
+    set ComputeCenter(value: ComputecenterComponent) {
+        this._ComputeCenter = value;
+    }
 
-  get Id(): number {
-    return this._Id;
-  }
+    get Id(): number {
+        return this._Id;
+    }
 
-  set Id(value: number) {
-    this._Id = value;
-  }
+    set Id(value: number) {
+        this._Id = value;
+    }
 
-  get Name(): string {
-    return this._Name;
-  }
+    get Name(): string {
+        return this._Name;
+    }
 
-  set Name(value: string) {
-    this._Name = value;
-  }
+    set Name(value: string) {
+        this._Name = value;
+    }
 
-  set Comment(value: string){
-    this._Comment = value;
-  }
+    set Comment(value: string) {
+        this._Comment = value;
+    }
 
-  get Comment():string {
-    return this._Comment;
-  }
-  get Shortname(): string {
-    return this._Shortname;
-  }
+    get Comment(): string {
+        return this._Comment;
+    }
 
-  set Shortname(value: string) {
-    this._Shortname = value;
-  }
+    get Shortname(): string {
+        return this._Shortname;
+    }
 
-  get Institute(): string {
-    return this._Institute;
-  }
+    set Shortname(value: string) {
+        this._Shortname = value;
+    }
 
-  set Institute(value: string) {
-    this._Institute = value;
-  }
+    get Institute(): string {
+        return this._Institute;
+    }
 
-  get Workgroup(): string {
-    return this._Workgroup;
-  }
+    set Institute(value: string) {
+        this._Institute = value;
+    }
 
-  set Workgroup(value: string) {
-    this._Workgroup = value;
-  }
+    get Workgroup(): string {
+        return this._Workgroup;
+    }
 
-  get Lifetime(): number {
-    return this._Lifetime;
-  }
+    set Workgroup(value: string) {
+        this._Workgroup = value;
+    }
 
-  set Lifetime(value: number) {
-    this._Lifetime = value;
-  }
+    get Lifetime(): number {
+        return this._Lifetime;
+    }
 
-  get VMsRequested(): number {
-    return this._VMsRequested;
-  }
+    set Lifetime(value: number) {
+        this._Lifetime = value;
+    }
 
-  set VMsRequested(value: number) {
-    this._VMsRequested = value;
-  }
+    get VMsRequested(): number {
+        return this._VMsRequested;
+    }
 
-  get CoresPerVM(): number {
-    return this._CoresPerVM;
-  }
+    set VMsRequested(value: number) {
+        this._VMsRequested = value;
+    }
 
-  set CoresPerVM(value: number) {
-    this._CoresPerVM = value;
-  }
+    get CoresPerVM(): number {
+        return this._CoresPerVM;
+    }
 
-  get RamPerVM(): number {
-    return this._RamPerVM;
-  }
+    set CoresPerVM(value: number) {
+        this._CoresPerVM = value;
+    }
 
-  set RamPerVM(value: number) {
-    this._RamPerVM = value;
-  }
+    get RamPerVM(): number {
+        return this._RamPerVM;
+    }
 
-  get VolumeLimit(): number {
-    return this._VolumeLimit;
-  }
+    set RamPerVM(value: number) {
+        this._RamPerVM = value;
+    }
 
-  set VolumeLimit(value: number) {
-    this._VolumeLimit = value;
-  }
+    get VolumeLimit(): number {
+        return this._VolumeLimit;
+    }
+
+    set VolumeLimit(value: number) {
+        this._VolumeLimit = value;
+    }
 
 
     get VolumeCounter(): number {
-    return this._VolumeCounter;
-  }
+        return this._VolumeCounter;
+    }
 
-  set VolumeCounter(value: number) {
-    this._VolumeCounter = value;
-  }
+    set VolumeCounter(value: number) {
+        this._VolumeCounter = value;
+    }
 
-  get ObjectStorage(): number {
-    return this._ObjectStorage;
-  }
+    get ObjectStorage(): number {
+        return this._ObjectStorage;
+    }
 
-  set ObjectStorage(value: number) {
-    this._ObjectStorage = value;
-  }
+    set ObjectStorage(value: number) {
+        this._ObjectStorage = value;
+    }
 
-  get SpecialHardware(): number {
-    return this._SpecialHardware;
-  }
+    get SpecialHardware(): number {
+        return this._SpecialHardware;
+    }
 
-  set SpecialHardware(value: number) {
-    this._SpecialHardware = value;
-  }
+    set SpecialHardware(value: number) {
+        this._SpecialHardware = value;
+    }
 
-  get Description(): string {
-    return this._Description;
-  }
+    get Description(): string {
+        return this._Description;
+    }
 
-  set Description(value: string) {
-    this._Description = value;
-  }
+    set Description(value: string) {
+        this._Description = value;
+    }
 
-  get DateSubmitted(): string {
-    return this._DateSubmitted;
-  }
+    get DateSubmitted(): string {
+        return this._DateSubmitted;
+    }
 
-  set DateSubmitted(value: string) {
-    this._DateSubmitted = value;
-  }
+    set DateSubmitted(value: string) {
+        this._DateSubmitted = value;
+    }
 
-  get DateStatusChanged(): string {
-    return this._DateStatusChanged;
-  }
+    get DateStatusChanged(): string {
+        return this._DateStatusChanged;
+    }
 
-  set DateStatusChanged(value: string) {
-    this._DateStatusChanged = value;
-  }
+    set DateStatusChanged(value: string) {
+        this._DateStatusChanged = value;
+    }
 
-  get User(): number {
-    return this._User;
-  }
+    get User(): number {
+        return this._User;
+    }
 
-  set User(value: number) {
-    this._User = value;
-  }
+    set User(value: number) {
+        this._User = value;
+    }
 
-  get Status(): number {
-    return this._Status;
-  }
+    get Status(): number {
+        return this._Status;
+    }
 
-  set Status(value: number) {
-    this._Status = value;
-  }
+    set Status(value: number) {
+        this._Status = value;
+    }
 
 
-  get UserEmail(): number {
-    return this._UserEmail;
-  }
+    get UserEmail(): number {
+        return this._UserEmail;
+    }
 
-  set UserEmail(value: number) {
-    this._UserEmail = value;
-  }
-    get PerunId():number{
-     return this._PerunId;
-  }
+    set UserEmail(value: number) {
+        this._UserEmail = value;
+    }
 
-  set PerunId(value:number){
-     this._PerunId=value;
-  }
+    get PerunId(): number {
+        return this._PerunId;
+    }
+
+    set PerunId(value: number) {
+        this._PerunId = value;
+    }
 }
