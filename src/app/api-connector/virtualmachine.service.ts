08c8202a5e889f1f259a2d5a20ed1ac5da086e88
David Weinholz <david-weinholz@web.de>
2019-03-08 15:19:40 +0100

    startet fixing services

--- src/app/api-connector/virtualmachine.service.ts
+++ src/app/api-connector/virtualmachine.service.ts
@@ -1,29 +1,30 @@
 import ***REMOVED***Injectable***REMOVED*** from '@angular/core';
 import ***REMOVED***ApiSettings***REMOVED*** from './api-settings.service'
-import ***REMOVED***Observable, throwError***REMOVED*** from 'rxjs';
-import ***REMOVED***catchError***REMOVED*** from 'rxjs/operators';
+import ***REMOVED***Observable***REMOVED*** from 'rxjs';
 import ***REMOVED***HttpClient, HttpHeaders, HttpParams***REMOVED*** from '@angular/common/http';
 import ***REMOVED***Cookie***REMOVED*** from 'ng2-cookies/ng2-cookies';
 import ***REMOVED***VirtualMachine***REMOVED*** from '../virtualmachines/virtualmachinemodels/virtualmachine';
 import ***REMOVED***Volume***REMOVED*** from '../virtualmachines/virtualmachinemodels/volume';
 
-const header = new HttpHeaders(***REMOVED***
+const header: HttpHeaders = new HttpHeaders(***REMOVED***
     'X-CSRFToken': Cookie.get('csrftoken')
 ***REMOVED***);
 
+/**
+ * Service which provides vm methods.
+ */
 @Injectable()
 export class VirtualmachineService ***REMOVED***
     data: string;
-    baseVmUrl = `$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***vms/';
+    baseVmUrl: string = `$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***vms/`;
 
     constructor(private http: HttpClient) ***REMOVED***
     ***REMOVED***
 
-
     startVM(flavor: string, image: string, servername: string, project: string, projectid: string,
             volumename?: string, diskspace?: string): Observable<any> ***REMOVED***
 
-        const params = new HttpParams()
+        const params: HttpParams = new HttpParams()
             .set('flavor', flavor)
             .set('image', image)
             .set('servername', servername)
@@ -34,154 +35,141 @@ export class VirtualmachineService ***REMOVED***
 
         return this.http.post(this.baseVmUrl, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
     ***REMOVED***
 
     getAllVM(): Observable<VirtualMachine[]> ***REMOVED***
 
-        return this.http.get<VirtualMachine[]>(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***voManager/vms/', ***REMOVED***
-            withCredentials: true,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+        return this.http.get<VirtualMachine[]>(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***voManager/vms/`, ***REMOVED***
+            withCredentials: true
+        ***REMOVED***)
     ***REMOVED***
-
 
     getVmsFromLoggedInUser(): Observable<VirtualMachine[]> ***REMOVED***
 
         return this.http.get<VirtualMachine[]>(this.baseVmUrl, ***REMOVED***
-            withCredentials: true,
+            withCredentials: true
 
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+        ***REMOVED***)
     ***REMOVED***
 
     getActiveVmsByProject(groupid: string): Observable<VirtualMachine[]> ***REMOVED***
 
-        return this.http.get<VirtualMachine[]>(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***projects/' + groupid + '/vms/', ***REMOVED***
-            withCredentials: true,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+        return this.http.get<VirtualMachine[]>(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***projects/$***REMOVED***groupid***REMOVED***/vms/`, ***REMOVED***
+            withCredentials: true
+        ***REMOVED***)
     ***REMOVED***
-
 
     checkStatusInactiveVms(): Observable<VirtualMachine[]> ***REMOVED***
 
-        return this.http.get<VirtualMachine[]>(this.baseVmUrl + 'status/', ***REMOVED***
-            withCredentials: true,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+        return this.http.get<VirtualMachine[]>(`$***REMOVED***this.baseVmUrl***REMOVED***status/`, ***REMOVED***
+            withCredentials: true
+        ***REMOVED***)
     ***REMOVED***
 
-
     checkVmStatus(openstack_id: string): Observable<any> ***REMOVED***
-        return this.http.post(this.baseVmUrl + openstack_id + '/status/', null, ***REMOVED***
+        return this.http.post(`$***REMOVED***this.baseVmUrl***REMOVED***$***REMOVED***openstack_id***REMOVED***/status/`, null, ***REMOVED***
             withCredentials: true,
 
             headers: header
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
-
+        ***REMOVED***)
     ***REMOVED***
 
     deleteVM(openstack_id: string): Observable<any> ***REMOVED***
 
-        return this.http.delete(this.baseVmUrl + openstack_id + '/', ***REMOVED***
+        return this.http.delete(`$***REMOVED***this.baseVmUrl***REMOVED***$***REMOVED***openstack_id***REMOVED***/`, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
 
     ***REMOVED***
 
     stopVM(openstack_id: string): Observable<any> ***REMOVED***
-        const params = new HttpParams().set('os_action', 'stop');
+        const params: HttpParams = new HttpParams().set('os_action', 'stop');
 
-
-        return this.http.post(this.baseVmUrl + openstack_id + '/action/', params, ***REMOVED***
+        return this.http.post(`$***REMOVED***this.baseVmUrl***REMOVED***$***REMOVED***openstack_id***REMOVED***/action/`, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
     ***REMOVED***
 
-
     rebootVM(openstack_id: string, reboot_type: string): Observable<any> ***REMOVED***
-        const params = new HttpParams().set('os_action', 'reboot').set('reboot_type', reboot_type);
+        const params: HttpParams = new HttpParams().set('os_action', 'reboot').set('reboot_type', reboot_type);
 
-
-        return this.http.post(this.baseVmUrl + openstack_id + '/action/', params, ***REMOVED***
+        return this.http.post(`$***REMOVED***this.baseVmUrl***REMOVED***$***REMOVED***openstack_id***REMOVED***/action/`, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
     ***REMOVED***
 
     resumeVM(openstack_id: string): Observable<any> ***REMOVED***
 
-        const params = new HttpParams().set('os_action', 'resume');
+        const params: HttpParams = new HttpParams().set('os_action', 'resume');
 
-
-        return this.http.post(this.baseVmUrl + openstack_id + '/action/', params, ***REMOVED***
+        return this.http.post(`$***REMOVED***this.baseVmUrl***REMOVED***$***REMOVED***openstack_id***REMOVED***/action/`, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
 
     ***REMOVED***
-
 
     getVolumesByUser(): Observable<Volume[]> ***REMOVED***
-        return this.http.get<Volume[]>(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/', ***REMOVED***
-            withCredentials: true,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+        return this.http.get<Volume[]>(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/`, ***REMOVED***
+            withCredentials: true
+        ***REMOVED***)
 
     ***REMOVED***
 
-
-    createVolume(volume_name: string, volume_diskspace: string, vm_openstackid): Observable<any> ***REMOVED***
-        const params = new HttpParams().set('volume_name', volume_name)
+    createVolume(volume_name: string, volume_diskspace: string, vm_openstackid: string): Observable<any> ***REMOVED***
+        const params: HttpParams = new HttpParams().set('volume_name', volume_name)
             .set('volume_diskspace', volume_diskspace)
             .set('vm_openstackid', vm_openstackid);
 
-        return this.http.post(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/', params, ***REMOVED***
+        return this.http.post(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/`, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
     ***REMOVED***
 
     attachVolumetoServer(volume_id: string, instance_id: string): Observable<any> ***REMOVED***
 
-        const params = new HttpParams().set('instance_id', instance_id).set('os_action', 'attach');
+        const params: HttpParams = new HttpParams().set('instance_id', instance_id).set('os_action', 'attach');
 
+        return this.http.post(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/$***REMOVED***volume_id***REMOVED***/action/`, params, ***REMOVED***
+                withCredentials: true,
+                headers: header
+            ***REMOVED***
+        )
 
-        return this.http.post(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/' + volume_id + '/action/', params, ***REMOVED***
-            withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
     ***REMOVED***
-
 
     renameVolume(volume_id: string, new_volume_name: string): Observable<any> ***REMOVED***
-        const params = new HttpParams().set('new_volume_name', new_volume_name);
+        const params: HttpParams = new HttpParams().set('new_volume_name', new_volume_name);
 
-        return this.http.patch(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/' + volume_id + '/', params, ***REMOVED***
+        return this.http.patch(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/$***REMOVED***volume_id***REMOVED***/`, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
 
     ***REMOVED***
-
 
     deleteVolume(volume_id: string): Observable<any> ***REMOVED***
 
-        return this.http.delete(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/' + volume_id + '/', ***REMOVED***
+        return this.http.delete(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/$***REMOVED***volume_id***REMOVED***/`, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
     ***REMOVED***
-
 
     deleteVolumeAttachment(volume_id: string, instance_id: string): Observable<any> ***REMOVED***
 
-        const params = new HttpParams().set('instance_id', instance_id).set('os_action', 'detach');
+        const params: HttpParams = new HttpParams().set('instance_id', instance_id).set('os_action', 'detach');
 
-        return this.http.post(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/' + volume_id + '/action/', params, ***REMOVED***
+        return this.http.post(`$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***volumes/$***REMOVED***volume_id***REMOVED***/action/`, params, ***REMOVED***
             withCredentials: true,
-            headers: header,
-        ***REMOVED***).pipe(catchError((error: any) => throwError(error)));
+            headers: header
+        ***REMOVED***)
     ***REMOVED***
-
 
 ***REMOVED***
