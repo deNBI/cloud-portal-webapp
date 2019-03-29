fdda8cc9b776db1530de383c8d9affcee4b66ebc
David Weinholz <david-weinholz@web.de>
2019-03-29 10:17:30 +0100

    refactor(SpecialHardware):more removed

--- src/app/vo_manager/resources/resources.ts
+++ src/app/vo_manager/resources/resources.ts
@@ -10,11 +10,10 @@ export class Resources ***REMOVED***
     private _totalVolumeLimit: number = 0;
     private _totalVolumeCounter: number = 0;
     private _totalObjectStorage: number = 0;
-    private _totalFPGA: number = 0;
     private _totalGPU: number = 0;
 
     constructor(resource_name: string, totalRam: number, totalCores: number, totalVms: number, totalVolumeLimit: number,
-                totalVolumeCounter: number, totalObjectStorage: number, totalFPGA: number, totalGPU: number) ***REMOVED***
+                totalVolumeCounter: number, totalObjectStorage: number,  totalGPU: number) ***REMOVED***
         this._resource_name = resource_name;
         this._totalRam = totalRam;
         this._totalCores = totalCores;
@@ -22,7 +21,6 @@ export class Resources ***REMOVED***
         this._totalVolumeLimit = totalVolumeLimit;
         this._totalVolumeCounter = totalVolumeCounter;
         this._totalObjectStorage = totalObjectStorage;
-        this._totalFPGA = totalFPGA;
         this._totalGPU = totalGPU;
     ***REMOVED***
 
@@ -80,14 +78,6 @@ export class Resources ***REMOVED***
 
     set totalObjectStorage(value: number) ***REMOVED***
         this._totalObjectStorage = value;
-    ***REMOVED***
-
-    get totalFPGA(): number ***REMOVED***
-        return this._totalFPGA;
-    ***REMOVED***
-
-    set totalFPGA(value: number) ***REMOVED***
-        this._totalFPGA = value;
     ***REMOVED***
 
     get totalGPU(): number ***REMOVED***
