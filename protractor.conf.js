cb2a4f5f3a8f313929b78bd2e068fcda27735f4a
David Weinholz <david-weinholz@web.de>
2019-04-13 15:25:15 +0200

    feat(Protractor):updated test

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,12 +1,19 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const ***REMOVED*** SpecReporter ***REMOVED*** = require('jasmine-spec-reporter');
+const ***REMOVED***SpecReporter***REMOVED*** = require('jasmine-spec-reporter');
 
 exports.config = ***REMOVED***
+  params: ***REMOVED***
+    timeout: 10000,
+    login: ***REMOVED***
+      email: 'default',
+      password: 'default'
+    ***REMOVED***
+  ***REMOVED***,
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    'spec.ts'
   ],
   capabilities: ***REMOVED***
     'browserName': 'chrome'
@@ -17,14 +24,15 @@ exports.config = ***REMOVED***
   jasmineNodeOpts: ***REMOVED***
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() ***REMOVED******REMOVED***
+    print: function () ***REMOVED***
+    ***REMOVED***
   ***REMOVED***,
-  beforeLaunch: function() ***REMOVED***
+  beforeLaunch: function () ***REMOVED***
     require('ts-node').register(***REMOVED***
       project: 'e2e/tsconfig.e2e.json'
     ***REMOVED***);
   ***REMOVED***,
   onPrepare() ***REMOVED***
-    jasmine.getEnv().addReporter(new SpecReporter(***REMOVED*** spec: ***REMOVED*** displayStacktrace: true ***REMOVED*** ***REMOVED***));
+    jasmine.getEnv().addReporter(new SpecReporter(***REMOVED***spec: ***REMOVED***displayStacktrace: true***REMOVED******REMOVED***));
   ***REMOVED***
 ***REMOVED***;
---
+++ spec.ts
@@ -0,0 +1,102 @@
+// spec.js
+import ***REMOVED***browser, by, element, protractor***REMOVED*** from 'protractor';
+
+describe('Application test', function () ***REMOVED***
+  const timeout: number = browser.params.timeout;
+  const until = protractor.ExpectedConditions;
+
+
+  beforeEach(function () ***REMOVED***
+    browser.waitForAngularEnabled(false);
+    browser.get('https://portal-dev.denbi.de/portal/');
+  ***REMOVED***);
+
+  it('should be login page', function () ***REMOVED***
+
+    browser.wait(until.urlContains('login.elixir-czech'), timeout).then(function (result) ***REMOVED***
+      expect(result).toEqual(true);
+    ***REMOVED***);
+  ***REMOVED***);
+
+  it('should login', function () ***REMOVED***
+    var el = element(by.className('metalist list-group'));
+    el.click();
+    // Input Email
+    browser.wait(until.urlContains('accounts.google.com/signin/oauth/'), timeout).then(function () ***REMOVED***
+      element(by.id('identifierId')).sendKeys(browser.params.login.email);
+      // Click next btn
+      element(by.id('identifierNext')).click();
+    ***REMOVED***);
+
+    browser.wait(until.urlContains('accounts.google.com/signin/v2/challenge'), timeout).then(function () ***REMOVED***
+      const password = element(by.name('password'));
+      browser.wait(until.elementToBeClickable(password), timeout).then(function () ***REMOVED***
+        element(by.name('password')).sendKeys(browser.params.login.password);
+        element(by.id('passwordNext')).click();
+      ***REMOVED***)
+
+    ***REMOVED***);
+
+    const authorize = element(by.name('authorize'));
+    browser.wait(until.elementToBeClickable(authorize), timeout).then(function () ***REMOVED***
+      authorize.click();
+    ***REMOVED***)
+
+    browser.wait(until.urlContains('userinfo'), timeout).then(function (result) ***REMOVED***
+      expect(result).toEqual(true);
+    ***REMOVED***);
+
+
+    // Input password
+  ***REMOVED***);
+
+
+  it('should send a cloud application', function () ***REMOVED***
+    browser.get('https://portal-dev.denbi.de/portal/webapp/#/applications/newCloudApplication');
+    browser.wait(until.urlContains('applications/newCloudApplication'), timeout);
+
+    // fill  Formular
+    element(by.name('project_application_name')).sendKeys('ProtractorTest');
+    element(by.name('project_application_shortname')).sendKeys('ProtractorTest');
+    element(by.name('project_application_description')).sendKeys('ProtractorTest Description');
+    element(by.name('project_application_lifetime')).sendKeys('4');
+    element(by.name('project_application_institute')).sendKeys('Proctractor Institute');
+    element(by.name('project_application_workgroup')).sendKeys('Proctractor Workgroup');
+    // submit
+    element(by.className('btn btn-sm btn-success')).click();
+    // Todo give verification Btn ID in Angular
+    const verificationBtn = element(by.className('btn btn-success col-md4'));
+    browser.wait(until.elementToBeClickable(verificationBtn), timeout).then(function () ***REMOVED***
+      element(by.className('btn btn-success col-md4')).click()
+    ***REMOVED***);
+    // Todo give acknowledege btn id
+    const acknowledgeBtn = element(by.name('_save'));
+    browser.wait(until.elementToBeClickable(acknowledgeBtn), timeout).then(function () ***REMOVED***
+      element(by.name('_save')).click()
+
+    ***REMOVED***);
+
+    const redirectBtn = element(by.className('btn btn-secondary'));
+    browser.wait(until.elementToBeClickable(redirectBtn), timeout).then(function () ***REMOVED***
+      element(by.className('btn btn-secondary')).click()
+
+    ***REMOVED***)
+    browser.wait(until.urlContains('applications')).then(function () ***REMOVED***
+      browser.waitForAngularEnabled(true);
+
+      var isProtractorApp = false;
+      // todo give user_application table id
+      element.all(by.tagName('td')).each(function (element, index) ***REMOVED***
+        element.getText().then(function (text) ***REMOVED***
+          if (text === 'ProtractorTest')
+            isProtractorApp = true;
+          return false;
+        ***REMOVED***)
+      ***REMOVED***).then(function () ***REMOVED***
+        expect(isProtractorApp).toEqual(true);
+      ***REMOVED***)
+
+    ***REMOVED***);
+
+  ***REMOVED***);
+***REMOVED***);
