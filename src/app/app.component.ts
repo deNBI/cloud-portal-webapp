110e5b5a25b683aa4ade06212af9a307cc60226b
Ewgenij Katchko <ekatchko@techfak.uni-bielefeld.de>
2019-04-19 16:54:17 +0200

    feature(matomo): can track user navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ViewChild, OnInit} from '@angular/core';
 import {Http, RequestOptions, XHRBackend} from '@angular/http';
 import {ModalDirective} from 'ngx-bootstrap';
+import {Angulartics2Piwik} from 'angulartics2/piwik';
 
 /**
  * App component.
@@ -13,9 +14,17 @@ import {ModalDirective} from 'ngx-bootstrap';
         deps: [XHRBackend, RequestOptions, AppComponent]
     }]
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
 
     @ViewChild('timeoutModal') modal: ModalDirective;
+
+    constructor(private angulartics2Piwik: Angulartics2Piwik) {
+
+    }
+
+    ngOnInit(): void {
+      this.angulartics2Piwik.startTracking();
+    }
 
     ngAfterViewInit(): void {
     }
--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,20 @@ if (environment.production) {
     enableProdMode();
 }
 
+// Constructing tracking code
+const matomoScript = document.createElement('script');
+matomoScript.type = 'text/javascript';
+matomoScript.innerHTML =
+  'var _paq = _paq || [];\n' +
+  "  _paq.push(['enableLinkTracking']);\n" +
+  '  (function() {\n' +
+  `    let u="${environment.matomoServer}";\n` +
+  "    _paq.push(['setTrackerUrl', u+'matomo.php']);\n" +
+  "    _paq.push(['setSiteId', '2']);\n" +
+  "    let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];\n" +
+  "    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);\n" +
+  '  })();';
+
+document.getElementsByTagName('head')[0].appendChild(matomoScript);
+
 platformBrowserDynamic().bootstrapModule(AppModule);
