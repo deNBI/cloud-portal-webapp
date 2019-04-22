5203dbc0ded252fa60b95622446953db2040dd28
David Weinholz <david-weinholz@web.de>
2019-04-22 12:20:47 +0200

    feat(Applicaitons):added pi field

--- src/app/applications/addcloudapplication.component.ts
+++ src/app/applications/addcloudapplication.component.ts
@@ -12,186 +12,189 @@ import {ApplicationBaseClass} from 'app/shared/shared_modules/baseClass/applicat
  * This components provides the functions to create a new Cloud Application.
  */
 @Component({
-    selector: 'app-addcloudapplication',
-    templateUrl: 'addcloudapplication.component.html',
-    providers: [ApiSettings, ApplicationsService, FlavorService],
-    styleUrls: ['addcloudapplication.component.css']
+  selector: 'app-addcloudapplication',
+  templateUrl: 'addcloudapplication.component.html',
+  providers: [ApiSettings, ApplicationsService, FlavorService],
+  styleUrls: ['addcloudapplication.component.css']
 })
 
 export class AddcloudapplicationComponent extends ApplicationBaseClass {
 
-    /**
-     * If it is in production or dev mode.
-     * @type {boolean}
-     */
-    public production: boolean = environment.production;
+  /**
+   * If it is in production or dev mode.
+   * @type {boolean}
+   */
+  public production: boolean = environment.production;
 
-    /**
-     * List of all collapse booleans.
-     */
-    public collapseList: boolean[];
+  /**
+   * List of all collapse booleans.
+   */
+  public collapseList: boolean[];
 
-    /**
-     * Contains errors recieved when submitting an application.
-     */
-    public error: string[];
-    /**
-     * Default vms requested in form.
-     * @type {number}
-     */
-    public project_application_vms_requested: number = 5;
-    /**
-     * List of flavors.
-     */
-    public flavorList: Flavor[];
-    /**
-     * List of flavor types.
-     */
-    public typeList: FlavorType[];
+  /**
+   * Contains errors recieved when submitting an application.
+   */
+  public error: string[];
+  /**
+   * Default vms requested in form.
+   * @type {number}
+   */
+  public project_application_vms_requested: number = 5;
+  /**
+   * List of flavors.
+   */
+  public flavorList: Flavor[];
+  /**
+   * List of flavor types.
+   */
+  public typeList: FlavorType[];
+
+  public project_application_pi_approve: boolean = false;
 
 
-    public acknowledgeModalTitle: string = 'Acknowledge';
-    public acknowledgeModalType: string = 'info';
+  public acknowledgeModalTitle: string = 'Acknowledge';
+  public acknowledgeModalType: string = 'info';
 
-    /**
-     * If project is openstack project (everytime true)
-     * @type {boolean} if it is a openstack project
-     */
-    project_application_openstack_project: boolean = true;
+  /**
+   * If project is openstack project (everytime true)
+   * @type {boolean} if it is a openstack project
+   */
+  project_application_openstack_project: boolean = true;
 
-    /**
-     * Constructor.
-     * Initialize special hardware and gets list of flavor and flavortypes.
-     * @param {ApplicationsService} applicationsservice
-     * @param {FlavorService} flavorservice
-     */
-    constructor(applicationsservice: ApplicationsService, private flavorservice: FlavorService) {
-        super(null, null, applicationsservice, null);
-        this.getListOfFlavors();
-        this.getListOfTypes();
+  /**
+   * Constructor.
+   * Initialize special hardware and gets list of flavor and flavortypes.
+   * @param {ApplicationsService} applicationsservice
+   * @param {FlavorService} flavorservice
+   */
+  constructor(applicationsservice: ApplicationsService, private flavorservice: FlavorService) {
+    super(null, null, applicationsservice, null);
+    this.getListOfFlavors();
+    this.getListOfTypes();
 
+  }
+
+  /**
+   * Gets a list of all available types of flavors from the flavorservice and uses them in the function setListOfTypes
+   */
+  getListOfTypes(): void {
+    this.flavorservice.getListOfTypesAvailable().subscribe((types: FlavorType[]) => this.setListOfTypes(types));
+  }
+
+  /**
+   * Uses the param types to safe the available FlavorTypes to the array typeList.
+   * Also it fills the array collapseList with booleans of value 'false' so all flavor-categories are shown in the application form.
+   * @param types array of all available FlavorTypes
+   */
+  setListOfTypes(types: FlavorType[]): void {
+    this.typeList = types;
+    this.collapseList = new Array(types.length) as boolean[];
+    for (const type of types) {
+
+      this.collapseList.push(false); // AS FIX
+      if (type.long_name === 'Standart Flavor') {
+        this.collapseList[this.typeList.indexOf(type)] = true;
+      }
     }
 
-    /**
-     * Gets a list of all available types of flavors from the flavorservice and uses them in the function setListOfTypes
-     */
-    getListOfTypes(): void {
-        this.flavorservice.getListOfTypesAvailable().subscribe((types: FlavorType[]) => this.setListOfTypes(types));
-    }
+  }
 
-    /**
-     * Uses the param types to safe the available FlavorTypes to the array typeList.
-     * Also it fills the array collapseList with booleans of value 'false' so all flavor-categories are shown in the application form.
-     * @param types array of all available FlavorTypes
-     */
-    setListOfTypes(types: FlavorType[]): void {
-        this.typeList = types;
-        this.collapseList = new Array(types.length) as boolean[];
-        for (const type of types) {
+  /**
+   * gets a list of all available Flavors from the flavorservice and puts them into the array flavorList
+   */
+  getListOfFlavors(): void {
+    this.flavorservice.getListOfFlavorsAvailable().subscribe((flavors: Flavor[]) => this.flavorList = flavors);
+  }
 
-            this.collapseList.push(false); // AS FIX
-            if (type.long_name === 'Standart Flavor') {
-                this.collapseList[this.typeList.indexOf(type)] = true;
-            }
+  /**
+   * Submits a new cloud application.
+   * Therefore checks if the different values are valid.
+   * @param {NgForm} form
+   */
+  onSubmit(form: NgForm): void {
+    this.error = null;
+    if (this.wronginput) {
+
+      this.updateNotificationModal(
+        'Failed',
+        'The application was not submitted, please check the required fields and try again.',
+        true,
+        'danger');
+      this.notificationModalStay = true;
+    } else {
+      const values: { [key: string]: string | number | boolean } = {};
+      values['project_application_openstack_project'] = this.project_application_openstack_project;
+      values['project_application_pi_approved'] = this.project_application_pi_approve;
+      for (const value in form.controls) {
+        if (form.controls[value].disabled) {
+          continue;
+        }
+        if (form.controls[value].value) {
+
+          values[value] = form.controls[value].value;
+        }
+      }
+      this.applicationsservice.addNewApplication(values).toPromise()
+        .then(() => {
+          this.updateNotificationModal('Success', 'The application was submitted', true, 'success');
+          this.notificationModalStay = false;
+        }).catch((error: string) => {
+        const error_json: string = error;
+        this.error = [];
+        for (const key of Object.keys(error_json)) {
+          this.error.push(key.split('_')[2])
+
         }
 
+        this.updateNotificationModal(
+          'Failed',
+          'The application was not submitted, please check the required fields and try again.',
+          true,
+          'danger');
+        this.notificationModalStay = true;
+      })
     }
+  }
 
-    /**
-     * gets a list of all available Flavors from the flavorservice and puts them into the array flavorList
-     */
-    getListOfFlavors(): void {
-        this.flavorservice.getListOfFlavorsAvailable().subscribe((flavors: Flavor[]) => this.flavorList = flavors);
+  sendTestApplication(): void {
+    const values: { [key: string]: string | number | boolean } = {};
+
+    values['project_application_comment'] = 'TestApplication';
+    values['project_application_description'] = 'TestApplication';
+    values['project_application_institute'] = 'TestApplication';
+    values['project_application_lifetime'] = 3;
+    values['project_application_name'] = 'TestApplication';
+    values['project_application_openstack_project'] = true;
+    for (const flavor of this.flavorList) {
+      const fname: string = `project_application_${flavor.name}`;
+      values[fname] = 1;
     }
+    values['project_application_report_allowed'] = true;
+    values['project_application_shortname'] = 'TestApplication';
+    values['project_application_volume_counter'] = 5;
+    values['project_application_volume_limit'] = 20;
+    values['project_application_workgroup'] = 'TestApplication';
 
-    /**
-     * Submits a new cloud application.
-     * Therefore checks if the different values are valid.
-     * @param {NgForm} form
-     */
-    onSubmit(form: NgForm): void {
-        this.error = null;
-        if (this.wronginput) {
+    this.applicationsservice.addNewApplication(values).toPromise()
+      .then(() => {
+        this.updateNotificationModal('Success', 'The application was submitted', true, 'success');
+        this.notificationModalStay = false;
+      }).catch((error: string) => {
+      const error_json: string = error;
+      this.error = [];
+      for (const key of Object.keys(error_json)) {
+        this.error.push(key.split('_')[2])
 
-            this.updateNotificationModal(
-                'Failed',
-                'The application was not submitted, please check the required fields and try again.',
-                true,
-                'danger');
-            this.notificationModalStay = true;
-        } else {
-            const values: { [key: string]: string | number | boolean } = {};
-            values['project_application_openstack_project'] = this.project_application_openstack_project;
-            for (const value in form.controls) {
-                if (form.controls[value].disabled) {
-                  continue;
-                }
-                if (form.controls[value].value) {
+      }
 
-                    values[value] = form.controls[value].value;
-                }
-            }
-            this.applicationsservice.addNewApplication(values).toPromise()
-                .then(() => {
-                    this.updateNotificationModal('Success', 'The application was submitted', true, 'success');
-                    this.notificationModalStay = false;
-                }).catch((error: string) => {
-                const error_json: string = error;
-                this.error = [];
-                for (const key of Object.keys(error_json)) {
-                    this.error.push(key.split('_')[2])
+      this.updateNotificationModal(
+        'Failed',
+        'The application was not submitted, please check the required fields and try again.',
+        true,
+        'danger');
+      this.notificationModalStay = true;
+    })
 
-                }
-
-                this.updateNotificationModal(
-                    'Failed',
-                    'The application was not submitted, please check the required fields and try again.',
-                    true,
-                    'danger');
-                this.notificationModalStay = true;
-            })
-        }
-    }
-
-    sendTestApplication(): void {
-        const values: { [key: string]: string | number | boolean } = {};
-
-        values['project_application_comment'] = 'TestApplication';
-        values['project_application_description'] = 'TestApplication';
-        values['project_application_institute'] = 'TestApplication';
-        values['project_application_lifetime'] = 3;
-        values['project_application_name'] = 'TestApplication';
-        values['project_application_openstack_project'] = true;
-        for (const flavor of this.flavorList) {
-            const fname: string = `project_application_${flavor.name}`;
-            values[fname] = 1;
-        }
-        values['project_application_report_allowed'] = true;
-        values['project_application_shortname'] = 'TestApplication';
-        values['project_application_volume_counter'] = 5;
-        values['project_application_volume_limit'] = 20;
-        values['project_application_workgroup'] = 'TestApplication';
-
-        this.applicationsservice.addNewApplication(values).toPromise()
-            .then(() => {
-                this.updateNotificationModal('Success', 'The application was submitted', true, 'success');
-                this.notificationModalStay = false;
-            }).catch((error: string) => {
-            const error_json: string = error;
-            this.error = [];
-            for (const key of Object.keys(error_json)) {
-                this.error.push(key.split('_')[2])
-
-            }
-
-            this.updateNotificationModal(
-                'Failed',
-                'The application was not submitted, please check the required fields and try again.',
-                true,
-                'danger');
-            this.notificationModalStay = true;
-        })
-
-    }
+  }
 
 }
