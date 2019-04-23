602e5579e6aa621d11f477c235f320c6b950a1b9
Ewgenij Katchko <ekatchko@techfak.uni-bielefeld.de>
2019-04-23 15:20:41 +0200

    feat(Validation): closing validation modal redirects to /applications

--- src/app/validation-application/validation-application.component.ts
+++ src/app/validation-application/validation-application.component.ts
@@ -12,31 +12,34 @@ import ***REMOVED***ApplicationBaseClass***REMOVED*** from "../shared/shared_mod
 ***REMOVED***)
 export class ValidationApplicationComponent extends ApplicationBaseClass implements OnInit ***REMOVED***
 
+  application: Application;
+
+  isLoaded: boolean = false;
+  hash: string;
+  validated: boolean = false;
+
   constructor(private applicationsService: ApplicationsService, private activatedRoute: ActivatedRoute) ***REMOVED***
     super(null, null, applicationsService, null)
 
   ***REMOVED***
 
-  application: Application;
-  isLoaded: boolean = false;
-  hash: string;
-  validated: boolean = false;
-
-  approveApplication() ***REMOVED***
+  approveApplication(): any ***REMOVED***
     this.applicationsService.validateApplicationAsPIByHash(this.hash).subscribe(res => ***REMOVED***
       if (res['project_application_pi_approved']) ***REMOVED***
-        this.validated=true;
+        this.validated = true;
         this.updateNotificationModal(
           'Success',
           'The application was successfully validated.',
           true,
           'success');
+        this.notificationModalStay = false;
       ***REMOVED*** else ***REMOVED***
         this.updateNotificationModal(
           'Failed',
           'The application was not successfully validated.',
           true,
           'danger');
+        this.notificationModalStay = true;
       ***REMOVED***
     ***REMOVED***)
   ***REMOVED***
