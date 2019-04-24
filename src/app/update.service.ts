537b575d7ea1b702172caa3abe343286fb805611
David Weinholz <david-weinholz@web.de>
2019-04-24 23:23:14 +0200

    test update

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -5,17 +5,20 @@ import ***REMOVED***SwUpdate***REMOVED*** from '@angular/service-worker';
 @Injectable()
 export class UpdateService ***REMOVED***
 
-    constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) ***REMOVED***
-        this.swUpdate.available.subscribe(evt => ***REMOVED***
-            const snack = this.snackbar.open('Update Available', 'Reload', ***REMOVED***
-                duration: 10000
-            ***REMOVED***);
+  constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) ***REMOVED***
+    console.log('service worker activated');
+    this.swUpdate.checkForUpdate().then(res =>***REMOVED***console.log('update')***REMOVED***)
+    this.swUpdate.available.subscribe(evt => ***REMOVED***
+      console.log('new action');
+      const snack = this.snackbar.open('Update Available', 'Reload', ***REMOVED***
+        duration: 10000
+      ***REMOVED***);
 
-            snack
-                .onAction()
-                .subscribe(() => ***REMOVED***
-                    window.location.reload();
-                ***REMOVED***);
+      snack
+        .onAction()
+        .subscribe(() => ***REMOVED***
+          window.location.reload();
         ***REMOVED***);
-    ***REMOVED***
+    ***REMOVED***);
+  ***REMOVED***
 ***REMOVED***
