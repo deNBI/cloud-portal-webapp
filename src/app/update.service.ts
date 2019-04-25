f9081ce8f8c0de7a940309f4f402b6973dadab85
David Weinholz <david-weinholz@web.de>
2019-04-25 11:10:45 +0200

    more testing

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -1,24 +1,33 @@
-import {Injectable} from '@angular/core';
+import {ApplicationRef, Injectable} from '@angular/core';
 import {MatSnackBar} from '@angular/material';
 import {SwUpdate} from '@angular/service-worker';
+import {concat, interval} from "rxjs";
+import {first} from 'rxjs/operators';
 
 @Injectable()
 export class UpdateService {
 
-  constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
-    console.log('service worker activated');
-    this.swUpdate.checkForUpdate().then(res =>{console.log('update')})
-    this.swUpdate.available.subscribe(evt => {
-      console.log('new action');
-      const snack = this.snackbar.open('Update Available', 'Reload', {
-        duration: 10000
+  constructor(private appRef: ApplicationRef, private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
+    const isStable = appRef.isStable.pipe(first(isStable => isStable === true));
+
+    const intervalTime = interval(60 * 1000);
+    const checkUpdatesInIntervall = concat(isStable, intervalTime);
+    checkUpdatesInIntervall.subscribe(() => this.swUpdate.checkForUpdate().subscribe(() => {
+      this.swUpdate.available.subscribe(evt => {
+        console.log('new action');
+        const snack = this.snackbar.open('Update Available', 'Reload', {
+          duration: 10000
+        });
+
+        snack
+          .onAction()
+          .subscribe(() => {
+            window.location.reload();
+          });
       });
 
-      snack
-        .onAction()
-        .subscribe(() => {
-          window.location.reload();
-        });
-    });
+    }))
+
+
   }
 }
