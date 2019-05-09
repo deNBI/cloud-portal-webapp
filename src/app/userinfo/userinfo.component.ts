ac5450a8cef34bb376f7f64e1d820a5745a07963
Ewgenij Katchko <ekatchko@techfak.uni-bielefeld.de>
2019-05-09 15:01:59 +0200

    fix(jsnlog): remove test button in userinfo

--- src/app/userinfo/userinfo.component.ts
+++ src/app/userinfo/userinfo.component.ts
@@ -151,9 +151,4 @@ export class UserInfoComponent implements OnInit {
   joinFreemium(): void {
     this.groupService.addMemberToFreemium().subscribe();
   }
-
-
-  throwError() {
-    throw "message";
-  }
 }
