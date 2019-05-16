274b9e202478c6be40046a7174062170a94616be
eKatchko <47833533+eKatchko@users.noreply.github.com>
2019-05-16 13:12:40 +0200

    Fix/jsnlog (#477)
    * fix(jsnlog): filter logged messages from batch, reduced batch time from 10 to 3 minutes

--- src/app/error-handler/UncaughtExceptionHandler.service.ts
+++ src/app/error-handler/UncaughtExceptionHandler.service.ts
@@ -1,24 +1,39 @@
-import ***REMOVED*** ErrorHandler ***REMOVED*** from '@angular/core';
+import ***REMOVED***ErrorHandler***REMOVED*** from '@angular/core';
 import ***REMOVED*** JL ***REMOVED*** from 'jsnlog';
 import ***REMOVED*** Cookie ***REMOVED*** from 'ng2-cookies';
 import ***REMOVED*** ApiSettings ***REMOVED*** from '../api-connector/api-settings.service';
 
 // Before Send Function
-const beforeSendFunction: any = function (xhr: XMLHttpRequest, json: JSON): any ***REMOVED***
+function beforeSendFunction(xhr: XMLHttpRequest, json: JSON): any ***REMOVED***
   for (const message of json['lg']) ***REMOVED***
     message['identifier'] = Cookie.get('csrftoken').slice(5, 10);
   ***REMOVED***
+  json = filter(json);
   xhr.setRequestHeader('X-CSRFToken', Cookie.get('csrftoken'));
   xhr.withCredentials = true;
-***REMOVED***;
+***REMOVED***
 
+function filter(json: JSON): JSON ***REMOVED***
+  const ems: string[] = [];
+  for (let count: number = 0; count < json['lg'].length; count++) ***REMOVED***
+    const message: string = json['lg'][count]['m'];
+    if (ems.indexOf(message) === -1) ***REMOVED***
+      ems.push(message);
+    ***REMOVED*** else ***REMOVED***
+      delete json['lg'][count]
+    ***REMOVED***
+  ***REMOVED***
+
+  return json;
+***REMOVED***
 // Ajax Appender and Settings
 const appender: JL.JSNLogAjaxAppender = JL.createAjaxAppender('ajax appender');
+
 appender.setOptions(***REMOVED***
   beforeSend: beforeSendFunction,
   url: `$***REMOVED***ApiSettings.getApiBaseURL()***REMOVED***jsnlog/log/`,
   batchSize: 30,
-  batchTimeout: 600000,
+  batchTimeout: 180000,
   maxBatchSize: 50,
   sendTimeout: 10000
                     ***REMOVED***);
@@ -34,7 +49,8 @@ JL().setOptions(***REMOVED***
 /**
  * ErrorHandler Class implementing JSNLog
  */
-export class UncaughtExceptionHandler implements ErrorHandler ***REMOVED***
+export class UncaughtExceptionHandler implements ErrorHandler***REMOVED***
+
   handleError(error: any): any ***REMOVED***
     JL().fatalException('Uncaught Exception', error);
   ***REMOVED***
