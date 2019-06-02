c31d8276036292897870a657b857da08d4e8b521
David Weinholz <david-weinholz@web.de>
2019-06-02 22:03:58 +0200

    filtering server side, added debounce time

--- src/app/api-connector/bioconda.service.ts
+++ src/app/api-connector/bioconda.service.ts
@@ -1,22 +1,30 @@
 import {Injectable} from '@angular/core';
 import {ApiSettings} from './api-settings.service'
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Cookie} from 'ng2-cookies/ng2-cookies';
 
 const header: HttpHeaders = new HttpHeaders({
                                               'X-CSRFToken': Cookie.get('csrftoken')
                                             });
+
 @Injectable()
 export class BiocondaService {
 
   constructor(private http: HttpClient) {
   }
 
-  getBiocondaTools(): Observable<any> {
+  getBiocondaTools(page: number, name?: string, version?: string, build?: string): Observable<any> {
+    const params: HttpParams = new HttpParams()
+      .set('page', page.toString())
+      .set('build', build)
+      .set('version', version)
+      .set('name', name);
+
     return this.http.get(`${ApiSettings.getApiBaseURL()}conda/bioconda/`, {
       headers: header,
-      withCredentials: true
+      withCredentials: true,
+      params: params
     })
   }
 
--- src/app/virtualmachines/conda/bioconda.component.ts
+++ src/app/virtualmachines/conda/bioconda.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {BiocondaService} from '../../api-connector/bioconda.service';
-import {VirtualMachine} from '../virtualmachinemodels/virtualmachine';
+import {fromEvent, Subject,} from 'rxjs';
+import {debounceTime, map, distinctUntilChanged} from 'rxjs/operators';
+import {ModalDirective, PaginationComponent} from 'ngx-bootstrap';
 
 export interface IBiocondaTool {
   name: string,
@@ -9,104 +11,119 @@ export interface IBiocondaTool {
 }
 
 @Component({
-  selector: 'app-bioconda',
-  templateUrl: 'bioconda.component.html',
-  providers: [BiocondaService],
+             selector: 'app-bioconda',
+             templateUrl: 'bioconda.component.html',
+             providers: [BiocondaService]
            })
-export class BiocondaComponent implements OnInit{
+export class BiocondaComponent implements OnInit {
+  FIRST_PAGE: number = 1;
+  DEBOUNCE_TIME: number = 300;
 
   bioconda_tools: IBiocondaTool[] = [];
 
-  tools_filtered: IBiocondaTool[];
-
-  tools_returned: IBiocondaTool[];
-
   toolsPerPage: number = 10;
 
-  currentPage: number = 1;
+  currentPage: number = this.FIRST_PAGE;
 
   toolsStart: number = 0;
 
   toolsEnd: number = this.toolsPerPage;
 
-  filterToolName: string;
+  filterToolName: string = '';
 
-  filterToolVersion: string;
+  filterToolVersion: string = '';
 
-  filterToolBuild: string;
+  filterToolBuild: string = '';
+  isSearching: boolean;
 
   max_pages: number = 15;
+  total_pages: number;
 
-  constructor(private condaService: BiocondaService) {}
+  filternameChanged: Subject<string> = new Subject<string>();
+  filterVersionChanged: Subject<string> = new Subject<string>();
+  filterBuildChanged: Subject<string> = new Subject<string>();
 
-  ngOnInit(): void {
-    this.getBiocondaTools();
+  @ViewChild('pagination') pagination: PaginationComponent;
+
+  constructor(private condaService: BiocondaService) {
   }
 
-  getBiocondaTools(): void {
-    this.condaService.getBiocondaTools().subscribe(
+  ngOnInit(): void {
+    this.getBiocondaTools(this.FIRST_PAGE);
+
+    this.filternameChanged
+      .pipe(
+        debounceTime(this.DEBOUNCE_TIME),
+        distinctUntilChanged())
+      .subscribe((filterName: string) => {
+        this.filterToolName = filterName;
+        this.getBiocondaTools(this.FIRST_PAGE)
+
+      });
+
+    this.filterVersionChanged
+      .pipe(
+        debounceTime(this.DEBOUNCE_TIME),
+        distinctUntilChanged())
+      .subscribe((filterVersion: string) => {
+        this.filterToolVersion = filterVersion;
+        this.getBiocondaTools(this.FIRST_PAGE)
+
+      });
+
+    this.filterBuildChanged
+      .pipe(
+        debounceTime(this.DEBOUNCE_TIME),
+        distinctUntilChanged())
+      .subscribe((filterBuild: string) => {
+        this.filterToolBuild = filterBuild;
+        this.getBiocondaTools(this.FIRST_PAGE)
+
+      });
+
+  }
+
+  changedNameFilter(text: string): void {
+    this.filternameChanged.next(text);
+
+  }
+
+  changedVersionFilter(text: string): void {
+    this.filterVersionChanged.next(text);
+
+  }
+
+  changedBuildFilter(text: string): void {
+    this.filterBuildChanged.next(text);
+
+  }
+
+  getBiocondaTools(page: number): void {
+    this.condaService.getBiocondaTools(page, this.filterToolName, this.filterToolVersion, this.filterToolBuild).subscribe(
       res => {
-        for (const line in res['packages']) {
-          this.bioconda_tools.push({name: res['packages'][line]['name'],
-                                     version: res['packages'][line]['version'],
-                                     build: res['packages'][line]['build']})
+        this.bioconda_tools = [];
+
+        for (const line of res['packages']) {
+          this.bioconda_tools.push({
+                                     name: line['name'],
+                                     version: line['version'],
+                                     build: line['build']
+                                   })
         }
-        this.applyFilter()});
+
+        this.total_pages = res['num_pages'];
+        this.toolsStart = 0;
+        this.toolsEnd = this.toolsPerPage;
+
+        this.currentPage = page;
+        this.pagination.selectPage(this.currentPage);
+
+      });
   }
 
   pageChanged(event): void {
-
-    const startItem: number = (event.page - 1) * event.itemsPerPage;
-    const endItem: number = event.page * event.itemsPerPage;
-    this.toolsStart = startItem;
-    this.toolsEnd = endItem;
-    this.tools_returned = this.tools_filtered.slice(startItem, endItem)
+    this.getBiocondaTools(event.page);
 
   }
 
-  checkFilter(tool: IBiocondaTool): boolean {
-    return this.filterName(tool) && this.filterVersion(tool) && this.filterBuild(tool);
-
-  }
-
-  applyFilter(): void {
-    this.tools_filtered = this.bioconda_tools.filter(vm => this.checkFilter(vm));
-
-    this.toolsStart = 0;
-    this.toolsEnd = this.toolsPerPage;
-
-    this.tools_returned = this.tools_filtered.slice(this.toolsStart, this.toolsEnd);
-    this.currentPage = 1
-
-  }
-
-  filterName(tool: IBiocondaTool): boolean {
-    if (!this.filterToolName) {
-      return true;
-    } else if (tool.name.includes(this.filterToolName)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  filterVersion(tool: IBiocondaTool): boolean {
-    if (!this.filterToolVersion) {
-      return true;
-    } else if (tool.version.includes(this.filterToolVersion)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  filterBuild(tool: IBiocondaTool): boolean {
-    if (!this.filterToolBuild) {
-      return true;
-    } else if (tool.build.includes(this.filterToolBuild)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
