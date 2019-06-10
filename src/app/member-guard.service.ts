5d18f6efc56cf297f72d1a4eacb66ae243f11dc0
David Weinholz <david-weinholz@web.de>
2019-06-10 22:47:59 +0200

    fix(loop):remove cookie

--- src/app/member-guard.service.ts
+++ src/app/member-guard.service.ts
@@ -26,6 +26,8 @@ export class MemberGuardService implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | boolean {
     const cookieValue: string = this.cookieService.get('redirect_after_login');
+    this.cookieService.delete('redirect_after_login');
+
     let redirect_url: string = state.url;
     if (cookieValue) {
       redirect_url = null;
