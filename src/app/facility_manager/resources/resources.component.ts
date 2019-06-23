203a4f090a9d5e71294baa4851c0e8e4cee06d9a
David Weinholz <david-weinholz@web.de>
2019-06-23 12:03:05 +0200

    fixed some linitng

--- src/app/facility_manager/resources/resources.component.ts
+++ src/app/facility_manager/resources/resources.component.ts
@@ -8,6 +8,9 @@ import ***REMOVED***CoreFactor***REMOVED*** from './core-factor';
 import ***REMOVED***RamFactor***REMOVED*** from './ram-factor';
 import ***REMOVED***forkJoin***REMOVED*** from 'rxjs';
 
+/**
+ * Facility resource component.
+ */
 @Component(***REMOVED***
              selector: 'app-resources',
              templateUrl: './resources.component.html',
@@ -26,6 +29,9 @@ export class ResourcesComponent implements OnInit ***REMOVED***
   isLoaded: boolean = false;
   resources: Resources [];
 
+  /**
+   * Id of the table which will be converted to pdf or csv.
+   */
   tableId: string = 'contentToConvert';
   today: number = Date.now();
   coreFactors: CoreFactor[] = [];
@@ -38,9 +44,40 @@ export class ResourcesComponent implements OnInit ***REMOVED***
   constructor(private facilityService: FacilityService, private exportAsService: ExportAsService) ***REMOVED***
   ***REMOVED***
 
-  public tableToCSV(): void ***REMOVED***
-    this.exportAsService.save(this.exportAsConfigCSV, this.tableId);
+  addCoreFactor(cores: string | number, factor: string | number, description: string): void ***REMOVED***
+    if (cores && factor) ***REMOVED***
+      const re: any = /\,/gi;
+      factor = factor.toString().replace(re, '.');
+      this.facilityService.addCoresFactor(this.selectedFacility['FacilityId'], cores, factor, description)
+        .subscribe((res: CoreFactor[]) => ***REMOVED***
+          this.coreFactors = res;
+          this.getSelectedFacilityResources()
+        ***REMOVED***)
+    ***REMOVED***
+  ***REMOVED***
 
+  addRamFactor(ram: string | number, factor: string | number, description: string): void ***REMOVED***
+    if (ram && factor) ***REMOVED***
+      const re: any = /\,/gi;
+      factor = factor.toString().replace(re, '.');
+      this.facilityService.addRamFactor(this.selectedFacility['FacilityId'], ram, factor, description).subscribe((res: RamFactor[]) => ***REMOVED***
+        this.ramFactors = res;
+        this.getSelectedFacilityResources()
+      ***REMOVED***)
+    ***REMOVED***
+  ***REMOVED***
+
+  onChangeSelectedFacility(): void ***REMOVED***
+    this.getSelectedFacilityResources()
+  ***REMOVED***
+
+  ngOnInit(): void ***REMOVED***
+    this.facilityService.getManagerFacilities().subscribe((result: [string, number][]) => ***REMOVED***
+      this.managerFacilities = result;
+      this.selectedFacility = this.managerFacilities[0];
+      this.getSelectedFacilityResources();
+
+    ***REMOVED***)
   ***REMOVED***
 
   public deleteCoreFactor(id: string | number): void ***REMOVED***
@@ -58,16 +95,6 @@ export class ResourcesComponent implements OnInit ***REMOVED***
     ***REMOVED***)
   ***REMOVED***
 
-  public getRamCoreFactors(): void ***REMOVED***
-    forkJoin(
-      this.facilityService.getCoreFactor(this.selectedFacility['FacilityId']),
-      this.facilityService.getRamFactor(this.selectedFacility['FacilityId'])).subscribe(res => ***REMOVED***
-      this.coreFactors = res[0];
-      this.ramFactors = res[1];
-    ***REMOVED***)
-
-  ***REMOVED***
-
   public getSelectedFacilityResources(): void ***REMOVED***
     this.facilityService.getFacilityResources(this.selectedFacility['FacilityId']).subscribe((res: Resources[]) => ***REMOVED***
 
@@ -78,56 +105,36 @@ export class ResourcesComponent implements OnInit ***REMOVED***
 
   ***REMOVED***
 
-  addCoreFactor(cores: string | number, factor: string | number,description:string): void ***REMOVED***
-    if (cores && factor) ***REMOVED***
-      let re = /\,/gi;
-      factor = factor.toString().replace(re, '.');
-      this.facilityService.addCoresFactor(this.selectedFacility['FacilityId'], cores, factor,description).subscribe(res => ***REMOVED***
-        this.coreFactors = res;
-        this.getSelectedFacilityResources()
-      ***REMOVED***)
-    ***REMOVED***
+  public getRamCoreFactors(): void ***REMOVED***
+    forkJoin(
+      this.facilityService.getCoreFactor(this.selectedFacility['FacilityId']),
+      this.facilityService.getRamFactor(this.selectedFacility['FacilityId'])).subscribe((res: any) => ***REMOVED***
+      this.coreFactors = res[0];
+      this.ramFactors = res[1];
+    ***REMOVED***)
+
   ***REMOVED***
 
-  addRamFactor(ram: string | number, factor: string | number,description:string): void ***REMOVED***
-    if (ram && factor) ***REMOVED***
-      const re = /\,/gi;
-      factor = factor.toString().replace(re, '.');
-      this.facilityService.addRamFactor(this.selectedFacility['FacilityId'], ram, factor,description).subscribe(res => ***REMOVED***
-        this.ramFactors = res;
-        this.getSelectedFacilityResources()
-      ***REMOVED***)
-    ***REMOVED***
+  public tableToCSV(): void ***REMOVED***
+    this.exportAsService.save(this.exportAsConfigCSV, this.tableId);
+
   ***REMOVED***
 
   public tableToPDF(): void ***REMOVED***
     const data: object = document.getElementById(this.tableId);
-    html2canvas(data).then(canvas => ***REMOVED***
+    html2canvas(data).then((canvas: any) => ***REMOVED***
       // Few necessary setting options
       const imgWidth: number = 208;
       const pageHeight: number = 295;
       const imgHeight: number = canvas.height * imgWidth / canvas.width;
       const heightLeft: number = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png');
-      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-      const position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-      pdf.save(this.selectedFacility['Facility'] + '.pdf'); // Generated PDF
+      const contentDataURL: string = canvas.toDataURL('image/png');
+      const pdf: jspdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      const position: number = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save(`$***REMOVED***this.selectedFacility['Facility']***REMOVED***.pdf`); // Generated PDF
     ***REMOVED***);
-  ***REMOVED***
-
-  onChangeSelectedFacility(value): void ***REMOVED***
-    this.getSelectedFacilityResources()
-  ***REMOVED***
-
-  ngOnInit() ***REMOVED***
-    this.facilityService.getManagerFacilities().subscribe((result: [string, number][]) => ***REMOVED***
-      this.managerFacilities = result;
-      this.selectedFacility = this.managerFacilities[0];
-      this.getSelectedFacilityResources();
-
-    ***REMOVED***)
   ***REMOVED***
 
 ***REMOVED***
