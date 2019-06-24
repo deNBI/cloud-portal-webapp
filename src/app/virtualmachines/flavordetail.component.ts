43d38868391ba2748c9edb4277d9e26b7c9d65e1
David Weinholz <david-weinholz@web.de>
2019-06-24 10:30:34 +0200

    feature(Flavors):added cards

--- src/app/virtualmachines/flavordetail.component.ts
+++ src/app/virtualmachines/flavordetail.component.ts
@@ -1,12 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Flavor} from './virtualmachinemodels/flavor'
 
 @Component({
-    selector: 'app-flavor-detail',
-    templateUrl: 'flavordetail.component.html'
+             selector: 'app-flavor-detail',
+             templateUrl: 'flavordetail.component.html'
 
-})
+           })
 export class FlavorDetailComponent {
-    @Input() flavor: Flavor;
-    @Input() collapse2: boolean;
+  @Input() selectedFlavor: Flavor;
+  @Input() flavors: Flavor[];
+  @Output() readonly selectedFlavorChange: EventEmitter<Flavor> = new EventEmitter();
+  flavors_per_row: number = 4;
+  flavors_visible: number = this.flavors_per_row;
+
+  setSelectedFlavor(flavor: Flavor): void {
+    this.selectedFlavor = flavor;
+    this.selectedFlavorChange.emit(this.selectedFlavor);
+  }
 }
