b735b3d2bfdb06a46ab4e9d68ce03ca44c377d72
David Weinholz <david-weinholz@web.de>
2019-08-07 12:00:37 +0200

    feature(Snapshot):visual difference in snapshots and images

--- src/app/virtualmachines/virtualmachinemodels/image.ts
+++ src/app/virtualmachines/virtualmachinemodels/image.ts
@@ -2,11 +2,12 @@
  * Image class.
  */
 export class Image {
-    id: string;
-    name: string;
-    status: string;
-    min_disk: number;
-    min_ram: number;
-    description: string;
+  id: string;
+  name: string;
+  status: string;
+  min_disk: number;
+  min_ram: number;
+  description: string;
+  is_snapshot: boolean;
 
 }
