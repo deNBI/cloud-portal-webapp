45194c6a6aeb1e91370f5fe4e0aaa492aba6054f
David Weinholz <david-weinholz@web.de>
2019-08-27 01:45:40 +0200

    login with relog param

--- e2e/simple_vm_application_test.ts
+++ e2e/simple_vm_application_test.ts
@@ -4,7 +4,7 @@ import {LoginPage} from './page_objects/login.po';
 import {FormularPage} from "./page_objects/application_formular.po";
 import {ApplicationOverviewPage} from "./page_objects/application_overview.po";
 
-describe('Cloud Application Test', function () {
+describe('Simple Application Test', function () {
     const formularPage: FormularPage = new FormularPage();
     const applicationOverviewPage: ApplicationOverviewPage = new ApplicationOverviewPage();
     const loginPage: LoginPage = new LoginPage();
@@ -15,8 +15,8 @@ describe('Cloud Application Test', function () {
         await loginPage.login(browser.params.login.email_user, browser.params.login.password_user, true);
     });
 
-    it('should send a cloud application', async function () {
-        await formularPage.navigateToCloudApplication();
+    it('should send a simple vm application', async function () {
+        await formularPage.navigateToSimpleVmApplication();
         console.log('Getting form.');
         await formularPage.fillFormular();
 
