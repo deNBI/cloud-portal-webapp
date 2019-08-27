cd043b35d2e7ac64cdebc79562fbb5bdae3b20d5
David Weinholz <david-weinholz@web.de>
2019-08-27 11:53:36 +0200

    make pages static

--- e2e/page_objects/application_overview.po.ts
+++ e2e/page_objects/application_overview.po.ts
@@ -1,11 +1,10 @@
-import {browser, by, element, protractor, ProtractorExpectedConditions} from 'protractor';
+import {by, element} from 'protractor';
 import {Util} from "../util";
 
 export class ApplicationOverviewPage {
-    private timeout: number = browser.params.timeout;
-    private OWN_APPLICATION_ID: string = "own_applications";
+    private static OWN_APPLICATION_ID: string = "own_applications";
 
-    async isApplicationRequestPresent(application_name: string): Promise<boolean> {
+    static async isApplicationRequestPresent(application_name: string): Promise<boolean> {
         await Util.waitForPage('applications');
         await Util.waitForPresenceOfElement(this.OWN_APPLICATION_ID);
         const elm = element(by.id(application_name));
