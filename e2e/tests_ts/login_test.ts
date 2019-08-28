536002ad279d0b1e676b959e221b5f18e5ae1e2d
David Weinholz <david-weinholz@web.de>
2019-08-28 15:25:32 +0200

    added custom timeout to util

--- e2e/tests_ts/login_test.ts
+++ e2e/tests_ts/login_test.ts
@@ -4,23 +4,23 @@ import {Util} from '../util';
 
 describe('Login test', async function () {
 
-  beforeAll(function () {
-    console.log('------------------------------Login test started');
-    browser.waitForAngularEnabled(false);
-    browser.get(browser.params.portal);
-  });
-
-  it('should login', async function () {
-    LoginPage.login(browser.params.login.email_user, browser.params.login.password_user, browser.params.login.auth_user);
-    await Util.waitForPage('userinfo').then(function (result) {
-      expect(result).toEqual(true);
+    beforeAll(function () {
+        console.log('------------------------------Login test started');
+        browser.waitForAngularEnabled(false);
+        browser.get(browser.params.portal);
     });
-  });
 
-  it('should restart', async function () {
-    await LoginPage.logOut();
-    await Util.waitForPage('data:,').then(function (result) {
-      expect(result).toEqual(true);
+    it('should login', async function () {
+        LoginPage.login(browser.params.login.email_user, browser.params.login.password_user, browser.params.login.auth_user);
+        let result: boolean = await Util.waitForPage('userinfo');
+        expect(result).toEqual(true);
+
     });
-  })
+
+    it('should restart', async function () {
+        await LoginPage.logOut();
+        let result: boolean = await Util.waitForPage('data:,');
+        expect(result).toEqual(true);
+
+    })
 });
