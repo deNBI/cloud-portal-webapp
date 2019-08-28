0087b02ffcb81a8281a2c0f5b8834c13c8755d14
David Weinholz <david-weinholz@web.de>
2019-08-28 12:35:47 +0200

    use more util

--- e2e/tests_ts/cloud_application_test.ts
+++ e2e/tests_ts/cloud_application_test.ts
@@ -1,9 +1,9 @@
 // spec.js
 import {browser, by, element, protractor} from 'protractor';
-import {LoginPage} from './page_objects/login.po';
-import {FormularPage} from "./page_objects/application_formular.po";
-import {ApplicationOverviewPage} from "./page_objects/application_overview.po";
-import {Util} from "./util";
+import {LoginPage} from '../page_objects/login.po';
+import {FormularPage} from "../page_objects/application_formular.po";
+import {ApplicationOverviewPage} from "../page_objects/application_overview.po";
+import {Util} from "../util";
 
 describe('Cloud Application Test', function () {
 
--- e2e/tests_ts/simple_vm_modification_test.ts
+++ e2e/tests_ts/simple_vm_modification_test.ts
@@ -1,19 +1,20 @@
 // spec.js
 import {browser, by, element, protractor} from 'protractor';
-import {LoginPage} from './page_objects/login.po';
-import {FormularPage} from "./page_objects/application_formular.po";
-import {ApplicationOverviewPage} from "./page_objects/application_overview.po";
-import {Util} from "./util";
+import {LoginPage} from '../page_objects/login.po';
+import {FormularPage} from "../page_objects/application_formular.po";
+import {ApplicationOverviewPage} from "../page_objects/application_overview.po";
+import {Util} from "../util";
 
-describe('Simple Application Approval Test', function () {
+describe('Simple Application Modification Test', function () {
 
     beforeEach(async function () {
         await browser.waitForAngularEnabled(false);
         await LoginPage.login(browser.params.login.email_vo, browser.params.login.password_vo, browser.params.login.auth_vo, true);
     });
 
-    it('should approve A SIMPLE VM APPLICATION', async function () {
+    it('should send a modification request', async function () {
         await ApplicationOverviewPage.navigateToApplicationOverview();
+        await Util.waitForElementToBeClickableById('extension_' + Util.SIMPLE_VM_APPLICATION_NAME);
 
         await ApplicationOverviewPage.approveSimpleVm(Util.SIMPLE_VM_APPLICATION_NAME);
         await Util.waitForTextPresenceInElementById('notification_message', "The new project was created and assigned to de.NBI Cloud Portal - Development.");
