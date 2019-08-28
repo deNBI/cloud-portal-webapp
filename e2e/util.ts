52269134dedd71a7c1d19d015c28540d403232bc
David Weinholz <david-weinholz@web.de>
2019-08-28 11:45:27 +0200

    added approval simple vm

--- e2e/util.ts
+++ e2e/util.ts
@@ -3,13 +3,32 @@ import {browser, by, element, protractor, ProtractorExpectedConditions} from 'pr
 export class Util {
     private static timeout: number = browser.params.timeout;
     private static auth = browser.params.login.auth;
+    private static _SIMPLE_VM_APPLICATION_NAME: string = "PTSimpleVM";
+    private static _OPENSTACK_APPLICATION_NAME: string = "PTOpenStack";
 
+
+    static get SIMPLE_VM_APPLICATION_NAME(): string {
+        return this._SIMPLE_VM_APPLICATION_NAME;
+    }
+
+    static get OPENSTACK_APPLICATION_NAME(): string {
+        return this._OPENSTACK_APPLICATION_NAME;
+    }
 
     static async waitForPage(url: string): Promise<boolean> {
         const until: ProtractorExpectedConditions = protractor.ExpectedConditions;
 
         console.log(`Waiting until page contains ${url}`);
         return await browser.driver.wait(until.urlContains(url), this.timeout);
+    }
+
+
+    static async waitForTextPresenceInElementById(id: string, text: string): Promise<boolean> {
+        const until: ProtractorExpectedConditions = protractor.ExpectedConditions;
+
+        console.log(`Waiting until text [${text}] appears in  element ${id}`);
+        const elem = element(by.id(id));
+        return await browser.driver.wait(until.textToBePresentInElement(elem, text), this.timeout, 'Text taking too long to appear in the Element');
     }
 
     static async waitForPresenceOfElement(id: string): Promise<boolean> {
