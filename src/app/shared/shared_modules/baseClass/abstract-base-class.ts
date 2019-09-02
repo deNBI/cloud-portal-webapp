f6693df66543e96c4c6b8fe5715964f7750b4808
David Weinholz <david-weinholz@web.de>
2019-09-02 13:53:51 +0200

    added message if not found

--- src/app/shared/shared_modules/baseClass/abstract-base-class.ts
+++ src/app/shared/shared_modules/baseClass/abstract-base-class.ts
@@ -28,7 +28,8 @@ enum Vm_Statuses ***REMOVED***
   DELETED = 3,
   CLIENT_OFFLINE = 4,
   'POWERING OFF' = 5,
-  RESTARTING = 6
+  RESTARTING = 6,
+  'NOT FOUND' = 7
 ***REMOVED***
 
 export abstract class AbstractBaseClasse ***REMOVED***
