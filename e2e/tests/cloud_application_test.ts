4df7918ca50206b8b14668f90ac322a308dca574
David Weinholz <david-weinholz@web.de>
2019-10-04 18:19:35 +0200

    .

--- e2e/tests/cloud_application_test.ts
+++ e2e/tests/cloud_application_test.ts
@@ -1,38 +1,36 @@
 // spec.js
 import {browser, by, element, protractor} from 'protractor';
 import {LoginPage} from '../page_objects/login.po';
-import {FormularPage} from "../page_objects/application_formular.po";
-import {ApplicationOverviewPage} from "../page_objects/application_overview.po";
-import {Util} from "../util";
+import {FormularPage} from '../page_objects/application_formular.po';
+import {ApplicationOverviewPage} from '../page_objects/application_overview.po';
+import {Util} from '../util';
 
 describe('Cloud Application Test', function () {
 
-    beforeAll(async function () {
-        await browser.waitForAngularEnabled(false);
-        await LoginPage.login(browser.params.login.email_user, browser.params.login.password_user, browser.params.login.auth_user, true);
-    });
+  beforeAll(async function () {
+    await browser.waitForAngularEnabled(false);
+    await LoginPage.login(browser.params.login.email_user, browser.params.login.password_user, browser.params.login.auth_user, true);
+  });
 
-    it('should navigate to cloud application form', async function () {
-        console.log("Starting send a cloud  application test!");
-        await FormularPage.navigateToCloudApplication();
-    });
+  it('should navigate to cloud application form', async function () {
+    console.log('Starting send a cloud  application test!');
+    await FormularPage.navigateToCloudApplication();
+  });
 
-    it('should fill cloud application form', async function () {
+  it('should fill cloud application form', async function () {
 
-        await FormularPage.fillApplicationFormular(Util.OPENSTACK_APPLICATION_NAME);
-    });
+    await FormularPage.fillApplicationFormular(Util.OPENSTACK_APPLICATION_NAME);
+  });
 
+  it('should submit cloud application ', async function () {
 
-    it('should submit cloud application ', async function () {
+    await FormularPage.submitApplication();
 
-        await FormularPage.submitApplication();
-        let isPresent: boolean = await ApplicationOverviewPage.isApplicationRequestPresent(Util.OPENSTACK_APPLICATION_NAME);
-        expect(isPresent).toBeTruthy();
-    });
+  });
 
-    it('should have a cloud vm application in the application overview', async function () {
+  it('should successfully submitted the application', async function () {
 
-        let isPresent: boolean = await ApplicationOverviewPage.isApplicationRequestPresent(Util.OPENSTACK_APPLICATION_NAME);
-        expect(isPresent).toBeTruthy();
-    });
+    const isPresent: boolean = await FormularPage.isApplicationSubmitted();
+    expect(isPresent).toBeTruthy();
+  });
 });
