c91b82f8d2b3727aa74a82197250b9aa819f3a4d
David Weinholz <david-weinholz@web.de>
2019-10-04 15:53:01 +0200

    started refactoring

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -28,9 +28,10 @@ exports.config = {
     },
     allScriptsTimeout: 11000,
     specs: [
-        'tests_ts/login_test.ts',
-      /*
-        'tests_ts/simple_vm_application_test.ts',
+        'tests/login_test.ts',
+
+        'tests/simple_vm_application_test.ts',
+       /*
         'tests_ts/simple_vm_approval_test.ts',
         'tests_ts/member_test.ts',
         'tests_ts/simple_vm_modification_test.ts',
