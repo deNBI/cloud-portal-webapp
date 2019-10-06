401910a4b8c037b5b5ce88e1cf60454f53f744e2
David Weinholz <david-weinholz@web.de>
2019-10-06 20:35:58 +0200

    all refactored

--- e2e/page_objects/volume_overview.po.ts
+++ e2e/page_objects/volume_overview.po.ts
@@ -53,6 +53,7 @@ export class VolumeOverviewPage {
     await Util.clickElementById(this.CREATE_ATTACH_BUTTON);
     await Util.waitForVisibilityOfElementById(this.CREATE_MODAL);
     await Util.clickOptionOfSelect(`${this.OPTION_PROJECT_PREFIX}${Util.SIMPLE_VM_APPLICATION_NAME}`, this.PROJECT_SELECT_ID);
+    await Util.waitForPresenceOfElementById(this.VM_SELECT_ID);
     await Util.clickOptionOfSelect(`${this.OPTION_VM_PREFIX}${vm}`, this.VM_SELECT_ID);
     await Util.sendTextToElementById(this.NAME_INPUT_ID, Util.VOLUME_NAME);
     await Util.sendTextToElementById(this.SPACE_INPUT_ID, Util.VOLUME_SPACE);
