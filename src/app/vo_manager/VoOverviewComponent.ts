b5d7e58eb8ba2b10d1382af47a8c8ed95f2d3cc6
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2019-10-07 14:27:01 +0200

    feat(newsletter): added dsgvo info

--- src/app/vo_manager/VoOverviewComponent.ts
+++ src/app/vo_manager/VoOverviewComponent.ts
@@ -41,6 +41,7 @@ export class VoOverviewComponent extends FilterBaseClass implements OnInit {
   public selectedProjectSnapshots: SnapshotModel[] = [];
   computecenters: ComputecenterComponent[] = [];
 
+
   selectedProjectType: string = 'ALL';
   selectedFacility: string | number = 'ALL';
 
