1d92d91732bcdfc1f36fa67a6f422dc51685e508
David Weinholz <david-weinholz@web.de>
2019-10-09 12:04:52 +0200

    fix(Applications):fixed spinner

--- src/app/projectmanagement/overview.component.ts
+++ src/app/projectmanagement/overview.component.ts
@@ -105,6 +105,9 @@ export class OverviewComponent extends ApplicationBaseClass implements OnInit {
       this.application_action = 'approved';
       this.application_member_name = membername;
       this.application_action_done = true;
+      this.getUserProjectApplications();
+      this.getMembersOfTheProject();
+      this.loaded = true;
 
     });
   }
@@ -307,6 +310,7 @@ export class OverviewComponent extends ApplicationBaseClass implements OnInit {
                    this.application_member_name = membername;
                    this.application_action_done = true;
                    this.getUserProjectApplications();
+                   this.loaded = true;
 
                  }
       );
@@ -322,6 +326,8 @@ export class OverviewComponent extends ApplicationBaseClass implements OnInit {
 
       const newProjectApplications: ProjectMemberApplication[] = [];
       if (applications.length === 0) {
+        this.project.ProjectMemberApplications = [];
+
         this.loaded = true;
 
       }
@@ -335,7 +341,6 @@ export class OverviewComponent extends ApplicationBaseClass implements OnInit {
             `${dateApplicationCreated.date()}.${(dateApplicationCreated.month() + 1)}.${dateApplicationCreated.year()}`
           );
         newProjectApplications.push(newMemberApplication);
-
         this.project.ProjectMemberApplications = newProjectApplications;
         this.loaded = true;
 
