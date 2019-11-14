50f86027af2aa2850bfa91e59ab0a114c7319e26
eKatchko <47833533+eKatchko@users.noreply.github.com>
2019-11-15 00:23:45 +0100

    feature(news): alert that mail to ALL posts to newsservice (#845)
    * feature(news): alert that mail to ALL posts to newsservice
    
    * feature(fm-mail): checkbox if post news

--- src/app/api-connector/facility.service.ts
+++ src/app/api-connector/facility.service.ts
@@ -278,9 +278,14 @@ export class FacilityService {
    * @param reply reply address
    * @returns {Observable<any>}
    */
-  sendMailToFacility(facility: string, subject: string, message: string, project_type: string, reply?: string): Observable<any> {
-    const params: HttpParams = new HttpParams().set('subject', subject)
-      .set('facility_id', facility).set('message', message).set('reply', reply).set('type', project_type);
+  sendMailToFacility(facility: string, subject: string, message: string, project_type: string, reply?: string, sendNews?: any): Observable<any> {
+    const params: HttpParams = new HttpParams()
+      .set('subject', subject)
+      .set('facility_id', facility)
+      .set('message', message)
+      .set('reply', reply)
+      .set('type', project_type)
+      .set('sendNews', sendNews);
 
     return this.http.post(`${ApiSettings.getApiBaseURL()}facilityManagers/current/facilityMail/`, params, {
                             withCredentials: true,
