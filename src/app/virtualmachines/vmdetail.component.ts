8a2e5f5f2604e83be6a83eeafe86e38af277894c
Ewgenij Katchko <ekatchko@techfak.uni-bielefeld.de>
2020-02-04 14:00:50 +0100

    feat(resenv): add rstudio support, fix mode suggestions, conda tab to ansible, add res-env link to detail page, adjust wiki link

--- src/app/virtualmachines/vmdetail.component.ts
+++ src/app/virtualmachines/vmdetail.component.ts
@@ -20,17 +20,18 @@ import {Subject} from 'rxjs';
 import {PlaybookService} from '../api-connector/playbook.service';
 import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 import {CondaPackage} from './condaPackage.model';
+import {TemplateNames} from './conda/template-names';
 
 /**
  * VM Detail page component
  */
 @Component({
-  selector: 'app-virtual-machine-detail',
-  templateUrl: 'vmdetail.component.html',
-  styleUrls: ['./vmdetail.component.scss'],
+             selector: 'app-virtual-machine-detail',
+             templateUrl: 'vmdetail.component.html',
+             styleUrls: ['./vmdetail.component.scss'],
              providers: [FlavorService, FacilityService, VoService, UserService, GroupService, ApiSettings,
                VoService, CreditsService, VirtualmachineService, ImageService, PlaybookService]
-})
+           })
 
 export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
 
@@ -44,6 +45,7 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
   snapshotSearchTerm: Subject<string> = new Subject<string>();
   errorMessage: boolean = false;
   private _condaPackages: CondaPackage[] = [];
+  res_env_url: string = '';
 
   DEBOUNCE_TIME: number = 300;
 
@@ -91,7 +93,8 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
               private applicationService: ApplicationsService,
               private flavorService: FlavorService,
               private imageService: ImageService,
-              private playbookService: PlaybookService) {
+              private playbookService: PlaybookService,
+              private groupService: GroupService) {
     super();
   }
 
@@ -104,11 +107,10 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
   }
 
   ngOnInit(): void {
-
     this.activatedRoute.params.subscribe((paramsId: any) => {
-     this.vm_id = paramsId.id;
-     this.getVmById();
-     this.snapshotSearchTerm
+      this.vm_id = paramsId.id;
+      this.getVmById();
+      this.snapshotSearchTerm
         .pipe(
           debounceTime(this.DEBOUNCE_TIME),
           distinctUntilChanged())
@@ -147,11 +149,11 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
     this.virtualmachineService.checkVmStatus(this.virtualMachine.openstackid)
       .subscribe((updated_vm: VirtualMachine) => {
 
-          if (updated_vm.created_at !== '') {
-            updated_vm.created_at = new Date(parseInt(updated_vm.created_at, 10) * 1000).toLocaleDateString();
-          }
-          this.virtualMachine = updated_vm;
-        }
+                   if (updated_vm.created_at !== '') {
+                     updated_vm.created_at = new Date(parseInt(updated_vm.created_at, 10) * 1000).toLocaleDateString();
+                   }
+                   this.virtualMachine = updated_vm;
+                 }
       )
   }
 
@@ -295,37 +297,37 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
   stopVm(): void {
     this.virtualmachineService.stopVM(this.virtualMachine.openstackid)
       .subscribe((updated_vm: VirtualMachine) => {
-          this.status_changed = 0;
+                   this.status_changed = 0;
 
-          if (updated_vm.created_at !== '') {
-            updated_vm.created_at = new Date(parseInt(updated_vm.created_at, 10) * 1000).toLocaleDateString();
-          }
-          updated_vm.cardState = 0;
-          this.virtualMachine = updated_vm;
+                   if (updated_vm.created_at !== '') {
+                     updated_vm.created_at = new Date(parseInt(updated_vm.created_at, 10) * 1000).toLocaleDateString();
+                   }
+                   updated_vm.cardState = 0;
+                   this.virtualMachine = updated_vm;
 
-          switch (updated_vm.status) {
-            case VirtualMachineStates.SHUTOFF:
-              this.status_changed = 1;
-              break;
-            case VirtualMachineStates.POWERING_OFF:
-              this.check_status_loop(VirtualMachineStates.SHUTOFF, true);
-              break;
-            default:
-              this.status_changed = 2;
-              break;
+                   switch (updated_vm.status) {
+                     case VirtualMachineStates.SHUTOFF:
+                       this.status_changed = 1;
+                       break;
+                     case VirtualMachineStates.POWERING_OFF:
+                       this.check_status_loop(VirtualMachineStates.SHUTOFF, true);
+                       break;
+                     default:
+                       this.status_changed = 2;
+                       break;
 
-          }
+                   }
 
-        }
+                 }
       )
   }
 
   checkVmTillActive(): void {
-      if (this.virtualMachine.status !== VirtualMachineStates.ACTIVE &&
-        this.virtualMachine.status !== VirtualMachineStates.SHUTOFF &&
-        this.virtualMachine.status !== VirtualMachineStates.DELETED) {
-        this.check_status_loop(VirtualMachineStates.ACTIVE);
-      }
+    if (this.virtualMachine.status !== VirtualMachineStates.ACTIVE &&
+      this.virtualMachine.status !== VirtualMachineStates.SHUTOFF &&
+      this.virtualMachine.status !== VirtualMachineStates.DELETED) {
+      this.check_status_loop(VirtualMachineStates.ACTIVE);
+    }
   }
 
   resumeVM(): void {
@@ -378,36 +380,37 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
           this.errorMessage = true;
           // TODO: Redirect back to overview
         } else {
-           this.playbookService.getPlaybookForVM(this.vm_id).subscribe((pb: Object) => {
-             if (pb != null) {
-               let pbs: string = pb['playbooks'].toString();
-               if (pbs != null) {
-                 pbs = pbs.replace(/\\/g, '');
-                 pbs = pbs.replace('"[', '[');
-                 pbs = pbs.replace(']"', ']');
-                 const pkgs: Object = JSON.parse(pbs);
-                 if (pkgs != null) {
-                   const package_list: Object = pkgs['bioconda'];
-                   if (package_list != null) {
-                     for (const packageObject in package_list['packages']) {
-                       if (package_list['packages'].hasOwnProperty(packageObject)) {
-                         const c_index: string = packageObject;
-                         const c_package: any = package_list['packages'][c_index];
-                         this._condaPackages.push(new CondaPackage(c_package.name, c_package.version, c_package.build));
-                       }
-                     }
-                   }
-                 }
-               }
-             }
-           });
-           this.title = vm['name'];
-           this.virtualMachine = vm;
-           this.startDate = parseInt(this.virtualMachine.created_at, 10) * 1000;
-           this.stopDate = parseInt(this.virtualMachine.stopped_at, 10) * 1000;
-           this.stopDate = parseInt(this.virtualMachine.stopped_at, 10) * 1000;
-           this.getImageDetails(this.virtualMachine.projectid, this.virtualMachine.image);
-           this.isLoaded = true;
+          this.playbookService.getPlaybookForVM(this.vm_id).subscribe((pb: Object) => {
+            if (pb != null) {
+              let pbs: string = pb['playbooks'].toString();
+              if (pbs != null) {
+                pbs = pbs.replace(/\\/g, '');
+                pbs = pbs.replace('"[', '[');
+                pbs = pbs.replace(']"', ']');
+                const pkgs: Object = JSON.parse(pbs);
+                if (pkgs != null) {
+                  const package_list: Object = pkgs['bioconda'];
+                  if (package_list != null) {
+                    for (const packageObject in package_list['packages']) {
+                      if (package_list['packages'].hasOwnProperty(packageObject)) {
+                        const c_index: string = packageObject;
+                        const c_package: any = package_list['packages'][c_index];
+                        this._condaPackages.push(new CondaPackage(c_package.name, c_package.version, c_package.build));
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          });
+          this.checkAndGetForcDetails(vm);
+          this.title = vm['name'];
+          this.virtualMachine = vm;
+          this.startDate = parseInt(this.virtualMachine.created_at, 10) * 1000;
+          this.stopDate = parseInt(this.virtualMachine.stopped_at, 10) * 1000;
+          this.stopDate = parseInt(this.virtualMachine.stopped_at, 10) * 1000;
+          this.getImageDetails(this.virtualMachine.projectid, this.virtualMachine.image);
+          this.isLoaded = true;
         }
       }
     );
@@ -421,7 +424,7 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
       },
       (error: any) => {
         this.isLoaded = false;
-    }
+      }
     );
 
     return newImage;
@@ -441,5 +444,33 @@ export class VmDetailComponent extends AbstractBaseClasse implements OnInit {
       document.removeEventListener('copy', null);
     });
     document.execCommand('copy');
+  }
+
+  checkAndGetForcDetails(vm: VirtualMachine): void {
+    let checkForForc: boolean = true;
+    for (const mode of vm.modes) {
+      if (TemplateNames.ALL_TEMPLATE_NAMES.indexOf(mode.name) !== -1) {
+        checkForForc = false;
+      }
+    }
+    if (checkForForc) {
+      this.groupService.getClientHasForc(vm.client.id, 'true').subscribe((hasForc: JSON) => {
+        if (hasForc['hasForc'] === 'True') {
+          this.groupService.getClientForcUrl(vm.client.id).subscribe((response: JSON) => {
+            if (response['forc_url'] !== 'None') {
+              this.virtualmachineService.getLocationUrl(vm.openstackid)
+                .subscribe((url: any) => {
+                  if (url !== '') {
+                    vm.res_env_url = `${response['forc_url']}${url}/`;
+                  } else {
+                    vm.res_env_url = '';
+                  }
+                });
+            }
+          });
+        }
+      });
+    }
+
   }
 }
