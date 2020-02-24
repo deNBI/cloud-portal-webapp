0e5740343fb66f00e89703bcdacec0963b792834
dweinholz <david-weinholz@web.de>
2020-02-24 13:10:12 +0100

    Update links.ts

--- src/links/links.ts
+++ src/links/links.ts
@@ -1,6 +1,6 @@
 export const WIKI_VOLUME: string = 'https://cloud.denbi.de/wiki/portal/volumes/#mount-a-volume';
 export const WIKI_GROUP_INVITATIONS: string = 'https://cloud.denbi.de/wiki/portal/project_overview/#inviting-members';
-export const WIKI_SNAPSHOTS: string = 'https://cloud.denbi.de/wiki/portal/snapsho/';
+export const WIKI_SNAPSHOTS: string = 'https://cloud.denbi.de/wiki/portal/snapshots/';
 export const WIKI: string = 'https://cloud.denbi.de/wiki/';
 export const WIKI_GENERATE_KEYS: string = 'https://cloud.denbi.de/wiki/quickstart/#generate-ssh-keys';
 export const WIKI_NEWS_MANAGEMENT: string = 'https://cloud.denbi.de/wiki/cloud_admin/#news-management';
