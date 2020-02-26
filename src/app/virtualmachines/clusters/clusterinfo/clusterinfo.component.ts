487d5f62140d2c67fa5a98315677baef9548572e
David Weinholz <david-weinholz@web.de>
2020-02-26 16:54:47 +0100

    fixed linting

--- src/app/virtualmachines/clusters/clusterinfo/clusterinfo.component.ts
+++ src/app/virtualmachines/clusters/clusterinfo/clusterinfo.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {Clusterinfo} from '../clusterinfo';
 
+/**
+ * Clusterinfo component
+ */
 @Component({
              selector: 'app-clusterinfo',
              templateUrl: './clusterinfo.component.html'
