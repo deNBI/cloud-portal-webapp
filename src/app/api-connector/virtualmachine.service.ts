bdb406a2d0c0de31172d9a0942aa5e11a43c62a0
Viktor <43116821+vktrrdk@users.noreply.github.com>
2020-03-04 17:04:39 +0100

    feat(Volume):multiple volumes possible
    * started working on feat
    
    * add volume in own row with add button and discard button
    
    * implementing list to show which volumes will be mounted to the new machine
    
    * fix(Linting):blacked code
    
    * added regex-and input checking for volume name and storage values
    
    * added restrictions for adding volumes to list
    
    * machines now can start with multiple vms
    
    * added comments/documentation
    
    * api gets empty volume_name
    
    * fixed bug
    
    * fix(Linting):blacked code
    
    * changed test for volume
    
    * fixed tslint
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: dweinholz <david-weinholz@web.de>

--- src/app/api-connector/virtualmachine.service.ts
+++ src/app/api-connector/virtualmachine.service.ts
@@ -70,7 +70,7 @@ export class VirtualmachineService {
   }
 
   startVM(flavor: string, image: Image, servername: string, project: string, projectid: string,
-          http: boolean, https: boolean, udp: boolean, volume_name?: string, volume_storage?: string,
+          http: boolean, https: boolean, udp: boolean, volumes: Volume[],
           playbook_information?: string, user_key_url?: string): Observable<any> {
 
     const params: HttpParams = new HttpParams()
@@ -79,8 +79,7 @@ export class VirtualmachineService {
       .set('servername', servername)
       .set('project', project)
       .set('projectid', projectid)
-      .set('volume_storage', volume_storage)
-      .set('volume_name', volume_name)
+      .set('volumes', JSON.stringify(volumes))
       .set('http_allowed', http.toString())
       .set('https_allowed', https.toString())
       .set('udp_allowed', udp.toString())
