6002d07d09e01735cc51972d3114e22cd9985ec7
David Weinholz <david-weinholz@web.de>
2020-03-10 14:45:26 +0100

    updaed links script

--- src/links/check_links.sh
+++ src/links/check_links.sh
@@ -4,8 +4,11 @@ WIKI_GROUP_INVITATIONS=https://cloud.denbi.de/wiki/portal/project_overview/#invi
 WIKI_SNAPSHOTS=https://cloud.denbi.de/wiki/portal/snapshots/
 WIKI=https://cloud.denbi.de/wiki/
 WIKI_GENERATE_KEYS=https://cloud.denbi.de/wiki/quickstart/#generate-ssh-keys
+WIKI_VOLUME_OVERVIEW=https://cloud.denbi.de/wiki/portal/volumes/
 NEWS=https://cloud.denbi.de/news/
-WIKI_LNKS=("$WIKI_MOUNT_VOLUME" "$WIKI_GROUP_INVITATIONS" "$WIKI_SNAPSHOTS" "$WIKI" "$WIKI_GENERATE_KEYS" "$NEWS")
+WIKI_EXTEND_VOLUME=https://cloud.denbi.de/portal/volumes/#extend-a-volume;
+
+WIKI_LNKS=("$WIKI_VOLUME_OVERVIEW" "$WIKI_EXTEND_VOLUME" "$WIKI_MOUNT_VOLUME" "$WIKI_GROUP_INVITATIONS" "$WIKI_SNAPSHOTS" "$WIKI" "$WIKI_GENERATE_KEYS" "$NEWS")
 for i in "${WIKI_LNKS[@]}"; do
   echo "$i"
   status_code=$(curl -o /dev/null -Isw '%{http_code}\n' "$i")
