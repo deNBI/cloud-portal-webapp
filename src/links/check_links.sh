684e02568ca9f10f7ce10c6c9896d2adfbbd2f68
David Weinholz <david-weinholz@web.de>
2020-03-10 14:38:45 +0100

    feat(Volume=:added links

--- src/links/check_links.sh
+++ src/links/check_links.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-WIKI_VOLUME=https://cloud.denbi.de/wiki/portal/volumes/#mount-a-volume
+WIKI_MOUNT_VOLUME=https://cloud.denbi.de/wiki/portal/volumes/#mount-a-volume
 WIKI_GROUP_INVITATIONS=https://cloud.denbi.de/wiki/portal/project_overview/#inviting-members
 WIKI_SNAPSHOTS=https://cloud.denbi.de/wiki/portal/snapshots/
 WIKI=https://cloud.denbi.de/wiki/
 WIKI_GENERATE_KEYS=https://cloud.denbi.de/wiki/quickstart/#generate-ssh-keys
 NEWS=https://cloud.denbi.de/news/
-WIKI_LNKS=("$WIKI_VOLUME" "$WIKI_GROUP_INVITATIONS" "$WIKI_SNAPSHOTS" "$WIKI" "$WIKI_GENERATE_KEYS" "$NEWS")
+WIKI_LNKS=("$WIKI_MOUNT_VOLUME" "$WIKI_GROUP_INVITATIONS" "$WIKI_SNAPSHOTS" "$WIKI" "$WIKI_GENERATE_KEYS" "$NEWS")
 for i in "${WIKI_LNKS[@]}"; do
   echo "$i"
   status_code=$(curl -o /dev/null -Isw '%{http_code}\n' "$i")
--- src/links/links.ts
+++ src/links/links.ts
@@ -1,4 +1,4 @@
-export const WIKI_VOLUME: string = 'https://cloud.denbi.de/wiki/portal/volumes/#mount-a-volume';
+export const WIKI_MOUNT_VOLUME: string = 'https://cloud.denbi.de/wiki/portal/volumes/#mount-a-volume';
 export const WIKI_GROUP_INVITATIONS: string = 'https://cloud.denbi.de/wiki/portal/project_overview/#inviting-members';
 export const WIKI_SNAPSHOTS: string = 'https://cloud.denbi.de/wiki/portal/snapshots/';
 export const WIKI: string = 'https://cloud.denbi.de/wiki/';
@@ -6,14 +6,16 @@ export const WIKI_GENERATE_KEYS: string = 'https://cloud.denbi.de/wiki/quickstar
 export const WIKI_NEWS_MANAGEMENT: string = 'https://cloud.denbi.de/wiki/cloud_admin/#news-management';
 export const WIKI_SIMPLEVM_CUSTOMISATION: string = 'https://cloud.denbi.de/portal/customisation';
 export const WIKI_EXTEND_VOLUME: string = 'https://cloud.denbi.de/portal/volumes/#extend-a-volume';
+export const WIKI_VOLUME_OVERVIEW: string = 'https://cloud.denbi.de/wiki/portal/volumes/';
 export const WIKI_LNKS: string[] =
   [
-    WIKI_VOLUME,
+    WIKI_MOUNT_VOLUME,
     WIKI_GROUP_INVITATIONS,
     WIKI_SNAPSHOTS,
     WIKI,
     WIKI_GENERATE_KEYS,
     WIKI_NEWS_MANAGEMENT,
     WIKI_SIMPLEVM_CUSTOMISATION,
-    WIKI_EXTEND_VOLUME
+    WIKI_EXTEND_VOLUME,
+    WIKI_VOLUME_OVERVIEW
   ];
