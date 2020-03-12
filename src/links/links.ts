e1b0904fc8a201b21147af0cd8b8936b377748ba
eKatchko <47833533+eKatchko@users.noreply.github.com>
2020-03-12 16:12:04 +0100

    Update links.ts

--- src/links/links.ts
+++ src/links/links.ts
@@ -4,8 +4,8 @@ export const WIKI_SNAPSHOTS: string = 'https://cloud.denbi.de/wiki/portal/snapsh
 export const WIKI: string = 'https://cloud.denbi.de/wiki/';
 export const WIKI_GENERATE_KEYS: string = 'https://cloud.denbi.de/wiki/quickstart/#generate-ssh-keys';
 export const WIKI_NEWS_MANAGEMENT: string = 'https://cloud.denbi.de/wiki/cloud_admin/#news-management';
-export const WIKI_SIMPLEVM_CUSTOMISATION: string = 'https://cloud.denbi.de/portal/customisation';
-export const WIKI_EXTEND_VOLUME: string = 'https://cloud.denbi.de/portal/volumes/#extend-a-volume';
+export const WIKI_SIMPLEVM_CUSTOMISATION: string = 'https://cloud.denbi.de/wiki/portal/customisation';
+export const WIKI_EXTEND_VOLUME: string = 'https://cloud.denbi.de/wiki/portal/volumes/#extend-a-volume';
 export const WIKI_VOLUME_OVERVIEW: string = 'https://cloud.denbi.de/wiki/portal/volumes/';
 export const WIKI_LNKS: string[] =
   [
