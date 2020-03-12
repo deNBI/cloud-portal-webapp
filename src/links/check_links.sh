d604a8d0f84f23ad9f5333f9d90beb285e0b80ef
eKatchko <47833533+eKatchko@users.noreply.github.com>
2020-03-12 16:11:23 +0100

    Update check_links.sh

--- src/links/check_links.sh
+++ src/links/check_links.sh
@@ -6,7 +6,7 @@ WIKI=https://cloud.denbi.de/wiki/
 WIKI_GENERATE_KEYS=https://cloud.denbi.de/wiki/quickstart/#generate-ssh-keys
 WIKI_VOLUME_OVERVIEW=https://cloud.denbi.de/wiki/portal/volumes/
 NEWS=https://cloud.denbi.de/news/
-WIKI_EXTEND_VOLUME=https://cloud.denbi.de/portal/volumes/#extend-a-volume;
+WIKI_EXTEND_VOLUME=https://cloud.denbi.de/wiki/portal/volumes/#extend-a-volume;
 
 WIKI_LNKS=("$WIKI_VOLUME_OVERVIEW" "$WIKI_EXTEND_VOLUME" "$WIKI_MOUNT_VOLUME" "$WIKI_GROUP_INVITATIONS" "$WIKI_SNAPSHOTS" "$WIKI" "$WIKI_GENERATE_KEYS" "$NEWS")
 for i in "${WIKI_LNKS[@]}"; do
