2ec653e17e7c98028b5c3359d67a1e7dc2fba0fa
ekatchko <ewgenijk@gmx.de>
2020-09-23 18:17:52 +0200

    fix(credits): negative total credits to 0. alert to contact

--- src/app/applications/application.model/application.model.ts
+++ src/app/applications/application.model/application.model.ts
@@ -202,8 +202,12 @@ export class Application {
 
   public calcTotalModificationCredits(): number {
     if (this.project_modification_request != null) {
-      return (Math.round(this.project_application_initial_credits * 10) / 10)
-        + (Math.round((this.project_modification_request.extra_credits * 10) / 10))
+      const total_credits: number = this.project_application_initial_credits + this.project_modification_request.extra_credits;
+      if (total_credits <= 0) {
+        return 0;
+      } else {
+        return total_credits;
+      }
     } else {
       return this.project_application_initial_credits
     }
