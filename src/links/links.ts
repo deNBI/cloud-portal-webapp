c14a0868012d90227428c2bd7239455634879ec6
Viktor <43116821+vktrrdk@users.noreply.github.com>
2020-10-29 14:37:53 +0100

    fix(Wiki): refactored all hardcoded links and replaced faulty links (#2218)
    * fix(Wiki): refactored all hardcoded links and replaced faulty links
    
    * fix(Linting):blacked code (#2219)

--- src/links/links.ts
+++ src/links/links.ts
@@ -1,23 +1,23 @@
-export const WIKI_MOUNT_VOLUME: string = 'https://cloud.denbi.de/wiki/simple_vm/volumes/#mount-a-volume';
-export const WIKI_GROUP_INVITATIONS: string = 'https://cloud.denbi.de/wiki/simple_vm/project_overview/#inviting-members';
 export const WIKI_SNAPSHOTS: string = 'https://cloud.denbi.de/wiki/simple_vm/snapshots/';
 export const WIKI: string = 'https://cloud.denbi.de/wiki/';
 export const WIKI_GENERATE_KEYS: string = 'https://cloud.denbi.de/wiki/quickstart/#generate-ssh-keys';
-export const WIKI_NEWS_MANAGEMENT: string = 'https://cloud.denbi.de/wiki/cloud_admin/#news-management';
-export const WIKI_SIMPLEVM_CUSTOMISATION: string = 'https://cloud.denbi.de/wiki/simple_vm/customization';
+export const WIKI_NEWS_MANAGEMENT: string = 'https://cloud.denbi.de/wiki/cloud_admin/news_management/';
+export const WIKI_SIMPLEVM_CUSTOMISATION: string = 'https://cloud.denbi.de/wiki/simple_vm/customization/';
 export const WIKI_EXTEND_VOLUME: string = 'https://cloud.denbi.de/wiki/simple_vm/volumes/#extend-a-volume';
 export const WIKI_VOLUME_OVERVIEW: string = 'https://cloud.denbi.de/wiki/simple_vm/volumes/';
 export const WIKI_RESENV_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/customization/#research-environments';
+export const WIKI_RSTUDIO_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/customization/#rstudio';
+export const WIKI_GUACAMOLE_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/customization/#apache-guacamole';
 export const WIKI_NEW_INSTANCE_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/new_instance/';
 export const WIKI_INSTANCE_OVERVIEW_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/instance_overview/';
 export const WIKI_INSTANCE_DETAIL_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/instance_detail/';
-export const WIKI_RSTUDIO_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/customization/#rstudio';
-export const WIKI_GUACAMOLE_LINK: string = 'https://cloud.denbi.de/wiki/simple_vm/customization/#apache-guacamole';
 export const WIKI_LINK_ACCOUNTS: string = 'https://cloud.denbi.de/wiki/portal/user_information/#link-accounts-to-elixir';
+export const WIKI_PUBLICATIONS: string = 'https://cloud.denbi.de/wiki/citation_and_publication/#publications';
+export const WIKI_MEMBER_MANAGEMENT: string = 'https://cloud.denbi.de/wiki/portal/project_overview/#member-management';
+export const WIKI_FAQ: string = 'https://cloud.denbi.de/wiki/FAQ/';
+export const WIKI_MOTD: string = 'https://cloud.denbi.de/wiki/cloud_admin/news_management/#message-of-the-day';
 export const WIKI_LNKS: string[] =
   [
-    WIKI_MOUNT_VOLUME,
-    WIKI_GROUP_INVITATIONS,
     WIKI_SNAPSHOTS,
     WIKI,
     WIKI_GENERATE_KEYS,
@@ -31,5 +31,9 @@ export const WIKI_LNKS: string[] =
     WIKI_NEW_INSTANCE_LINK,
     WIKI_INSTANCE_OVERVIEW_LINK,
     WIKI_INSTANCE_DETAIL_LINK,
-    WIKI_LINK_ACCOUNTS
+    WIKI_LINK_ACCOUNTS,
+    WIKI_PUBLICATIONS,
+    WIKI_MEMBER_MANAGEMENT,
+    WIKI_FAQ,
+    WIKI_MOTD
   ];
