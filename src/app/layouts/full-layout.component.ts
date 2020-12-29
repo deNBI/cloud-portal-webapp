4320c31eb5a3126ba28d12c7d21af45ab71907f6
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2020-12-29 11:20:32 +0100

    sorting loaded applications by project type

--- src/app/layouts/full-layout.component.ts
+++ src/app/layouts/full-layout.component.ts
@@ -100,6 +100,15 @@ export class FullLayoutComponent extends ApplicationBaseClassComponent implement
       this.project_enumeration.forEach((enumeration: ProjectEnumeration): void => {
         this.pushAdditionalStates(enumeration);
       });
+      this.project_enumeration.sort((a: ProjectEnumeration, b: ProjectEnumeration): number => {
+        if (a.is_open_stack && !b.is_open_stack){
+          return -1;
+        }
+        if (!a.is_open_stack && b.is_open_stack){
+          return 1;
+        }
+        return 0;
+      });
     });
   }
 
