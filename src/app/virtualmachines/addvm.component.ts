aa225972c8490e2ddacb1efeaf00a81bb9029c3f
dweinholz <dweinholz@users.noreply.github.com>
2021-02-09 09:32:44 +0000

    fix(Linting):blacked code

--- src/app/virtualmachines/addvm.component.ts
+++ src/app/virtualmachines/addvm.component.ts
@@ -560,7 +560,7 @@ export class VirtualMachineComponent implements OnInit, DoCheck {
     this.groupService.getClient(this.selectedProject[1].toString()).subscribe((client: Client): void => {
               this.loadProjectData();
 
-      if (client.status && client.status === 'Connected' && client.activated) {
+              if (client.status && client.status === 'Connected' && client.activated) {
         this.client_avaiable = true;
 
         this.client_checked = true;
@@ -570,8 +570,8 @@ export class VirtualMachineComponent implements OnInit, DoCheck {
         this.client_checked = true;
 
       }
-      this.selectedProjectClient = client;
-      this.imageService.getBlockedImageTagsResenv(Number(this.selectedProjectClient.id), 'true')
+              this.selectedProjectClient = client;
+              this.imageService.getBlockedImageTagsResenv(Number(this.selectedProjectClient.id), 'true')
         .subscribe((tags: BlockedImageTagResenv[]): void => {
           this.blockedImageTagsResenv = tags;
         });
