6471cae7971f82db2669f2d5b9bffff422773ec0
David Weinholz <david-weinholz@web.de>
2021-03-01 10:07:52 +0100

    added logger

--- e2e/log4jsGen.js
+++ e2e/log4jsGen.js
@@ -1,10 +1,11 @@
 'use strict';
 var log4js = require('log4js');
 log4js.configure({
-  appenders: [
-    {type: 'console'},
-    {type: 'file', filename: 'logs/cheese.log', category: 'logs'}
-  ]
+
+  appenders: {
+    fileLog: {type: 'file', filename: './logs/ExecutionLog.log'},
+    console: {type: 'log4js-protractor-appender'}
+  }
 });
 var log4jsGen = log4js.getLogger('logs');
 
