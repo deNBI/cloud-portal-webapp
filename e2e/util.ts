9eeb3f432793ea6fbb06f08d29e394c095252e4e
David Weinholz <david-weinholz@web.de>
2021-03-16 12:04:43 +0100

    fixed get text

--- e2e/util.ts
+++ e2e/util.ts
@@ -215,7 +215,7 @@ export class Util {
 
     await this.waitForVisibilityOfElementById(id, timeout);
 
-    return await element(by.id(id)).getText();
+    return await element(by.id(id)).getAttribute('textContent') ;
   }
 
   static async clickElementByElement(elem: any,
