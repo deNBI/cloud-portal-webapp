44c79da835c7954497cc64d0505ecf602d243661
dweinholz <david-weinholz@web.de>
2021-03-24 11:55:01 +0100

    Update protractor.conf.js
    changed order (so public keys will be always set for vm tests)

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -76,6 +76,7 @@ exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     'tests/terminate_all_pt.ts',
+    'tests/public_key_test.ts',
     'tests/simple_vm_application_test.ts',
     'tests/simple_vm_approval_test.ts',
     'tests/member_test.ts',
@@ -89,8 +90,6 @@ exports.config = {
     'tests/cloud_application_modification_test.ts',
     'tests/cloud_modification_approval_test.ts',
     'tests/virtual_machine_tests.ts',
-    'tests/terminate_all_pt.ts',
-    'tests/public_key_test.ts'
   ],
   capabilities: {
     browserName: 'chrome',
