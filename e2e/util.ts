b7b1708294ca06fb3d42d2b8c64917a7a551f64e
dweinholz <david-weinholz@web.de>
2021-04-06 09:52:34 +0200

    log error

--- e2e/util.ts
+++ e2e/util.ts
@@ -131,15 +131,15 @@ export class Util {
     await Util.waitForElementToBeClickableByLinkText(text)
     this.logInfo(`Clicking element with text: [${text}]`)
 
-    try {
+   // try {
       const location = await element(by.linkText(text)).getLocation()
       await browser.actions().mouseMove({x: location.x, y: location.y}).click().perform()
-    } catch (error) {
+   /* } catch (error) {
       this.logInfo(error)
       this.logInfo(`Coudln't click ${text} - Ignore Error`)
 
       return false
-    }
+    }*/
 
     return true
   }
