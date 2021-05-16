e61d36e4200fc0feaa7d8cf1568e28cb1379bd44
ekatchko <ewgenijk@gmx.de>
2021-05-16 13:45:56 +0200

    feat(credits): calculator to compute credits

---
+++ src/app/credits-calculator/credits-calculator.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreditsCalculatorComponent } from './credits-calculator.component';
+
+describe('CreditsCalculatorComponent', () => {
+  let component: CreditsCalculatorComponent;
+  let fixture: ComponentFixture<CreditsCalculatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreditsCalculatorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreditsCalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
