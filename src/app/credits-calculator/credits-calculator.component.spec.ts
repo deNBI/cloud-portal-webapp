5255f01913c70a6d387e48e4c1227f7678a740c1
denbicloud <denbicloud@users.noreply.github.com>
2021-05-17 06:37:52 +0000

    fix(Linting):blacked code

--- src/app/credits-calculator/credits-calculator.component.spec.ts
+++ src/app/credits-calculator/credits-calculator.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreditsCalculatorComponent } from './credits-calculator.component';
 
 describe('CreditsCalculatorComponent', () => {
-  let component: CreditsCalculatorComponent;
-  let fixture: ComponentFixture<CreditsCalculatorComponent>;
+	let component: CreditsCalculatorComponent;
+	let fixture: ComponentFixture<CreditsCalculatorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreditsCalculatorComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [CreditsCalculatorComponent],
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreditsCalculatorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CreditsCalculatorComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
