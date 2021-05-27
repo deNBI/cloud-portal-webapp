3d7e06b2aaf77cfd60d5eef00b328f9a28878e86
ekatchko <ewgenijk@gmx.de>
2021-05-27 19:30:19 +0200

    fix(cluster): possible cluster information timeout fix

--- src/app/virtualmachines/clusters/clusterinfo.ts
+++ src/app/virtualmachines/clusters/clusterinfo.ts
@@ -121,9 +121,9 @@ export class Clusterinfo {
 	}
 
 	private set_worker_baches(workerBatches: WorkerBatch[]): void {
-
-		this.worker_batches = workerBatches.map((workerBatch: WorkerBatch): WorkerBatch => new WorkerBatch(workerBatch.index, workerBatch));
-
+		this.worker_batches = workerBatches.map(
+			(workerBatch: WorkerBatch): WorkerBatch => new WorkerBatch(workerBatch.index, workerBatch),
+		);
 	}
 
 	private get_batches_count(): number {
