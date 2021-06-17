1310683a923fa6c8bb84586f73a50b464f5a02ec
eKatchko <47833533+eKatchko@users.noreply.github.com>
2021-06-18 01:05:04 +0200

    Revert "Dev"

--- src/app/api-connector/token-interceptor.ts
+++ src/app/api-connector/token-interceptor.ts
@@ -12,29 +12,11 @@ import { Cookie } from 'ng2-cookies';
 export class TokenInterceptor implements HttpInterceptor {
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		const skipIntercept: boolean = req.headers.has('skip');
-		const skipXRequestedWith: boolean = req.headers.has('skip-x-requested-with');
+		const modifiedReq: HttpRequest<any> = req.clone({
+			withCredentials: true,
+			headers: req.headers.set('X-CSRFToken', Cookie.get('csrftoken')),
+		});
 
-		if (skipIntercept) {
-			req = req.clone({
-				headers: req.headers.delete('skip'),
-			});
-
-			return next.handle(req);
-		} else if (skipXRequestedWith) {
-			const modifiedReq: HttpRequest<any> = req.clone({
-				withCredentials: true,
-				headers: req.headers.set('X-CSRFToken', Cookie.get('csrftoken')).delete('skip-x-requested-with'),
-			});
-
-			return next.handle(modifiedReq);
-		} else {
-			const modifiedReq: HttpRequest<any> = req.clone({
-				withCredentials: true,
-				headers: req.headers.set('X-CSRFToken', Cookie.get('csrftoken')).set('X-Requested-With', 'XMLHttpRequest'),
-			});
-
-			return next.handle(modifiedReq);
-		}
+		return next.handle(modifiedReq);
 	}
 }
