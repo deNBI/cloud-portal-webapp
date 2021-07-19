52336c79fa99b8c54870f6f38107fbf52324e1f9
dweinholz <david-weinholz@web.de>
2021-07-19 16:19:47 +0200

    updated some components

--- src/app/shared/title-headbar.component.ts
+++ src/app/shared/title-headbar.component.ts
@@ -5,14 +5,17 @@ import { UserService } from '../api-connector/user.service';
  * Title headbar component.
  */
 @Component({
-	templateUrl: 'title-headbar.component.html',
-	selector: 'app-title-headbar',
-	providers: [UserService],
+	           templateUrl: 'title-headbar.component.html',
+	           selector: 'app-title-headbar',
+	           providers: [UserService],
 
 })
 export class TitleHeadbarComponent {
 
 	@Input() page_title: string;
+	@Input() navbar_minimized: boolean;
+	brand_logo: string = 'static/webapp/assets/img/denbi-logo-color.svg';
+	brand_logo_minimized: string = 'static/webapp/assets/img/denbi-logo-minimized.svg';
 
 	constructor(private userService: UserService) {
 		this.userService = userService;
