fdd6b61fc8ac3f97e5f1737a505b2dc372971953
dweinholz <david-weinholz@web.de>
2021-08-19 10:48:27 +0200

    merged

--- src/app/virtualmachines/clusters/clusterstatus/clusterstatus.component.ts
+++ src/app/virtualmachines/clusters/clusterstatus/clusterstatus.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { Clusterinfo } from '../clusterinfo';
-import { VirtualMachineStates } from '../../virtualmachinemodels/virtualmachinestates';
+import {Component, Input} from '@angular/core';
+import {Clusterinfo} from '../clusterinfo';
+import {VirtualMachineStates} from '../../virtualmachinemodels/virtualmachinestates';
 
 /**
  * Clusterstatus component.
  */
 @Component({
-	selector: 'app-clusterstatus',
-	templateUrl: './clusterstatus.component.html',
-})
+	           selector: 'app-clusterstatus',
+	           templateUrl: './clusterstatus.component.html',
+           })
 export class ClusterstatusComponent {
-  @Input() cluster: Clusterinfo;
-  VirtualMachineStates: VirtualMachineStates = new VirtualMachineStates();
+	@Input() cluster: Clusterinfo;
+	@Input() show_text_status: boolean = true;
+	VirtualMachineStates: VirtualMachineStates = new VirtualMachineStates();
 
 }
