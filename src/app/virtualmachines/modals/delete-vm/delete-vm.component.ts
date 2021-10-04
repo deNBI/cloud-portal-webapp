96d74cfa7676d7c1af17bbda50183ba95286c182
nholsmoelle <nina.holsmoelle@uni-bielefeld.de>
2021-10-04 14:12:05 +0200

    feat(DeleteVM-Modal): Added checkbox for not-user-owned VM

--- src/app/virtualmachines/modals/delete-vm/delete-vm.component.ts
+++ src/app/virtualmachines/modals/delete-vm/delete-vm.component.ts
@@ -2,7 +2,9 @@ import {
 	Component, EventEmitter, OnDestroy,
 } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
-import { VirtualMachine } from '../../virtualmachinemodels/virtualmachine';
+import { VirtualMachine } from "../../virtualmachinemodels/virtualmachine";
+// import { User} from "../../../applications/application.model/user.model";
+import { elixir_id } from "../../../shared/globalvar";
 
 @Component({
 	selector: 'app-delete-vm',
@@ -13,6 +15,14 @@ export class DeleteVmComponent implements OnDestroy {
 	virtualMachine: VirtualMachine;
 	public event: EventEmitter<any> = new EventEmitter();
 	private submitted: boolean = false;
+
+	elixir_id: string = elixir_id;
+
+	/**
+	 * To check if the user agreed to deleting someone else's VM
+	 */
+	delete_foreign_vm: boolean = false;
+
 
 	constructor(public bsModalRef: BsModalRef) {
 		// eslint-disable-next-line no-empty-function
