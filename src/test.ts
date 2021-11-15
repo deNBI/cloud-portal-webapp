0c1af8a654eaed2a294c05fb03a1772839e5032f
dweinholz <david-weinholz@web.de>
2021-11-15 11:16:02 +0100

    update angular13

--- src/test.ts
+++ src/test.ts
@@ -26,8 +26,8 @@ __karma__.loaded = function () {
 getTestBed().initTestEnvironment(
 	BrowserDynamicTestingModule,
 	platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-},
+		teardown: { destroyAfterEach: false },
+	},
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
