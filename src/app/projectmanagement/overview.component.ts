95fb83b6681f265e943f6dbb362f907dcde744d0
nholsmoelle <51156179+nholsmoelle@users.noreply.github.com>
2021-12-09 10:35:28 +0100

    feat(ProjectOverview): Added leave project button (#4062)
    * feat(gui): Added button to toggle project member name visibility
    
    * feat(model): Added BooleanField for project member names visibility
    
    * fix(workshopManagement): Added notification if there are no workshops for a project
    
    * fix(readme): Typo
    
    * feat(Project Overview): Added possibility to switch member visibility in projects for non-admins
    
    * fix(Linting):blacked code
    
    * typo
    
    * clarification
    
    * fix(ProjectOverview): Disable member name visibility toggle button until request executed
    
    * clean-up
    
    * typo
    
    * Added leave_project button to GUI
    
    * fix(ProjectOverview): Only requesting and showing project member information for admins or in case member names should be visible
    
    * feat(Status): Added link to cloud status page
    
    * moved button to the bottom
    
    * fix(Linting):blacked code (#3948)
    
    Co-authored-by: vktrrdk <vktrrdk@users.noreply.github.com>
    
    * Centered button
    
    * Centered leave project button
    
    * Adjusted request
    
    * fixed typescript import
    
    * handle leaving with reload of enumerations
    
    * Typo
    
    * fix(Linting):blacked code
    
    * Clean-up
    
    * Clean-up
    
    * PIs and admins cannot leave project
    
    * Added package.json and package-lock.json
    
    * fix(Linting):blacked code (#4061)
    
    Co-authored-by: nholsmoelle <nholsmoelle@users.noreply.github.com>
    
    * linting
    
    * updated package.json and package-lock.json to dev state
    
    Co-authored-by: nholsmoelle <nina.holsmoelle@uni-bielefeld.de>
    Co-authored-by: vktrrdk <vrudko@techfak.uni-bielefeld.de>
    Co-authored-by: vktrrdk <vktrrdk@users.noreply.github.com>
    Co-authored-by: dweinholz <david-weinholz@web.de>
    Co-authored-by: denbicloud <46009071+denbicloud@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: denbicloud <denbicloud@users.noreply.github.com>
    Co-authored-by: Viktor <43116821+vktrrdk@users.noreply.github.com>
    Co-authored-by: nholsmoelle <nholsmoelle@users.noreply.github.com>

--- src/app/projectmanagement/overview.component.ts
+++ src/app/projectmanagement/overview.component.ts
@@ -1077,6 +1077,68 @@ private flavorService: FlavorService,
 	}
 
 	/**
+	 * Leave a project
+	 *
+	 * @param groupid  of the group
+	 * @param memberid of the member
+	 * @param projectname of the project
+	 */
+	public leaveProject(groupid: number, memberid: number, projectname: string): void {
+		if (this.project_application.project_application_pi.elixir_id === this.userinfo.ElixirId) {
+			this.updateNotificationModal(
+				'Denied',
+				'You cannot leave projects as PI.',
+				true,
+				'danger',
+			);
+		} else if (this.project.UserIsAdmin) {
+			// TODO: Allow admins to leave project if there is at least 1 other admin
+			this.updateNotificationModal(
+				'Denied',
+				'You cannot leave projects as admin.',
+				true,
+				'danger',
+			);
+		} else {
+			console.log('removing member');
+			this.subscription.add(
+				this.groupService.leaveGroup(groupid, memberid, this.project.ComputeCenter.FacilityId).subscribe(
+					(result: any): void => {
+
+						if (result.status === 200) {
+							this.updateNotificationModal(
+								'Success',
+								`You were removed from the project ${projectname}`,
+								true,
+								'success',
+							);
+							void this.router.navigate(['/userinfo']);
+							this.fullLayout.getGroupsEnumeration();
+
+						} else {
+							this.updateNotificationModal(
+								'Failed',
+								`Failed to leave the project ${projectname}!`,
+								true,
+								'danger',
+							);
+						}
+					},
+					(): void => {
+						this.updateNotificationModal(
+							'Failed',
+							`Failed to leave the project ${projectname}!`,
+							true,
+							'danger',
+						);
+					},
+				),
+			);
+		}
+
+	}
+
+	/**
 	 * Delete an application.
 	 *
 	 * @param application_id
