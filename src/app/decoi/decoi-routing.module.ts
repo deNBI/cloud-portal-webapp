38f7e8490fc2258dd1093d3ea1a38ae0d551f914
dweinholz <dweinholz@users.noreply.github.com>
2022-01-12 15:34:36 +0000

    fix(Linting):blacked code

--- src/app/decoi/decoi-routing.module.ts
+++ src/app/decoi/decoi-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {DecoiUploadComponent} from './decoi-upload/decoi-upload.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DecoiUploadComponent } from './decoi-upload/decoi-upload.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DecoiUploadComponent,
-    data: {
-      title: 'DeCoi Upload File'
-    }
+	{
+		path: '',
+		component: DecoiUploadComponent,
+		data: {
+			title: 'DeCoi Upload File',
+		},
 
-  }
+	},
 ];
 
 /**
  * Routing module for decoi
  */
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class DecoiRoutingModule { }
--- src/app/decoi/model/metadata.model.ts
+++ src/app/decoi/model/metadata.model.ts
@@ -1,19 +1,19 @@
-import {Multipart} from './multipart.model';
+import { Multipart } from './multipart.model';
 
 /**
  *  Model for multipart uploads
  */
 export class MetadataModel {
 
-  IMS_ID: string;
-  SENDING_LAB: string;
-  DATE_DRAW: string;
-  SEQ_TYPE: string;
-  SEQ_REASON: string;
-  SAMPLE_TYPE: string;
-  PUBLICATION_STATUS: string;
-  OWN_FASTA_ID: string;
-  FILE_NAME: string;
-  upload: Multipart;
+	IMS_ID: string;
+	SENDING_LAB: string;
+	DATE_DRAW: string;
+	SEQ_TYPE: string;
+	SEQ_REASON: string;
+	SAMPLE_TYPE: string;
+	PUBLICATION_STATUS: string;
+	OWN_FASTA_ID: string;
+	FILE_NAME: string;
+	upload: Multipart;
 
 }
