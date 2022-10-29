73189d96f980e3e07a6cfaf34f478757f2b6b2f5
dweinholz <david-weinholz@web.de>
2022-10-29 12:35:21 +0200

    fix(Vm):able to start vms again without resenvs

--- src/app/virtualmachines/conda/res-env.component.ts
+++ src/app/virtualmachines/conda/res-env.component.ts
@@ -1,175 +1,179 @@
 import {
-	Component, Input, OnChanges, OnDestroy, OnInit,
+		Component, Input, OnChanges, OnDestroy, OnInit,
 } from '@angular/core';
-import { UntypedFormControl, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { BiocondaService } from '../../api-connector/bioconda.service';
-import { ResearchEnvironment } from '../virtualmachinemodels/res-env';
-import { RandomNameGenerator } from '../../shared/randomNameGenerator';
-import { WIKI_RESENV_LINK, CLOUD_PORTAL_SUPPORT_MAIL } from '../../../links/links';
-import { BlockedImageTagResenv } from '../../facility_manager/image-tag';
+import {UntypedFormControl, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {BiocondaService} from '../../api-connector/bioconda.service';
+import {ResearchEnvironment} from '../virtualmachinemodels/res-env';
+import {RandomNameGenerator} from '../../shared/randomNameGenerator';
+import {WIKI_RESENV_LINK, CLOUD_PORTAL_SUPPORT_MAIL} from '../../../links/links';
+import {BlockedImageTagResenv} from '../../facility_manager/image-tag';
 
 /**
  * ResEnv.
  */
 @Component({
-	selector: 'app-res-env',
-	templateUrl: 'res-env.component.html',
-	providers: [BiocondaService],
+		selector: 'app-res-env',
+		templateUrl: 'res-env.component.html',
+		providers: [BiocondaService],
 })
 export class ResEnvComponent implements OnInit, OnChanges, OnDestroy {
-	@Input() clientid: string;
-	@Input() forc_url: string;
-	@Input() onlyNamespace: boolean = false;
-	@Input() imageName: string = '';
-	@Input() selectedImageTags: string[] = [];
-	@Input() blockedImageTagsResenv: BlockedImageTagResenv[];
-	@Input() workshopMode: boolean = false;
+		@Input() clientid: string;
+		@Input() forc_url: string;
+		@Input() onlyNamespace: boolean = false;
+		@Input() imageName: string = '';
+		@Input() selectedImageTags: string[] = [];
+		@Input() blockedImageTagsResenv: BlockedImageTagResenv[];
+		@Input() workshopMode: boolean = false;
 
-	Object: Object = Object;
+		Object: Object = Object;
 
-	templates_to_block: string[] = [];
+		templates_to_block: string[] = [];
 
-	user_key_url: UntypedFormControl = new UntypedFormControl('', [
-		Validators.required,
-		Validators.pattern('[a-zA-Z]{3,20}'),
-	]);
+		user_key_url: UntypedFormControl = new UntypedFormControl('', [
+				Validators.required,
+				Validators.pattern('[a-zA-Z]{3,20}'),
+		]);
 
-	selectedTemplate: ResearchEnvironment | undefined = undefined;
+		selectedTemplate: ResearchEnvironment | undefined = undefined;
 
-	templates: ResearchEnvironment[] = [];
+		templates: ResearchEnvironment[] = [];
 
-	undefinedTemplate: ResearchEnvironment = new ResearchEnvironment();
+		undefinedTemplate: ResearchEnvironment = new ResearchEnvironment();
 
-	rng: RandomNameGenerator;
+		rng: RandomNameGenerator;
 
-	WIKI_RESENV_LINK: string = WIKI_RESENV_LINK;
-	CLOUD_PORTAL_SUPPORT_MAIL: string = CLOUD_PORTAL_SUPPORT_MAIL;
+		WIKI_RESENV_LINK: string = WIKI_RESENV_LINK;
+		CLOUD_PORTAL_SUPPORT_MAIL: string = CLOUD_PORTAL_SUPPORT_MAIL;
 
-	create_only_backend: boolean = false;
+		create_only_backend: boolean = false;
 
-	subscription: Subscription = new Subscription();
+		subscription: Subscription = new Subscription();
 
-	constructor(private condaService: BiocondaService) {
-		this.condaService = condaService;
-	}
-
-	setUserKeyUrl(url: string): void {
-		this.user_key_url.setValue(url);
-	}
-
-	getUserKeyUrl(): string {
-		return this.user_key_url.value;
-	}
-
-	getCreateOnlyBackend(): boolean {
-		return this.create_only_backend;
-	}
-
-	setSelectedTemplate(template: ResearchEnvironment): void {
-		if (template === null) {
-			this.selectedTemplate = this.undefinedTemplate;
-			this.user_key_url.setValue('');
-
-			return;
+		constructor(private condaService: BiocondaService) {
+				this.condaService = condaService;
 		}
-		this.selectedTemplate = template;
-	}
 
-	ngOnInit(): void {
-		this.undefinedTemplate.template_name = 'undefined';
-		this.templates_to_block = [];
-		this.setSelectedTemplate(null);
-		this.subscription.add(
-			this.condaService.getForcTemplates(this.clientid).subscribe((templates: ResearchEnvironment[]): void => {
-				this.templates = templates;
-			}),
-		);
-		this.rng = new RandomNameGenerator();
-	}
+		setUserKeyUrl(url: string): void {
 
-	ngOnDestroy() {
-		this.subscription.unsubscribe();
-	}
+				if ( this.needsName()) {
+						this.user_key_url.setValue(url);
+				}
+		}
 
-	ngOnChanges(): void {
-		this.checkBlocked();
-	}
 
-	isValid(): boolean {
-		if (this.onlyNamespace) {
-			if (this.workshopMode) {
-				return true;
-			} else {
-				return this.user_key_url.errors === null;
-			}
-		} else if (this.selectedTemplate.template_name === 'undefined') {
-			if (this.workshopMode) {
+		getUserKeyUrl(): string {
+				return this.user_key_url.value;
+		}
+
+		getCreateOnlyBackend(): boolean {
+				return this.create_only_backend;
+		}
+
+		setSelectedTemplate(template: ResearchEnvironment): void {
+				if (template === null) {
+						this.selectedTemplate = this.undefinedTemplate;
+						this.user_key_url.setValue('');
+
+						return;
+				}
+				this.selectedTemplate = template;
+		}
+
+		ngOnInit(): void {
+				this.undefinedTemplate.template_name = 'undefined';
+				this.templates_to_block = [];
+				this.setSelectedTemplate(null);
+				this.subscription.add(
+						this.condaService.getForcTemplates(this.clientid).subscribe((templates: ResearchEnvironment[]): void => {
+								this.templates = templates;
+						}),
+				);
+				this.rng = new RandomNameGenerator();
+		}
+
+		ngOnDestroy() {
+				this.subscription.unsubscribe();
+		}
+
+		ngOnChanges(): void {
+				this.checkBlocked();
+		}
+
+		isValid(): boolean {
+				if (this.onlyNamespace) {
+						if (this.workshopMode) {
+								return true;
+						} else {
+								return this.user_key_url.errors === null;
+						}
+				} else if (this.selectedTemplate.template_name === 'undefined') {
+						if (this.workshopMode) {
+								return false;
+						} else {
+								return this.user_key_url.value.length === 0;
+						}
+				} else if (this.workshopMode) {
+						return true;
+				} else {
+						return this.user_key_url.errors === null;
+				}
+		}
+
+		needsName(): boolean {
+				if (this.onlyNamespace || this.selectedTemplate.template_name !== 'undefined') {
+						return this.user_key_url.errors !== null;
+				}
+
 				return false;
-			} else {
-				return this.user_key_url.value.length === 0;
-			}
-		} else if (this.workshopMode) {
-			return true;
-		} else {
-			return this.user_key_url.errors === null;
-		}
-	}
-
-	needsName(): boolean {
-		if (this.onlyNamespace || this.selectedTemplate.template_name !== 'undefined') {
-			return this.user_key_url.errors !== null;
 		}
 
-		return false;
-	}
+		needsTemplate(): boolean {
+				if (this.workshopMode && this.selectedTemplate.template_name === 'undefined') {
+						return true;
+				} else if (this.user_key_url.value.length !== 0 && !this.onlyNamespace) {
+						return this.selectedTemplate.template_name === 'undefined';
+				}
 
-	needsTemplate(): boolean {
-		if (this.workshopMode && this.selectedTemplate.template_name === 'undefined') {
-			return true;
-		} else if (this.user_key_url.value.length !== 0 && !this.onlyNamespace) {
-			return this.selectedTemplate.template_name === 'undefined';
+				return false;
 		}
 
-		return false;
-	}
-
-	okayNeeded(): boolean {
-		return this.selectedTemplate.template_name !== 'undefined';
-	}
-
-	setOnlyNamespace(template): void {
-		this.onlyNamespace = true;
-		this.create_only_backend = true;
-		this.setSelectedTemplate(template);
-	}
-
-	unsetOnlyNamespace(): void {
-		this.onlyNamespace = false;
-		this.user_key_url.setValue('');
-		this.setSelectedTemplate(null);
-	}
-
-	generateRandomName(): void {
-		this.user_key_url.setValue(this.rng.randomName());
-	}
-
-	checkBlocked(): void {
-		if (this.selectedImageTags === null || this.selectedImageTags === undefined) {
-			return;
+		okayNeeded(): boolean {
+				return this.selectedTemplate.template_name !== 'undefined';
 		}
-		for (const blockedTag of this.blockedImageTagsResenv) {
-			if (this.selectedImageTags.indexOf(blockedTag.tag) !== -1) {
-				this.templates_to_block = blockedTag.resenvs;
 
-				return;
-			}
+		setOnlyNamespace(template): void {
+				this.onlyNamespace = true;
+				this.create_only_backend = true;
+				this.setSelectedTemplate(template);
 		}
-		this.templates_to_block = [];
-	}
 
-	resetData(): void {
-		this.setSelectedTemplate(null);
-		this.create_only_backend = false;
-	}
+		unsetOnlyNamespace(): void {
+				this.onlyNamespace = false;
+				this.user_key_url.setValue('');
+				this.setSelectedTemplate(null);
+		}
+
+		generateRandomName(): void {
+				this.user_key_url.setValue(this.rng.randomName());
+		}
+
+		checkBlocked(): void {
+				if (this.selectedImageTags === null || this.selectedImageTags === undefined) {
+						return;
+				}
+				for (const blockedTag of this.blockedImageTagsResenv) {
+						if (this.selectedImageTags.indexOf(blockedTag.tag) !== -1) {
+								this.templates_to_block = blockedTag.resenvs;
+
+								return;
+						}
+				}
+				this.templates_to_block = [];
+		}
+
+		resetData(): void {
+				this.setSelectedTemplate(null);
+				this.create_only_backend = false;
+		}
 }
