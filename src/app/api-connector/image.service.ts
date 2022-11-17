cd56cfbb89a18882c94e1685b15964ef3da957c4
dweinholz <david-weinholz@web.de>
2022-11-17 13:29:29 +0100

    feat(Cluster):changed base images etc.

--- src/app/api-connector/image.service.ts
+++ src/app/api-connector/image.service.ts
@@ -17,6 +17,9 @@ import { ImageTypes } from '../virtualmachines/virtualmachinemodels/imageTypes';
  */
 @Injectable()
 export class ImageService {
+	BASE_IMAGE_TAG = 'base_image';
+	BASE_CLUSTER_IMAGE_TAG = 'base_cluster';
+
 	constructor(private http: HttpClient) {
 		this.http = http;
 	}
@@ -235,12 +238,15 @@ export class ImageService {
 		image_types[ImageTypes.IMAGE] = [];
 		image_types[ImageTypes.SNAPSHOT] = [];
 		image_types[ImageTypes.CUSTOM] = [];
+		image_types[ImageTypes.CLUSTER_IMAGE] = [];
 
 		for (const image of images) {
 			if (image.is_snapshot) {
 				image_types[ImageTypes.SNAPSHOT].push(image);
-			} else if (image.tags.includes('base_image')) {
+			} else if (image.tags.includes(this.BASE_IMAGE_TAG)) {
 				image_types[ImageTypes.IMAGE].push(image);
+			} else if (image.tags.includes(this.BASE_CLUSTER_IMAGE_TAG)) {
+				image_types[ImageTypes.CLUSTER_IMAGE].push(image);
 			} else {
 				image_types[ImageTypes.CUSTOM].push(image);
 			}
