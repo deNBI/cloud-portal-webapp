9fde09bd1461ac34ad51d1bd562f5dc81d5082aa
dweinholz <david-weinholz@web.de>
2022-12-07 10:25:53 +0100

    refactor image

--- src/app/virtualmachines/imagedetail.component.ts
+++ src/app/virtualmachines/imagedetail.component.ts
@@ -1,29 +1,36 @@
 import {
-	Component, EventEmitter, HostListener, Input, OnInit, Output,
+	Component, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output,
 } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { forkJoin, Subscription } from 'rxjs';
 import { Image } from './virtualmachinemodels/image';
 import { ImageService } from '../api-connector/image.service';
 import { ImageTypes } from './virtualmachinemodels/imageTypes';
 import { Flavor } from './virtualmachinemodels/flavor';
+import { BiocondaService } from '../api-connector/bioconda.service';
+import { Client } from '../vo_manager/clients/client.model';
 
 /**
  * Imagedetail component.
  */
 @Component({
-	selector: 'app-image-detail',
+	selector: 'app-image-detail[client][project_id]',
 	templateUrl: 'imagedetail.component.html',
 	styleUrls: ['./imagedetail.component.scss'],
-	providers: [ImageService],
+	providers: [ImageService, BiocondaService],
 })
-export class ImageDetailComponent implements OnInit {
+export class ImageDetailComponent implements OnInit, OnDestroy {
 	@Input() selectedImage: Image;
-	@Input() images: Image[];
-	@Input() resenv_names: string[];
 	@Input() selectedFlavor: Flavor;
 	@Output() readonly selectedImageChange: EventEmitter<Image> = new EventEmitter();
 	@Input() isCluster: boolean = false;
+	@Input() client: Client;
+	@Input() project_id: number;
+	images_loaded: boolean = false;
+	resenv_names: string[];
+	images: Image[] = [];
 
+	subscription: Subscription = new Subscription();
 	regexp_data_test_id: RegExp = /[ ().]/g;
 	carousel_activated: boolean = true;
 	images_per_row: number = 4;
@@ -67,7 +74,7 @@ export class ImageDetailComponent implements OnInit {
 		nav: true,
 	};
 
-	constructor(private imageService: ImageService) {
+	constructor(private imageService: ImageService, private condaService: BiocondaService) {
 		// eslint-disable-next-line no-empty-function
 	}
 
@@ -78,8 +85,25 @@ export class ImageDetailComponent implements OnInit {
 			this.selected_image_type = ImageTypes.IMAGE;
 		}
 		this.window_size = window.innerWidth;
-		this.image_types = this.imageService.sortImages(this.images, this.resenv_names);
-		this.image_selection = this.image_types[this.selected_image_type];
+		this.prepareImages();
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
+
+	prepareImages(): void {
+		this.images_loaded = false;
+		forkJoin([
+			this.condaService.getForcTemplates(this.client.id),
+			this.imageService.getImages(this.project_id),
+		]).subscribe((res: any) => {
+			res[0].forEach(resenv => this.resenv_names.push(resenv.template_name));
+			this.images = res[1];
+			this.image_types = this.imageService.sortImages(this.images, this.resenv_names);
+			this.image_selection = this.image_types[this.selected_image_type];
+			this.images_loaded = true;
+		});
 	}
 
 	setSelectedImageType(key: string): void {
