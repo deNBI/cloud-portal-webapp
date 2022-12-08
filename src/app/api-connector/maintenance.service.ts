6e3556497fbacdbfcdeb5aa2be2a261ff8703b2b
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2022-12-08 16:27:24 +0100

    feat(Maintenance): Show maintenance timeframes to user

--- src/app/api-connector/maintenance.service.ts
+++ src/app/api-connector/maintenance.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
-import { ApiSettings } from './api-settings.service'
-import { MaintenanceTimeFrame } from '../vo_manager/maintenance/maintenanceTimeFrame.model'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiSettings } from './api-settings.service';
+import { MaintenanceTimeFrame } from '../vo_manager/maintenance/maintenanceTimeFrame.model';
 
 /**
  * Service which provides the newest maintenance timeframes, e.g. to show in userInformation
@@ -11,22 +11,21 @@ import { MaintenanceTimeFrame } from '../vo_manager/maintenance/maintenanceTimeF
 @Injectable()
 export class MaintenanceService {
 	constructor(private http: HttpClient) {
-		this.http = http
+		this.http = http;
 	}
 
 	/**
 	 * returns the upcoming maintenance timeframes
 	 */
 	getFutureMaintenanceTimeFrames(): Observable<MaintenanceTimeFrame[]> {
-		//TODO adjust timeframe endpoint and add to api
 		return this.http
 			.get<MaintenanceTimeFrame[]>(`${ApiSettings.getApiBaseURL()}maintenance/`, {
 				withCredentials: true,
 			})
 			.pipe(
-				map((timeframes: MaintenanceTimeFrame[]): MaintenanceTimeFrame[] =>
-					timeframes.map((timeframe: MaintenanceTimeFrame): MaintenanceTimeFrame => new MaintenanceTimeFrame(timeframe))
-				)
-			)
+				map((maintenanceTimeFrames: MaintenanceTimeFrame[]): MaintenanceTimeFrame[] => maintenanceTimeFrames.map(
+					(maintenanceTimeFrame: MaintenanceTimeFrame): MaintenanceTimeFrame => new MaintenanceTimeFrame(maintenanceTimeFrame),
+				)),
+			);
 	}
 }
--- src/app/maintenance/maintenance-alert.component.ts
+++ src/app/maintenance/maintenance-alert.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core'
-import { Subscription } from 'rxjs'
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
-import { MaintenanceTimeFrame } from '../vo_manager/maintenance/maintenanceTimeFrame.model'
-import { MaintenanceService } from '../api-connector/maintenance.service'
+import { MaintenanceTimeFrame } from '../vo_manager/maintenance/maintenanceTimeFrame.model';
+import { MaintenanceService } from '../api-connector/maintenance.service';
 
 @Component({
 	selector: 'app-maintenance-alert',
@@ -11,34 +11,37 @@ import { MaintenanceService } from '../api-connector/maintenance.service'
 	providers: [MaintenanceService],
 })
 export class MaintenanceAlertComponent implements OnInit, OnDestroy {
-	subscription: Subscription = new Subscription()
-	maintenanceTimeFrames: MaintenanceTimeFrame[]
-	frames_loaded: boolean = false
-	error_on_loading: boolean = false
+	subscription: Subscription = new Subscription();
+	maintenanceTimeFrames: MaintenanceTimeFrame[];
+	frames_loaded: boolean = false;
+	error_on_loading: boolean = false;
 
 	constructor(private maintenanceService: MaintenanceService) {
 		// eslint-disable-next-line no-empty-function
 	}
 
 	ngOnInit(): void {
-		this.subscription = new Subscription()
+		this.subscription = new Subscription();
+		this.get_timeframes();
 	}
 
 	ngOnDestroy(): void {
-		this.subscription.unsubscribe()
+		this.subscription.unsubscribe();
 	}
 
 	get_timeframes(): void {
 		this.subscription.add(
 			this.maintenanceService.getFutureMaintenanceTimeFrames().subscribe({
 				next: (mtf: MaintenanceTimeFrame[]) => {
-					this.maintenanceTimeFrames = mtf
-					this.error_on_loading = false
+					this.maintenanceTimeFrames = mtf;
+					this.error_on_loading = false;
+					this.frames_loaded = true;
 				},
 				error: () => {
-					this.error_on_loading = true
+					this.error_on_loading = true;
+					this.frames_loaded = true;
 				},
-			})
-		)
+			}),
+		);
 	}
 }
