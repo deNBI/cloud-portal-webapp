509ad3e4238cd827f5c071446436743775e7fbfd
dweinholz <david-weinholz@web.de>
2023-01-16 09:24:31 +0100

    fix(PublicKey):will now be validated by the api

--- src/app/api-connector/key.service.ts
+++ src/app/api-connector/key.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { ApiSettings } from './api-settings.service';
-import { IResponseTemplate } from './response-template';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ApiSettings} from './api-settings.service';
+import {IResponseTemplate} from './response-template';
 
 /**
  * Service which provides public key methods.
@@ -10,32 +10,40 @@ import { IResponseTemplate } from './response-template';
 @Injectable()
 export class KeyService {
 
-	constructor(private http: HttpClient) {
-		this.http = http;
-	}
+		constructor(private http: HttpClient) {
+				this.http = http;
+		}
 
-	getKey(): Observable<IResponseTemplate> {
+		getKey(): Observable<IResponseTemplate> {
 
-		return this.http.get<IResponseTemplate>(`${ApiSettings.getApiBaseURL()}users/current/public_key/`, {
-			withCredentials: true,
-		});
+				return this.http.get<IResponseTemplate>(`${ApiSettings.getApiBaseURL()}users/current/public_key/`, {
+						withCredentials: true,
+				});
 
-	}
+		}
 
-	postKey(public_key_param: string): Observable<IResponseTemplate> {
-		const public_key: string = public_key_param.replace(/\r?\n|\r/gi, '');
-		const params: HttpParams = new HttpParams().set('public_key', public_key);
+		postKey(public_key_param: string): Observable<IResponseTemplate> {
+				const public_key: string = public_key_param.replace(/\r?\n|\r/gi, '');
+				const params: HttpParams = new HttpParams().set('public_key', public_key);
 
-		return this.http.put<IResponseTemplate>(`${ApiSettings.getApiBaseURL()}users/current/public_key/`, params, {
-			withCredentials: true,
-		});
-	}
+				return this.http.put<IResponseTemplate>(`${ApiSettings.getApiBaseURL()}users/current/public_key/`, params, {
+						withCredentials: true,
+				});
+		}
 
-	generateKey(): Observable<any> {
 
-		return this.http.post<any>(`${ApiSettings.getApiBaseURL()}users/current/public_key/generate/`, {
-			withCredentials: true,
-		});
-	}
+		validateKey(public_key_param: string): Observable<IResponseTemplate> {
+
+				return this.http.post<IResponseTemplate>(`${ApiSettings.getApiBaseURL()}users/current/public_key/validate/`, {'public_key': public_key_param}, {
+						withCredentials: true,
+				});
+		}
+
+		generateKey(): Observable<any> {
+
+				return this.http.post<any>(`${ApiSettings.getApiBaseURL()}users/current/public_key/generate/`, {
+						withCredentials: true,
+				});
+		}
 
 }
