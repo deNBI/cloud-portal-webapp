01780d918e71819aac0abdcff514e12cca7595f6
dweinholz <david-weinholz@web.de>
2023-03-01 09:50:27 +0100

    test

--- src/app/member-guard.service.ts
+++ src/app/member-guard.service.ts
@@ -1,93 +1,77 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
-	ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree,
+		ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree,
 } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
-import { HttpClient } from '@angular/common/http';
-import { map, switchMap } from 'rxjs/operators';
-import { now } from 'moment';
-import { UserService } from './api-connector/user.service';
-import { environment } from '../environments/environment';
-import { IResponseTemplate } from './api-connector/response-template';
-import { VoService } from './api-connector/vo.service';
-import { setElixirId, setVO } from './shared/globalvar';
+import {Observable} from 'rxjs';
+import {CookieService} from 'ngx-cookie-service';
+import {HttpClient} from '@angular/common/http';
+import {map, switchMap} from 'rxjs/operators';
+import {now} from 'moment';
+import {UserService} from './api-connector/user.service';
+import {environment} from '../environments/environment';
+import {IResponseTemplate} from './api-connector/response-template';
+import {VoService} from './api-connector/vo.service';
+import {setElixirId, setVO} from './shared/globalvar';
 
 /**
- * Guard which checks if the user is member of the vo.
+ * Guard which checks if the user is member of the VO.
  */
 @Injectable()
 export class MemberGuardService implements CanActivate {
-	constructor(
-		private http: HttpClient,
-		private cookieService: CookieService,
-		private router: Router,
-		private userService: UserService,
-		private voService: VoService,
-	) {
-		// constructor for MemberGuardService
-	}
 
-	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | boolean {
-		let cookieValue: string = null;
-
-		cookieValue = this.cookieService.get('redirect_after_login');
-		this.cookieService.delete('redirect_after_login');
-		this.cookieService.delete('redirect_after_login', '/');
-		this.cookieService.delete('redirect_after_login', '/', environment.domain);
-		this.cookieService.delete('redirect_after_login', '/portal', environment.domain);
-
-		if (this.cookieService.check('redirect_after_login')) {
-			this.cookieService.delete('redirect_after_login', '/', environment.domain);
-			this.cookieService.set('redirect_after_login', null, now(), '/', environment.domain);
-			this.cookieService.set('redirect_after_login', null, now(), '/portal', environment.domain);
+		constructor(
+				private http: HttpClient,
+				private cookieService: CookieService,
+				private router: Router,
+				private userService: UserService,
+				private voService: VoService,
+		) {
 		}
 
-		let redirect_url: string = state.url;
-		if (cookieValue) {
-			redirect_url = null;
-		}
+		canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | boolean {
 
-		return this.userService.getOnlyLoggedUserWithRedirect(redirect_url).pipe(
-			switchMap((res: any): Observable<any> => {
-				if (res['error']) {
-					window.location.href = environment.login;
-					const subject: Subject<boolean> = new Subject<boolean>();
-					subject.next(false);
+				const redirectUrl = state.url;
+				const cookieValue = this.cookieService.get('redirect_after_login');
+				console.debug(this.cookieService.getAll())
 
-					return subject.asObservable();
-				} else {
-					this.voService.isVo().subscribe((result: IResponseTemplate): void => {
-						setVO(result.value as boolean);
-					});
-					this.userService.getLoggedUserElixirId().subscribe((result: any): void => {
-						setElixirId(result['elixir_id']);
-					});
 
-					return this.userService.getIsCurrentUserVoMember().pipe(
-						map((memberinfo: any): any => {
-							if (!memberinfo['isMember']) {
-								return this.router.parseUrl('/registration-info');
-							}
-							if (cookieValue && cookieValue !== 'null') {
-								this.cookieService.delete('redirect_after_login', '/', environment.domain);
-								if (this.cookieService.check('redirect_after_login')) {
-									this.cookieService.set('redirect_after_login', null, now(), '/', environment.domain);
-									this.cookieService.set('redirect_after_login', null, now(), '/portal', environment.domain);
+			  this.cookieService.delete('redirect_after_login', '/', environment.domain);
+				this.cookieService.delete('redirect_after_login', '/portal', environment.domain);
+
+				return this.userService.getOnlyLoggedUserWithRedirect(cookieValue || redirectUrl).pipe(
+						switchMap((res: any): Observable<boolean | UrlTree> => {
+								if (res['error']) {
+										window.location.href = environment.debugin;
+										return new Observable<false>;
+
+								} else {
+										this.voService.isVo().subscribe((result: IResponseTemplate): void => {
+												setVO(result.value as boolean);
+										});
+										this.userService.getLoggedUserElixirId().subscribe((result: any): void => {
+												setElixirId(result['elixir_id']);
+										});
+
+										return this.userService.getIsCurrentUserVoMember().pipe(
+												map((memberInfo: any): boolean | UrlTree => {
+														if (!memberInfo['isMember']) {
+																return this.router.parseUrl('/registration-info');
+														}
+														console.debug("Helloooooo")
+														if (cookieValue) {
+																const val: string = cookieValue.substring(2, cookieValue.length - 1);
+																console.debug("###")
+																console.debug(val)
+														 		this.cookieService.delete('redirect_after_login', '/', environment.domain);
+																this.cookieService.delete('redirect_after_login', '/portal', environment.domain);
+																return this.router.parseUrl(val);
+														}
+
+														return true;
+												})
+										);
 								}
-								let val: string = cookieValue;
-								val = val.substring(2);
-								val = val.substring(0, val.length - 1);
-								cookieValue = null;
-
-								return this.router.parseUrl(val);
-							}
-
-							return true;
-						}),
-					);
-				}
-			}),
-		);
-	}
+						})
+				);
+		}
 }
