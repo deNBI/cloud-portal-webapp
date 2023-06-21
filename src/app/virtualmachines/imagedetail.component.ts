3582cc42d764924474ac8feec78eaca9a5e1c6f2
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2023-06-21 11:22:03 +0200

    fix(Research Environments): Sorting according to OS version and build-date

--- src/app/virtualmachines/imagedetail.component.ts
+++ src/app/virtualmachines/imagedetail.component.ts
@@ -133,61 +133,37 @@ export class ImageDetailComponent implements OnInit, OnDestroy {
 		});
 	}
 
-	compareVersions(a: string, b: string): number {
-		if (a === b) {
-			return 0;
-		}
-		const splitA = a.split('.');
-		const splitB = b.split('.');
-		const length = Math.max(splitA.length, splitB.length);
-		// eslint-disable-next-line no-plusplus
-		for (let i = 0; i < length; i++) {
-			if (
-				parseInt(splitA[i], 10) > parseInt(splitB[i], 10)
-				|| (splitA[i] === splitB[i] && Number.isNaN(splitB[i + 1]))
-			) {
-				return 1;
-			}
-			if (
-				parseInt(splitA[i], 10) < parseInt(splitB[i], 10)
-				|| (splitA[i] === splitB[i] && Number.isNaN(splitA[i + 1]))
-			) {
-				return -1;
-			}
-		}
-
-		return 0;
-	}
-
 	sortImages(a: Image, b: Image): number {
-		if (a === b) {
-			return 0;
+		let os_version_compare_value: number = 0;
+		if (a.os_version?.length > 0 && b.os_version?.length > 0) {
+			const a_s: string = a.os_version;
+			const b_s: string = b.os_version;
+			if (a === b) {
+				return 0;
+			}
+			const splitA = a_s.split('.');
+			const splitB = b_s.split('.');
+			const length = Math.max(splitA.length, splitB.length);
+			// eslint-disable-next-line no-plusplus
+			for (let i = 0; i < length; i++) {
+				if (
+					parseInt(splitA[i], 10) > parseInt(splitB[i], 10)
+					|| (splitA[i] === splitB[i] && Number.isNaN(splitB[i + 1]))
+				) {
+					os_version_compare_value = -1;
+				}
+				if (
+					parseInt(splitA[i], 10) < parseInt(splitB[i], 10)
+					|| (splitA[i] === splitB[i] && Number.isNaN(splitA[i + 1]))
+				) {
+					os_version_compare_value = 1;
+				}
+			}
 		}
-		let tags_a: string[] = a.tags;
-		tags_a = tags_a.filter(e => e !== 'deNBI');
-		tags_a = tags_a.filter(e => e !== 'portalclient');
-		tags_a = tags_a.filter(e => e !== 'base_image');
-		const tags_a_stringified: string = JSON.stringify(tags_a);
-
-		let tags_b: string[] = a.tags;
-		tags_b = tags_b.filter(e => e !== 'deNBI');
-		tags_b = tags_b.filter(e => e !== 'portalclient');
-		tags_b = tags_b.filter(e => e !== 'base_image');
-		const tags_b_stringified: string = JSON.stringify(tags_b);
-
-		const tag_compare_value: number = tags_a_stringified.localeCompare(tags_b_stringified);
-		if (tag_compare_value === 0) {
-			let os_version_compare_value: number = 0;
-			if (a.os_version && b.os_version) {
-				os_version_compare_value = this.compareVersions(a.os_version, b.os_version);
-			}
-			if (os_version_compare_value === 0) {
-				return a.name.localeCompare(b.name);
-			} else {
-				return os_version_compare_value;
-			}
+		if (os_version_compare_value === 0) {
+			return a.name.localeCompare(b.name) * -1;
 		} else {
-			return tag_compare_value;
+			return os_version_compare_value;
 		}
 	}
 
