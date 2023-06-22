b4734464728e579891a242bdcbd1a07bd2cc499b
dweinholz <dweinholz@users.noreply.github.com>
2023-06-22 06:58:21 +0000

    fix(Linting):blacked code

--- src/app/shared/modal/email/project-email-modal/project-email-modal.component.ts
+++ src/app/shared/modal/email/project-email-modal/project-email-modal.component.ts
@@ -1,63 +1,59 @@
 import {
-		Component, EventEmitter, Input, OnDestroy, OnInit,
+	Component, EventEmitter, Input, OnDestroy, OnInit,
 } from '@angular/core';
-import {BsModalRef} from 'ngx-bootstrap/modal';
-import {Application} from '../../../../applications/application.model/application.model';
-import {VoService} from '../../../../api-connector/vo.service';
-import {IResponseTemplate} from '../../../../api-connector/response-template';
-import {EmailService} from '../../../../api-connector/email.service';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Application } from '../../../../applications/application.model/application.model';
+import { VoService } from '../../../../api-connector/vo.service';
+import { IResponseTemplate } from '../../../../api-connector/response-template';
+import { EmailService } from '../../../../api-connector/email.service';
 
 @Component({
-		selector: 'app-project-email-modal',
-		templateUrl: './project-email-modal.component.html',
-		styleUrls: ['./projext-email-modal.component.scss'],
-		providers: [EmailService]
+	selector: 'app-project-email-modal',
+	templateUrl: './project-email-modal.component.html',
+	styleUrls: ['./projext-email-modal.component.scss'],
+	providers: [EmailService],
 })
 export class ProjectEmailModalComponent implements OnInit, OnDestroy {
-		// currently only for vo
-		@Input() selectedProjects: Application[];
-		emailAdminsOnly: boolean;
-		emailSubject: string;
-		emailReply: string;
-		emailText: string;
-		templates: string[];
+	// currently only for vo
+	@Input() selectedProjects: Application[];
+	emailAdminsOnly: boolean;
+	emailSubject: string;
+	emailReply: string;
+	emailText: string;
+	templates: string[];
 
-		public event: EventEmitter<boolean> = new EventEmitter();
+	public event: EventEmitter<boolean> = new EventEmitter();
 
-		constructor(public bsModalRef: BsModalRef, private voService: VoService, private emailService: EmailService) {
-				// eslint-disable-next-line no-empty-function
-		}
+	constructor(public bsModalRef: BsModalRef, private voService: VoService, private emailService: EmailService) {
+		// eslint-disable-next-line no-empty-function
+	}
 
-		ngOnInit() {
-				this.getMailTemplates();
-		}
+	ngOnInit() {
+		this.getMailTemplates();
+	}
 
-		getMailTemplates(): void {
-				this.emailService.getMailTemplates().subscribe((res: string[]) => {
-						this.templates = res;
-				});
-		}
+	getMailTemplates(): void {
+		this.emailService.getMailTemplates().subscribe((res: string[]) => {
+			this.templates = res;
+		});
+	}
 
-		sentProjectsMail(): void {
+	sentProjectsMail(): void {
+		const project_ids = this.selectedProjects.map((pr: Application) => pr.project_application_perun_id);
 
-				const project_ids = this.selectedProjects.map((pr: Application) => pr.project_application_perun_id);
+		this.emailService
+			.sendMailToProjects(project_ids, this.emailSubject, this.emailText, this.emailAdminsOnly, this.emailReply)
+			.subscribe(
+				(res: IResponseTemplate) => {
+					this.event.emit(res.value as boolean);
+				},
+				() => {
+					this.event.emit(false);
+				},
+			);
+	}
 
-
-				this.emailService
-						.sendMailToProjects(project_ids, this.emailSubject, this.emailText, this.emailAdminsOnly, this.emailReply)
-						.subscribe(
-								(res: IResponseTemplate) => {
-										this.event.emit(res.value as boolean);
-								},
-								() => {
-										this.event.emit(false);
-								},
-						);
-
-		}
-
-
-		ngOnDestroy(): void {
-				this.bsModalRef.hide();
-		}
+	ngOnDestroy(): void {
+		this.bsModalRef.hide();
+	}
 }
