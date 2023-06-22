00803c94c41cec7a0797875e5e5b5f7985b3ead9
dweinholz <david-weinholz@web.de>
2023-06-22 13:12:31 +0200

    feat(Mail):also templated mails for fm (#5766)
    * feat(Mail):also templated mails for fm
    
    * fixed linting
    
    * fix(Linting):blacked code
    
    ---------
    
    Co-authored-by: dweinholz <dweinholz@users.noreply.github.com>

--- src/app/api-connector/email.service.ts
+++ src/app/api-connector/email.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import {Flavor} from '../virtualmachines/virtualmachinemodels/flavor';
-import {ApiSettings} from './api-settings.service';
-import {map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { ApiSettings } from './api-settings.service';
+import { IResponseTemplate } from './response-template';
 
 /**
  * Service which provides methods for Flavors.
  */
 @Injectable()
 export class EmailService {
-	constructor(private http: HttpClient) {
-	}
-
+	constructor(private http: HttpClient) {}
 
 	getMailTemplates(): Observable<string[]> {
 		return this.http.get<string[]>(`${ApiSettings.getApiBaseURL()}emails/templates/`, {
@@ -20,5 +17,25 @@ export class EmailService {
 		});
 	}
 
-
+	sendMailToProjects(
+		projectIds: (string | number)[],
+		subject: string,
+		message: string,
+		adminsOnly: boolean,
+		reply?: string,
+	): Observable<any> {
+		return this.http.post<IResponseTemplate>(
+			`${ApiSettings.getApiBaseURL()}emails/templated/projects/`,
+			{
+				project_ids: projectIds,
+				subject,
+				message,
+				adminsOnly,
+				reply,
+			},
+			{
+				withCredentials: true,
+			},
+		);
+	}
 }
