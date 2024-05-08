dcc263703ab552454097c2078938565deeba0e49
dweinholz <29981967+dweinholz@users.noreply.github.com>
2024-05-08 06:47:07 +0000

    fix(Linting):blacked code

--- src/app/applications/application-list/application-list.component.spec.ts
+++ src/app/applications/application-list/application-list.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApplicationListComponent } from './application-list.component';
 
 describe('ApplicationListComponent', () => {
-  let component: ApplicationListComponent;
-  let fixture: ComponentFixture<ApplicationListComponent>;
+	let component: ApplicationListComponent;
+	let fixture: ComponentFixture<ApplicationListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ApplicationListComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ApplicationListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ApplicationListComponent],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(ApplicationListComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
