8adee854262dd361277f9aa83045f59b956bd849
Viktor Rudko <accounts@viktorrudko.de>
2024-05-29 12:07:33 +0200

    fix(Matomo): Adjust tracking code

--- src/assets/js/matomo.js
+++ src/assets/js/matomo.js
@@ -1,9 +1,15 @@
-var idSite = 2;
-var matomoTrackingApiUrl = "https.//cloud.denbi.de/matomo/"
-
-var _paq = window._paq = window._paq || [];
-_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-
+var _paq = (window._paq = window._paq || [])
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView'])
+_paq.push(['enableLinkTracking'])
+;(function () {
+	var u = 'https://piwik.cebitec.uni-bielefeld.de/'
+	_paq.push(['setTrackerUrl', u + 'matomo.php'])
+	_paq.push(['setSiteId', '22'])
+	var d = document,
+		g = d.createElement('script'),
+		s = d.getElementsByTagName('script')[0]
+	g.async = true
+	g.src = u + 'matomo.js'
+	s.parentNode.insertBefore(g, s)
+})()
