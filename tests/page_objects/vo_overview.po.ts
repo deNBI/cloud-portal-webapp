416873f77cc7b5087449669c0a82f45caf90d0f1
dweinholz <david-weinholz@web.de>
2024-10-14 14:07:10 +0200

    updated tests

--- tests/page_objects/vo_overview.po.ts
+++ tests/page_objects/vo_overview.po.ts
@@ -1,115 +1,116 @@
-import { expect, Page } from '@playwright/test';
-import { Util } from '../util';
+import { expect, Page } from '@playwright/test'
+import { Util } from '../util'
 
 /**
  *  Vo Overview Page.
  */
 export class VoOverviewPage {
-	private VO_OVERVIEW_URL: string = '/#/vo-manager/overview';
-	private FILTER_PROJECT_NAME_INPUT: string = 'filter_project_name';
-	private SHOW_TERMINATE_PREFIX: string = 'show_terminate_';
-	private TERMINATE_PROJECT_BTN: string = 'approve_terminate_project_btn';
-	private NOTIFICATION_MESSAGE: string = 'notification_message';
-	private CLOSE_NOTIFICATION_BTN: string = 'close_notification_modal_btn';
-	private PROJECT_TERMINATED_MESSAGE: string = 'The project was terminated.';
-	private PROJECT_TERMINATION_FORWARDED_TO_FACILITY: string = 'The request to terminate the project was forwarded to the facility manager.';
+	private VO_OVERVIEW_URL: string = '/#/vo-manager/overview'
+	private FILTER_PROJECT_NAME_INPUT: string = 'filter_project_name'
+	private SHOW_TERMINATE_PREFIX: string = 'show_terminate_'
+	private TERMINATE_PROJECT_BTN: string = 'approve_terminate_project_btn'
+	private NOTIFICATION_MESSAGE: string = 'notification_modal_message'
+	private NOTIFICATION_MESSAGE_TYPE: string = 'notification_message_type'
 
-	private TERMINATE_BUTTON_TEXT: string = 'Terminate Project';
-	private NOTIFICATION_MODAL_TITLE: string = 'notification_modal_title';
-	private SUCCESS: string = 'Success';
-	private SITE_LOADER: string = 'site-loader';
-	private SPINNER: string = 'spinner';
+	private CLOSE_NOTIFICATION_BTN: string = 'close_notification_modal_btn'
+	private PROJECT_TERMINATED_MESSAGE: string = 'The project was terminated.'
+	private PROJECT_TERMINATION_FORWARDED_TO_FACILITY: string =
+		'The request to terminate the project was forwarded to the facility manager.'
 
-	readonly page: Page;
-	readonly baseURL: string;
+	private TERMINATE_BUTTON_TEXT: string = 'Terminate Project'
+	private NOTIFICATION_MODAL_TITLE: string = 'notification_modal_title'
+	private SUCCESS: string = 'Success'
+	private SITE_LOADER: string = 'site-loader'
+	private SPINNER: string = 'spinner'
+
+	readonly page: Page
+	readonly baseURL: string
 
 	constructor(page: Page, baseURL) {
-		this.page = page;
-		this.baseURL = baseURL;
+		this.page = page
+		this.baseURL = baseURL
 	}
 
 	async goto() {
-		console.log('Goto vo manager overview Page');
-		await this.page.goto(this.baseURL + this.VO_OVERVIEW_URL, { waitUntil: 'networkidle' });
-		console.log(this.page.url());
+		console.log('Goto vo manager overview Page')
+		await this.page.goto(this.baseURL + this.VO_OVERVIEW_URL, { waitUntil: 'networkidle' })
+		console.log(this.page.url())
 
-		expect(this.page.url()).toContain(this.VO_OVERVIEW_URL);
-		await this.page.waitForSelector(Util.by_data_test_id_str(this.SITE_LOADER), { state: 'hidden' });
+		expect(this.page.url()).toContain(this.VO_OVERVIEW_URL)
+		await this.page.waitForSelector(Util.by_data_test_id_str(this.SITE_LOADER), { state: 'hidden' })
 
 		try {
-			await this.page.waitForSelector(Util.by_data_test_id_str(this.SPINNER), { state: 'visible', timeout: 5000 });
+			await this.page.waitForSelector(Util.by_data_test_id_str(this.SPINNER), { state: 'visible', timeout: 5000 })
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		} catch (error) {
-			console.log('Spinner not visible within 5 seconds, continuing...');
+			console.log('Spinner not visible within 5 seconds, continuing...')
 		}
 
-		await this.page.waitForSelector(Util.by_data_test_id_str(this.SPINNER), { state: 'hidden' });
+		await this.page.waitForSelector(Util.by_data_test_id_str(this.SPINNER), { state: 'hidden' })
 
 		// Add a 3-second wait at the end
-		await this.page.waitForTimeout(3000);
+		await this.page.waitForTimeout(3000)
 	}
 
 	async filterForProjects(filter: string): Promise<any> {
-		console.log(`Filter for ${filter} Projects`);
-		await this.page.type(Util.by_data_test_id_str(this.FILTER_PROJECT_NAME_INPUT), filter);
+		console.log(`Filter for ${filter} Projects`)
+		await this.page.type(Util.by_data_test_id_str(this.FILTER_PROJECT_NAME_INPUT), filter)
 	}
 
 	async terminateOpenStackProjects(project_name: string): Promise<any> {
-		console.log(`Terminate all openstack projects with name ${project_name}`);
-		await this.goto();
-		await this.filterForProjects(project_name);
+		console.log(`Terminate all openstack projects with name ${project_name}`)
+		await this.goto()
+		await this.filterForProjects(project_name)
 
 		let project_count: number = await this.page
 			.locator(Util.by_data_test_id_str_prefix(this.SHOW_TERMINATE_PREFIX + Util.OPENSTACK_APPLICATION_NAME))
-			.count();
-		console.log(`Terminating ${project_count} openstack projects with name ${project_name}`);
-		// eslint-disable-next-line no-plusplus
+			.count()
+		console.log(`Terminating ${project_count} openstack projects with name ${project_name}`)
+
 		while (project_count > 0) {
-			// eslint-disable-next-line no-await-in-loop
 			await this.page
 				.locator(Util.by_data_test_id_str_prefix(this.SHOW_TERMINATE_PREFIX + Util.OPENSTACK_APPLICATION_NAME))
 				.first()
-				.click();
-			// eslint-disable-next-line no-await-in-loop
-			await this.page.locator(Util.by_data_test_id_str(this.TERMINATE_PROJECT_BTN)).first().click();
+				.click()
 
-			// eslint-disable-next-line no-await-in-loop
-			await expect(this.page.locator(Util.by_data_test_id_str(this.NOTIFICATION_MESSAGE))).toHaveClass(/alert-success/);
-			// eslint-disable-next-line no-await-in-loop
-			await this.page.locator(Util.by_data_test_id_str(this.CLOSE_NOTIFICATION_BTN)).click();
-			await this.page.waitForTimeout(1500);
+			await this.page.locator(Util.by_data_test_id_str(this.TERMINATE_PROJECT_BTN)).first().click()
+
+			await expect(this.page.locator(Util.by_data_test_id_str(this.NOTIFICATION_MESSAGE_TYPE))).toHaveClass(
+				/alert-success/
+			)
+
+			await this.page.locator(Util.by_data_test_id_str(this.CLOSE_NOTIFICATION_BTN)).click()
+			await this.page.waitForTimeout(1500)
 
 			project_count = await this.page
 				.locator(Util.by_data_test_id_str_prefix(this.SHOW_TERMINATE_PREFIX + Util.OPENSTACK_APPLICATION_NAME))
-				.count();
+				.count()
 		}
-
 	}
 
 	async terminateSimpleVMProjects(project_name: string): Promise<any> {
-		console.log(`Terminate all simplevm projects with name ${project_name}`);
-		await this.goto();
-		await this.filterForProjects(project_name);
-		await this.page.waitForTimeout(7500);
+		console.log(`Terminate all simplevm projects with name ${project_name}`)
+		await this.goto()
+		await this.filterForProjects(project_name)
+		await this.page.waitForTimeout(7500)
 		const project_count: number = await this.page
 			.locator(Util.by_data_test_id_str_prefix(this.SHOW_TERMINATE_PREFIX + Util.SIMPLE_VM_APPLICATION_NAME))
-			.count();
-		console.log(`Terminating ${project_count} simplevm projects with name ${project_name}`);
-		// eslint-disable-next-line no-plusplus
+			.count()
+		console.log(`Terminating ${project_count} simplevm projects with name ${project_name}`)
+
 		for (let i = 0; i < project_count; i++) {
-			// eslint-disable-next-line no-await-in-loop
 			await this.page
 				.locator(Util.by_data_test_id_str_prefix(this.SHOW_TERMINATE_PREFIX + Util.SIMPLE_VM_APPLICATION_NAME))
 				.first()
-				.click();
-			// eslint-disable-next-line no-await-in-loop
-			await this.page.locator(Util.by_data_test_id_str(this.TERMINATE_PROJECT_BTN)).first().click();
+				.click()
 
-			// eslint-disable-next-line no-await-in-loop
+			await this.page.locator(Util.by_data_test_id_str(this.TERMINATE_PROJECT_BTN)).first().click()
+
 			await this.page.waitForSelector(
-				`data-test-id=${this.NOTIFICATION_MESSAGE} >> text=${this.PROJECT_TERMINATED_MESSAGE}`,
-			);
-			// eslint-disable-next-line no-await-in-loop
-			await this.page.locator(Util.by_data_test_id_str(this.CLOSE_NOTIFICATION_BTN)).click();
+				`data-test-id=${this.NOTIFICATION_MESSAGE} >> text=${this.PROJECT_TERMINATED_MESSAGE}`
+			)
+
+			await this.page.locator(Util.by_data_test_id_str(this.CLOSE_NOTIFICATION_BTN)).click()
 		}
 	}
 }
