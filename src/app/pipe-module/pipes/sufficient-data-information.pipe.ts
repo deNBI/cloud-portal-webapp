670f751a0a995f5a5dc28928a7216c27c0460288
dweinholz <29981967+dweinholz@users.noreply.github.com>
2025-05-21 11:51:58 +0000

    fix(Linting):blacked code

--- src/app/pipe-module/pipes/sufficient-data-information.pipe.ts
+++ src/app/pipe-module/pipes/sufficient-data-information.pipe.ts
@@ -6,14 +6,14 @@ import { Application } from 'app/applications/application.model/application.mode
 @Pipe({ name: 'sufficientHumanDataInformation', pure: false })
 export class SufficientHumanDataInformationGivenPipe implements PipeTransform {
 	transform(app: Application): boolean {
-        if (app.project_application_person_related_data) {
-            return (
-                app.project_application_no_personal_data || 
-                app.project_application_nonsensitive_data ||
-                app.project_application_sensitive_data    
-            )
-        } else {
-            return app.project_application_no_data_at_all
-        }
+		if (app.project_application_person_related_data) {
+			return (
+				app.project_application_no_personal_data ||
+				app.project_application_nonsensitive_data ||
+				app.project_application_sensitive_data
+			)
+		} else {
+			return app.project_application_no_data_at_all
+		}
 	}
 }
