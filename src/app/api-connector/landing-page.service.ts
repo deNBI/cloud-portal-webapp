bed91eb263774deae38a0a3189021228d30952a4
Viktor Rudko <43116821+vktrrdk@users.noreply.github.com>
2025-08-13 08:42:53 +0200

    Fix/kubernetes on overview (#6449)
    * wip: adjust hard-coded information to website information
    
    * feat(Project Type Overview): Load information from landing page with fallback
    
    * adjusted error handling and alingments

---
+++ src/app/api-connector/landing-page.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ApiSettings } from './api-settings.service'
+
+@Injectable({
+	providedIn: 'root'
+})
+export class LandingPageService {
+	constructor(private http: HttpClient) {}
+
+	getProjectTypeInformation(): Observable<any> {
+		return this.http.get<any>(`${ApiSettings.getWagtailBase()}project_types_information/`, {
+			withCredentials: true
+		})
+	}
+}
