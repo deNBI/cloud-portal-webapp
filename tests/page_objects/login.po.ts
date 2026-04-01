3b45848077b7b6cfbe5af889a60ce6dd9b2aa818
dweinholz <d.weinholz@fz-juelich.de>
2026-04-01 11:09:51 +0000

    fixed login test

--- tests/page_objects/login.po.ts
+++ tests/page_objects/login.po.ts
@@ -123,7 +123,7 @@ export class LoginPagePlaywright {
 
 			await newPage.waitForSelector(`text=Submit`, { timeout: 10000 }); // Wait for the button to appear
 
-			await newPage.locator(`text=Submit`).click();
+			await newPage.locator(`text=Apply for Membership`).click();
 			console.log("Submitted")
 		} catch (error) {
 			console.log(`Didn't load access denied: ${error}`);
